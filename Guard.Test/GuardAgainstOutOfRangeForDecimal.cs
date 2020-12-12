8cc7c67fc5b07a440a82dfc60c7f0eda322bcee9
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 14:19:11 +0330

    Create GuardAgainstOutOfRangeForDecimal.cs

---
+++ Guard.Test/GuardAgainstOutOfRangeForDecimal.cs
@@ -0,0 +1,47 @@
+﻿using Cactus.Blade.Guard;
+using System;
+using Xunit;
+
+namespace Guard.Test
+{
+    public class GuardAgainstOutOfRangeForDecimal
+    {
+        [Theory]
+        [InlineData(1.0, 1.0, 1.0)]
+        [InlineData(1.0, 1.0, 3.0)]
+        [InlineData(2.0, 1.0, 3.0)]
+        [InlineData(3.0, 1.0, 3.0)]
+        public void DoesNothingGivenInRangeValue(decimal input, decimal rangeFrom, decimal rangeTo)
+        {
+            Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo);
+        }
+
+        [Theory]
+        [InlineData(-1.0, 1.0, 3.0)]
+        [InlineData(0.0, 1.0, 3.0)]
+        [InlineData(4.0, 1.0, 3.0)]
+        public void ThrowsGivenOutOfRangeValue(decimal input, decimal rangeFrom, decimal rangeTo)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+
+        [Theory]
+        [InlineData(-1.0, 3.0, 1.0)]
+        [InlineData(0.0, 3.0, 1.0)]
+        [InlineData(4.0, 3.0, 1.0)]
+        public void ThrowsGivenInvalidArgumentValue(decimal input, decimal rangeFrom, decimal rangeTo)
+        {
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+
+        [Theory]
+        [InlineData(1.0, 0.0, 1.0, 1.0)]
+        [InlineData(1.0, 0.0, 3.0, 1.0)]
+        [InlineData(2.0, 1.0, 3.0, 2.0)]
+        [InlineData(3.0, 3.0, 3.0, 3.0)]
+        public void ReturnsExpectedValueGivenInRangeValue(decimal input, decimal rangeFrom, decimal rangeTo, decimal expected)
+        {
+            Assert.Equal(expected, Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+    }
+}
