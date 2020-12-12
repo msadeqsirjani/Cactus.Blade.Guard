89715cae4c6baf0778ba473a0f52b5d7bfd32d14
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 14:19:07 +0330

    Create GuardAgainstOutOfRangeForDouble.cs

---
+++ Guard.Test/GuardAgainstOutOfRangeForDouble.cs
@@ -0,0 +1,47 @@
+﻿using Cactus.Blade.Guard;
+using System;
+using Xunit;
+
+namespace Guard.Test
+{
+    public class GuardAgainstOutOfRangeForDouble
+    {
+        [Theory]
+        [InlineData(1.0, 1.0, 1.0)]
+        [InlineData(1.0, 1.0, 3.0)]
+        [InlineData(2.0, 1.0, 3.0)]
+        [InlineData(3.0, 1.0, 3.0)]
+        public void DoesNothingGivenInRangeValue(double input, double rangeFrom, double rangeTo)
+        {
+            Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo);
+        }
+
+        [Theory]
+        [InlineData(-1.0, 1.0, 3.0)]
+        [InlineData(0.0, 1.0, 3.0)]
+        [InlineData(4.0, 1.0, 3.0)]
+        public void ThrowsGivenOutOfRangeValue(double input, double rangeFrom, double rangeTo)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+
+        [Theory]
+        [InlineData(-1.0, 3.0, 1.0)]
+        [InlineData(0.0, 3.0, 1.0)]
+        [InlineData(4.0, 3.0, 1.0)]
+        public void ThrowsGivenInvalidArgumentValue(double input, double rangeFrom, double rangeTo)
+        {
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+
+        [Theory]
+        [InlineData(1.0, 0.0, 1.0, 1.0)]
+        [InlineData(1.0, 0.0, 3.0, 1.0)]
+        [InlineData(2.0, 1.0, 3.0, 2.0)]
+        [InlineData(3.0, 3.0, 3.0, 3.0)]
+        public void ReturnsExpectedValueGivenInRangeValue(double input, double rangeFrom, double rangeTo, double expected)
+        {
+            Assert.Equal(expected, Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+    }
+}
