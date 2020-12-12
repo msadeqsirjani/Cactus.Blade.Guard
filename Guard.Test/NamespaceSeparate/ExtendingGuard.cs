56653abcc251e02f521e9047c07e6e5c2ac79d6f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 14:18:40 +0330

    Create ExtendingGuard.cs

---
+++ Guard.Test/NamespaceSeparate/ExtendingGuard.cs
@@ -0,0 +1,17 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+namespace Guard.Test.NamespaceSeparate
+{
+    /// <summary>
+    /// An example Guard extension method. Throws if input is "foo".
+    /// </summary>
+    public static class FooGuard
+    {
+        public static void Foo(this IGuardClause guardClause, string input, string parameterName)
+        {
+            if (input?.ToLower() == "foo")
+                throw new ArgumentException("Should not have been foo!", parameterName);
+        }
+    }
+}
