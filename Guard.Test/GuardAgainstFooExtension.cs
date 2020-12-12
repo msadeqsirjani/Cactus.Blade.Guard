3b1a3c29a9bd4187e8b281878f6695a2408ef5da
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 14:18:34 +0330

    Create GuardAgainstFooExtension.cs

---
+++ Guard.Test/GuardAgainstFooExtension.cs
@@ -0,0 +1,21 @@
+﻿using Guard.Test.NamespaceSeparate;
+using System;
+using Xunit;
+
+namespace Guard.Test
+{
+    public class GuardAgainstFooExtension
+    {
+        [Fact]
+        public void ThrowsGivenFoo()
+        {
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Foo("foo", "aParameterName"));
+        }
+
+        [Fact]
+        public void DoesNothingGivenAnythingElse()
+        {
+            Cactus.Blade.Guard.Guard.Against.Foo("anythingElse", "aParameterName");
+        }
+    }
+}
