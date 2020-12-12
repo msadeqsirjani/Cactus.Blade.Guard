9196e83bca636a6417b11e16958fbe238125c523
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 14:18:49 +0330

    Create GuardAgainstOutOfRangeForInt.cs

---
+++ Guard.Test/GuardAgainstOutOfRangeForInt.cs
@@ -0,0 +1,47 @@
+﻿using Cactus.Blade.Guard;
+using System;
+using Xunit;
+
+namespace Guard.Test
+{
+    public class GuardAgainstOutOfRangeForInt
+    {
+        [Theory]
+        [InlineData(1, 1, 1)]
+        [InlineData(1, 1, 3)]
+        [InlineData(2, 1, 3)]
+        [InlineData(3, 1, 3)]
+        public void DoesNothingGivenInRangeValue(int input, int rangeFrom, int rangeTo)
+        {
+            Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo);
+        }
+
+        [Theory]
+        [InlineData(-1, 1, 3)]
+        [InlineData(0, 1, 3)]
+        [InlineData(4, 1, 3)]
+        public void ThrowsGivenOutOfRangeValue(int input, int rangeFrom, int rangeTo)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+
+        [Theory]
+        [InlineData(-1, 3, 1)]
+        [InlineData(0, 3, 1)]
+        [InlineData(4, 3, 1)]
+        public void ThrowsGivenInvalidArgumentValue(int input, int rangeFrom, int rangeTo)
+        {
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1, 1)]
+        [InlineData(1, 1, 3, 1)]
+        [InlineData(2, 1, 3, 2)]
+        [InlineData(3, 1, 3, 3)]
+        public void ReturnsExpectedValueGivenInRangeValue(int input, int rangeFrom, int rangeTo, int expected)
+        {
+            Assert.Equal(expected, Cactus.Blade.Guard.Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+    }
+}
