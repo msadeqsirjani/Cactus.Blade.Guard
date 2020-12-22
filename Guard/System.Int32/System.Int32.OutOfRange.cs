06fa184f7ca8853fad8f5a3579f020c3d0586766
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:17:42 +0330

    Create System.Int32.OutOfRange.cs

---
+++ Guard/System.Int32/System.Int32.OutOfRange.cs
@@ -0,0 +1,96 @@
+﻿using Cactus.Blade.Guard;
+using System;
+using System.ComponentModel;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="input"/> is less than <paramref name="rangeFrom"/> or greater than <paramref name="rangeTo"/>.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="rangeFrom"></param>
+    /// <param name="rangeTo"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static int OutOfRange(this IGuardClause _, int input, int rangeFrom, int rangeTo)
+    {
+        return Guard.Against.OutOfRange<int>(input, rangeFrom, rangeTo);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="input"/> is less than <paramref name="rangeFrom"/> or greater than <paramref name="rangeTo"/>.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="rangeFrom"></param>
+    /// <param name="rangeTo"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static int OutOfRange(this IGuardClause _, int input, string paramName, int rangeFrom, int rangeTo)
+    {
+        return Guard.Against.OutOfRange<int>(input, paramName, rangeFrom, rangeTo);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="input"/> is less than <paramref name="rangeFrom"/> or greater than <paramref name="rangeTo"/>.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <param name="rangeFrom"></param>
+    /// <param name="rangeTo"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static int OutOfRange(this IGuardClause _, int input, string paramName, string message, int rangeFrom, int rangeTo)
+    {
+        return Guard.Against.OutOfRange<int>(input, paramName, message, rangeFrom, rangeTo);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="InvalidEnumArgumentException" /> if <paramref name="input"/> is not a valid enum value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="InvalidEnumArgumentException"></exception>
+    public static int OutOfRange<T>(this IGuardClause _, int input) where T : struct, Enum
+    {
+        return Guard.Against.OutOfRange<T>(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="InvalidEnumArgumentException" /> if <paramref name="input"/> is not a valid enum value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="InvalidEnumArgumentException"></exception>
+    public static int OutOfRange<T>(this IGuardClause _, int input, string paramName) where T : struct, Enum
+    {
+        return Guard.Against.OutOfRange<T>(input, paramName, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="InvalidEnumArgumentException" /> if <paramref name="input"/> is not a valid enum value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="InvalidEnumArgumentException"></exception>
+    public static int OutOfRange<T>(this IGuardClause _, int input, string paramName, string message) where T : struct, Enum
+    {
+        if (!Enum.IsDefined(typeof(T), input))
+            throw new InvalidEnumArgumentException(paramName, input.ToInt32(), typeof(T));
+
+        return input;
+    }
+}
