ad6fb60a495ee4c37d18801037c069adade6f38d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:18:20 +0330

    Create System.NullableSingle.Default.cs

---
+++ Guard/System.NullableSingle/System.NullableSingle.Default.cs
@@ -0,0 +1,44 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static float? Default(this IGuardClause _, float? input)
+    {
+        return Guard.Against.Default<float?>(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static float? Default(this IGuardClause _, float? input, string paramName)
+    {
+        return Guard.Against.Default<float?>(input, paramName, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static float? Default(this IGuardClause _, float? input, string paramName, string message)
+    {
+        return Guard.Against.Default<float?>(input, paramName, message);
+    }
+}
