42f05c495c4d15b609672202afc3516e6dea4c2e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:18:02 +0330

    Create System.NullableGuid.NullOrEmpty.cs

---
+++ Guard/System.NullableGuid/System.NullableGuid.NullOrEmpty.cs
@@ -0,0 +1,50 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static Guid NullOrEmpty(this IGuardClause _, Guid? input)
+    {
+        return Guard.Against.NullOrEmpty(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="parameter"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static Guid NullOrEmpty(this IGuardClause _, Guid? input, string parameter)
+    {
+        return Guard.Against.NullOrEmpty(input, parameter, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static Guid NullOrEmpty(this IGuardClause _, Guid? input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= $"Required input {paramName} was null or empty.";
+
+        if (!input.HasValue || input.Value == Guid.Empty)
+            throw new ArgumentNullException(paramName, message);
+
+        return input.Value;
+    }
+}
