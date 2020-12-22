767f0953f683eaff717abd6cf899f4d34c70e010
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:19:19 +0330

    Create System.TimeSpan.Default.cs

---
+++ Guard/System.TimeSpan/System.TimeSpan.Default.cs
@@ -0,0 +1,44 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static TimeSpan Default(this IGuardClause _, TimeSpan input)
+    {
+        return Guard.Against.Default<TimeSpan>(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static TimeSpan Default(this IGuardClause _, TimeSpan input, string paramName)
+    {
+        return Guard.Against.Default<TimeSpan>(input, paramName, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static TimeSpan Default(this IGuardClause _, TimeSpan input, string paramName, string message)
+    {
+        return Guard.Against.Default<TimeSpan>(input, paramName, message);
+    }
+}
