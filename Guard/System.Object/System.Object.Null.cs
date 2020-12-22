75b035bcacdf48a90950b397ade8f3446c2a895a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:18:39 +0330

    Create System.Object.Null.cs

---
+++ Guard/System.Object/System.Object.Null.cs
@@ -0,0 +1,54 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static object Null(this IGuardClause _, object input)
+    {
+        input = Guard.Against.Null(input, null, null);
+
+        return input;
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="parameter"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static object Null(this IGuardClause _, object input, string parameter)
+    {
+        input = Guard.Against.Null(input, parameter, null);
+
+        return input;
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static object Null(this IGuardClause _, object input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= $"Required input {paramName} was null.";
+
+        if (input.IsNull())
+            throw new ArgumentNullException(paramName, message);
+
+        return input;
+    }
+}
