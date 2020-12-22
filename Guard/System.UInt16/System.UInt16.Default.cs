ed69e347da226f2818cf7ae83fe3cc9c104457ae
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:19:22 +0330

    Create System.UInt16.Default.cs

---
+++ Guard/System.UInt16/System.UInt16.Default.cs
@@ -0,0 +1,44 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static ushort Default(this IGuardClause _, ushort input)
+    {
+        return Guard.Against.Default<ushort>(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static ushort Default(this IGuardClause _, ushort input, string paramName)
+    {
+        return Guard.Against.Default<ushort>(input, paramName, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not default for that type.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static ushort Default(this IGuardClause _, ushort input, string paramName, string message)
+    {
+        return Guard.Against.Default<ushort>(input, paramName, message);
+    }
+}
