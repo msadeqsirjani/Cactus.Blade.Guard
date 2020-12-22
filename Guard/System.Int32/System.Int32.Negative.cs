2b022d02fca78b0104bbd70e85c8608db86dbbc3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:17:38 +0330

    Create System.Int32.Negative.cs

---
+++ Guard/System.Int32/System.Int32.Negative.cs
@@ -0,0 +1,44 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input"/> is negative.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not negative.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static int Negative(this IGuardClause _, int input)
+    {
+        return Guard.Against.Negative<int>(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input"/> is negative.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <returns><paramref name="input" /> if the value is not negative.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static int Negative(this IGuardClause _, int input, string paramName)
+    {
+        return Guard.Against.Negative<int>(input, paramName, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input"/> is negative.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not negative.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static int Negative(this IGuardClause _, int input, string paramName, string message)
+    {
+        return Guard.Against.Negative<int>(input, paramName, message);
+    }
+}
