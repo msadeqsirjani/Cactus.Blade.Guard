a815f50e3f4eb5b59505efad6f52ba4e054f612f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:18:35 +0330

    Create System.Int32.NegativeOrZero.cs

---
+++ Guard/System.Int32/System.Int32.NegativeOrZero.cs
@@ -0,0 +1,41 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentException"/> if <paramref name="input"/> is negative or zero.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not negative or zero.</returns>
+    public static int NegativeOrZero(this IGuardClause _, int input)
+    {
+        return Guard.Against.NegativeOrZero<int>(input);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException"/> if <paramref name="input"/> is negative or zero.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <returns><paramref name="input" /> if the value is not negative or zero.</returns>
+    public static int NegativeOrZero(this IGuardClause _, int input, string paramName)
+    {
+        return Guard.Against.NegativeOrZero<int>(input, paramName);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException"/> if <paramref name="input"/> is negative or zero.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not negative or zero.</returns>
+    public static int NegativeOrZero(this IGuardClause _, int input, string paramName, string message)
+    {
+        return Guard.Against.NegativeOrZero<int>(input, paramName, message);
+    }
+}
