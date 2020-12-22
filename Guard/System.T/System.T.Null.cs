3b14ce2dd78277d74f783cf4cc534719160a1071
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:19:41 +0330

    Create System.T.Null.cs

---
+++ Guard/System.T/System.T.Null.cs
@@ -0,0 +1,50 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static T Null<T>(this IGuardClause _, T input)
+    {
+        return Guard.Against.Null<T>(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="parameter"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static T Null<T>(this IGuardClause _, T input, string parameter)
+    {
+        return Guard.Against.Null(input, parameter, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static T Null<T>(this IGuardClause _, T input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= $"Required input {paramName} was null.";
+
+        if (input is null)
+            throw new ArgumentNullException(paramName, message);
+
+        return input;
+    }
+}
