308c0d0c956d622f02cdd58797e87c0eafe6d7b0
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:18:16 +0330

    Create System.NullableInt64.Null.cs

---
+++ Guard/System.NullableInt64/System.NullableInt64.Null.cs
@@ -0,0 +1,50 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static long Null(this IGuardClause _, long? input)
+    {
+        return Guard.Against.Null(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="parameter"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static long Null(this IGuardClause _, long? input, string parameter)
+    {
+        return Guard.Against.Null(input, parameter, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not null.</returns>
+    public static long Null(this IGuardClause _, long? input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= $"Required input {paramName} was null.";
+
+        if (!input.HasValue)
+            throw new ArgumentNullException(paramName, message);
+
+        return input.Value;
+    }
+}
