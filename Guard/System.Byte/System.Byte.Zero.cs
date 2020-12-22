92cd608fd68eb979ac99321fe8ddb86bf4fb74fc
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-22 18:17:31 +0330

    Create System.Byte.Zero.cs

---
+++ Guard/System.Byte/System.Byte.Zero.cs
@@ -0,0 +1,44 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is zero.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not zero.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static byte Zero(this IGuardClause _, byte input)
+    {
+        return Guard.Against.Zero<byte>(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is zero.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <returns><paramref name="input" /> if the value is not zero.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static byte Zero(this IGuardClause _, byte input, string paramName)
+    {
+        return Guard.Against.Zero<byte>(input, paramName, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is zero.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not zero.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static byte Zero(this IGuardClause _, byte input, string paramName, string message)
+    {
+        return Guard.Against.Zero<byte>(input, paramName, message);
+    }
+}
