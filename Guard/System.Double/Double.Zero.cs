1dc8a84cc1d8376698932519e5e82b29e70368ef
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:04 +0330

    Create Double.Zero.cs

---
+++ Guard/System.Double/Double.Zero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static double Zero(this IGuard _, double input)
+    {
+        return Guard.Against.Zero(input, null, null);
+    }
+
+    public static double Zero(this IGuard _, double input, string paramName)
+    {
+        return Guard.Against.Zero(input, paramName, null);
+    }
+
+
+    public static double Zero(this IGuard _, double input, string paramName, string message)
+    {
+        return Guard.Against.Zero<double>(input, paramName, message);
+    }
+}
