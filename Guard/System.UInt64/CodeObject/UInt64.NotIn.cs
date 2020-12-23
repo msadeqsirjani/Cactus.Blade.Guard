cb55bf88a2f7994ff2b77735aaac48f83da80bfc
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:21:42 +0330

    Create UInt64.NotIn.cs

---
+++ Guard/System.UInt64/CodeObject/UInt64.NotIn.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static ulong NotIn(this IGuard _, ulong value, params ulong[] values)
+    {
+        return Guard.Against.NotIn(value, null, null, values);
+    }
+
+    public static ulong NotIn(this IGuard _, ulong value, string paramName, params ulong[] values)
+    {
+        return Guard.Against.NotIn(value, paramName, null, values);
+    }
+
+    public static ulong NotIn(this IGuard _, ulong value, string paramName, string message, params ulong[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotIn(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.NotIn(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
