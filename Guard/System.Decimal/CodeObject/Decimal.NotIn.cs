f8ac6ece4edaab7afbb11a8618a2e03cb67b6fe4
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:18:24 +0330

    Create Decimal.NotIn.cs

---
+++ Guard/System.Decimal/CodeObject/Decimal.NotIn.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static decimal NotIn(this IGuard _, decimal value, params decimal[] values)
+    {
+        return Guard.Against.NotIn(value, null, null, values);
+    }
+
+    public static decimal NotIn(this IGuard _, decimal value, string paramName, params decimal[] values)
+    {
+        return Guard.Against.NotIn(value, paramName, null, values);
+    }
+
+    public static decimal NotIn(this IGuard _, decimal value, string paramName, string message, params decimal[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotIn(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.NotIn(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
