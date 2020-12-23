ac8aaac255d4e4f5ef947d290b9430df5a37c993
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:21:38 +0330

    Create UInt64.NotInRange.cs

---
+++ Guard/System.UInt64/CodeObject/UInt64.NotInRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static ulong NotInRange(this IGuard _, ulong value, ulong minimum, ulong maximum)
+    {
+        return Guard.Against.NotInRange(value, null, null, minimum, maximum);
+    }
+
+    public static ulong NotInRange(this IGuard _, ulong value, string paramName, ulong minimum, ulong maximum)
+    {
+        return Guard.Against.NotInRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static ulong NotInRange(this IGuard _, ulong value, string paramName, string message, ulong minimum, ulong maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotInRange(value, minimum, maximum);
+
+        if (value.NotInRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
