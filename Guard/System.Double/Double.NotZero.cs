113dfdd2d47e2f1446a09737bc48bdf0733e9e6a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:54 +0330

    Create Double.NotZero.cs

---
+++ Guard/System.Double/Double.NotZero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static double NotZero(this IGuard _, double input)
+    {
+        return Guard.Against.NotZero(input, null, null);
+    }
+
+    public static double NotZero(this IGuard _, double input, string paramName)
+    {
+        return Guard.Against.NotZero(input, paramName, null);
+    }
+
+
+    public static double NotZero(this IGuard _, double input, string paramName, string message)
+    {
+        return Guard.Against.NotZero<double>(input, paramName, message);
+    }
+}
