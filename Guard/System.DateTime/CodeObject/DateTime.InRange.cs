88a8b93f14b558bc3f3521c7c1245ab0a3c0d774
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:15:50 +0330

    Create DateTime.InRange.cs

---
+++ Guard/System.DateTime/CodeObject/DateTime.InRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static DateTime InRange(this IGuard _, DateTime value, DateTime minimum, DateTime maximum)
+    {
+        return Guard.Against.InRange(value, null, null, minimum, maximum);
+    }
+
+    public static DateTime InRange(this IGuard _, DateTime value, string paramName, DateTime minimum, DateTime maximum)
+    {
+        return Guard.Against.InRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static DateTime InRange(this IGuard _, DateTime value, string paramName, string message, DateTime minimum, DateTime maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.InRange(value, minimum, maximum);
+
+        if (value.InRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
