7d9a659545ea56744428a8d662f63316704ec5c5
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:21:46 +0330

    Create UInt64.In.cs

---
+++ Guard/System.UInt64/CodeObject/UInt64.In.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static ulong In(this IGuard _, ulong value, params ulong[] values)
+    {
+        return Guard.Against.In(value, null, null, values);
+    }
+
+    public static ulong In(this IGuard _, ulong value, string paramName, params ulong[] values)
+    {
+        return Guard.Against.In(value, paramName, null, values);
+    }
+
+    public static ulong In(this IGuard _, ulong value, string paramName, string message, params ulong[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.In(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.In(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
