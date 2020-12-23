9b739d098555c139e9a6709004e2f906a4bfce11
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:22:04 +0330

    Create UInt32.NotIn.cs

---
+++ Guard/System.UInt32/CodeObject/UInt32.NotIn.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static uint NotIn(this IGuard _, uint value, params uint[] values)
+    {
+        return Guard.Against.NotIn(value, null, null, values);
+    }
+
+    public static uint NotIn(this IGuard _, uint value, string paramName, params uint[] values)
+    {
+        return Guard.Against.NotIn(value, paramName, null, values);
+    }
+
+    public static uint NotIn(this IGuard _, uint value, string paramName, string message, params uint[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotIn(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.NotIn(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
