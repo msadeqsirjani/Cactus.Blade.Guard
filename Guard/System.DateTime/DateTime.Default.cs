6f221d837c0242c9f52845ea7a4d01bca6b7ee4f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:16:01 +0330

    Create DateTime.Default.cs

---
+++ Guard/System.DateTime/DateTime.Default.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    public static DateTime Default(this IGuard _, DateTime input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static DateTime Default(this IGuard _, DateTime input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static DateTime Default(this IGuard _, DateTime input, string paramName, string message)
+    {
+        return Guard.Against.Default<DateTime>(input, paramName, message);
+    }
+}
