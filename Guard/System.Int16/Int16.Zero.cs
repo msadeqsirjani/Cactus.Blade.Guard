f4cb463ae4dfb4d6cb227fcf898f79ddbff6e8ff
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:14:06 +0330

    Create Int16.Zero.cs

---
+++ Guard/System.Int16/Int16.Zero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static short Zero(this IGuard _, short input)
+    {
+        return Guard.Against.Zero(input, null, null);
+    }
+
+    public static short Zero(this IGuard _, short input, string paramName)
+    {
+        return Guard.Against.Zero(input, paramName, null);
+    }
+
+
+    public static short Zero(this IGuard _, short input, string paramName, string message)
+    {
+        return Guard.Against.Zero<short>(input, paramName, message);
+    }
+}
