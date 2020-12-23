6e22ab6f8396a4bf44688a72597c158db4a91323
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:21:45 +0330

    Create UInt64.InRange.cs

---
+++ Guard/System.UInt64/CodeObject/UInt64.InRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static ulong InRange(this IGuard _, ulong value, ulong minimum, ulong maximum)
+    {
+        return Guard.Against.InRange(value, null, null, minimum, maximum);
+    }
+
+    public static ulong InRange(this IGuard _, ulong value, string paramName, ulong minimum, ulong maximum)
+    {
+        return Guard.Against.InRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static ulong InRange(this IGuard _, ulong value, string paramName, string message, ulong minimum, ulong maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.InRange(value, minimum, maximum);
+
+        if (value.InRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
