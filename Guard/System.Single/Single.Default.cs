387cfa432b3b0a90d2a535ad48646eb70e41a136
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:15:20 +0330

    Create Single.Default.cs

---
+++ Guard/System.Single/Single.Default.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static float Default(this IGuard _, float input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static float Default(this IGuard _, float input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static float Default(this IGuard _, float input, string paramName, string message)
+    {
+        return Guard.Against.Default<float>(input, paramName, message);
+    }
+}
