a4b12e51e750dd79636c03a86d736a18d4be52b1
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:17:18 +0330

    Create Int16.NotIn.cs

---
+++ Guard/System.Int16/CodeObject/Int16.NotIn.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static short NotIn(this IGuard _, short value, params short[] values)
+    {
+        return Guard.Against.NotIn(value, null, null, values);
+    }
+
+    public static short NotIn(this IGuard _, short value, string paramName, params short[] values)
+    {
+        return Guard.Against.NotIn(value, paramName, null, values);
+    }
+
+    public static short NotIn(this IGuard _, short value, string paramName, string message, params short[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotIn(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.NotIn(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
