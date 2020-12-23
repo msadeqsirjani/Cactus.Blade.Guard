895eca83ada8bd9d82921c9fa101b9b4d21b8488
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:15:39 +0330

    Update System.T.Zero.cs

--- Guard/System.T/System.T.Zero.cs
+++ Guard/System.T/System.T.Zero.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
 using System.Collections.Generic;
-using System.Text;
-using Cactus.Blade.Guard;
 
 public static partial class GuardExtension
 {
-    /// <summary>
-    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is zero.
-    /// </summary>
-    /// <param name="_"></param>
-    /// <param name="input"></param>
-    /// <returns><paramref name="input" /> if the value is not zero.</returns>
-    /// <exception cref="ArgumentException"></exception>
     public static T Zero<T>(this IGuard _, T input)
         where T : struct
     {
@@ -19,15 +12,7 @@ public static partial class GuardExtension
 
         return input;
     }
-    
-    /// <summary>
-    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is zero.
-    /// </summary>
-    /// <param name="_"></param>
-    /// <param name="input"></param>
-    /// <param name="paramName"></param>
-    /// <returns><paramref name="input" /> if the value is not zero.</returns>
-    /// <exception cref="ArgumentException"></exception>
+
     public static T Zero<T>(this IGuard _, T input, string paramName)
         where T : struct
     {
@@ -35,21 +20,12 @@ public static partial class GuardExtension
 
         return input;
     }
-    
-    /// <summary>
-    /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is zero.
-    /// </summary>
-    /// <param name="_"></param>
-    /// <param name="input"></param>
-    /// <param name="paramName"></param>
-    /// <param name="message"></param>
-    /// <returns><paramref name="input" /> if the value is not zero.</returns>
-    /// <exception cref="ArgumentException"></exception>
+
     public static T Zero<T>(this IGuard _, T input, string paramName, string message)
         where T : struct
     {
         paramName ??= nameof(input);
-        message ??= $"Required input {paramName} cannot be zero.";
+        message ??= Message.Zero(input);
 
         if (EqualityComparer<T>.Default.Equals(input, default))
             throw new ArgumentException(message, paramName);
