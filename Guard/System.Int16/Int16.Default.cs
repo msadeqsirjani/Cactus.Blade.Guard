981d632d38032b8dc05d4fad4524c2e350a62a9b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:17:23 +0330

    Create Int16.Default.cs

---
+++ Guard/System.Int16/Int16.Default.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static short Default(this IGuard _, short input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static short Default(this IGuard _, short input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static short Default(this IGuard _, short input, string paramName, string message)
+    {
+        return Guard.Against.Default<short>(input, paramName, message);
+    }
+}
