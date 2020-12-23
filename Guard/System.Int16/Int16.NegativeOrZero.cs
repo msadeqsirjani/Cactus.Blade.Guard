222eb80837c1e52120724ecf32b9418f5ca46a30
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:13:59 +0330

    Create Int16.NegativeOrZero.cs

---
+++ Guard/System.Int16/Int16.NegativeOrZero.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static short NegativeOrZero(this IGuard _, short input)
+    {
+        return Guard.Against.NegativeOrZero(input, null, null);
+    }
+
+    public static short NegativeOrZero(this IGuard _, short input, string paramName)
+    {
+        return Guard.Against.NegativeOrZero(input, paramName, null);
+    }
+
+    public static short NegativeOrZero(this IGuard _, short input, string paramName, string message)
+    {
+        return Guard.Against.NegativeOrZero<short>(input, paramName, message);
+    }
+}
