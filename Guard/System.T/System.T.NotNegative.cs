d5eda0c59ce15171a170a0d139cd69976937a826
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:21:22 +0330

    Create System.T.NotNegative.cs

---
+++ Guard/System.T/System.T.NotNegative.cs
@@ -0,0 +1,54 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input"/> is negative.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <returns><paramref name="input" /> if the value is not negative.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    private static T NotNegative<T>(this IGuard _, T input)
+        where T : struct, IComparable
+    {
+        return Guard.Against.NotNegative(input, null, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input"/> is negative.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <returns><paramref name="input" /> if the value is not negative.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    private static T NotNegative<T>(this IGuard _, T input, string paramName)
+        where T : struct, IComparable
+    {
+        return Guard.Against.NotNegative(input, paramName, null);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentException" /> if <paramref name="input"/> is negative.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <returns><paramref name="input" /> if the value is not negative.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    private static T NotNegative<T>(this IGuard _, T input, string paramName, string message)
+        where T : struct, IComparable
+    {
+        paramName ??= nameof(input);
+        message ??= Message.NotNegative(input);
+
+        if (input.CompareTo(default(T)) >= 0)
+            throw new ArgumentException(message, paramName);
+
+        return input;
+    }
+}
