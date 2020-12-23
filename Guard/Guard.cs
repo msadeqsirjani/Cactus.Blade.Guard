0ed40c2ab053683799e54600346eac4523c3e704
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:13:45 +0330

    Update Guard.cs

--- Guard/Guard.cs
+++ Guard/Guard.cs
@@ -5,7 +5,6 @@ namespace Cactus.Blade.Guard
     /// <summary>
     /// An entry point to a set of Guard Clauses defined as extension methods on IGuardClause.
     /// </summary>
-    /// <remarks>See http://www.weeklydevtips.com/004 on Guard Clauses</remarks>
     public class Guard : IGuard
     {
         /// <summary>
