93dd948da79614fae4ec481674cf7acca1550655
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:22:12 +0330

    Create UInt32.In.cs

---
+++ Guard/System.UInt32/CodeObject/UInt32.In.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static uint In(this IGuard _, uint value, params uint[] values)
+    {
+        return Guard.Against.In(value, null, null, values);
+    }
+
+    public static uint In(this IGuard _, uint value, string paramName, params uint[] values)
+    {
+        return Guard.Against.In(value, paramName, null, values);
+    }
+
+    public static uint In(this IGuard _, uint value, string paramName, string message, params uint[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.In(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.In(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
