ebf2d72105bc9dbf1d69e6d70bbad005791c0120
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:52 +0330

    Create Double.NegativeOrZero.cs

---
+++ Guard/System.Double/Double.NegativeOrZero.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static double NegativeOrZero(this IGuard _, double input)
+    {
+        return Guard.Against.NegativeOrZero(input, null, null);
+    }
+
+    public static double NegativeOrZero(this IGuard _, double input, string paramName)
+    {
+        return Guard.Against.NegativeOrZero(input, paramName, null);
+    }
+
+    public static double NegativeOrZero(this IGuard _, double input, string paramName, string message)
+    {
+        return Guard.Against.NegativeOrZero<double>(input, paramName, message);
+    }
+}
