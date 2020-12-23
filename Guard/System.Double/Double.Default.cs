a2d079f0fdae3fb07971437dd22eb88ce8bed304
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:16:59 +0330

    Create Double.Default.cs

---
+++ Guard/System.Double/Double.Default.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static double Default(this IGuard _, double input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static double Default(this IGuard _, double input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static double Default(this IGuard _, double input, string paramName, string message)
+    {
+        return Guard.Against.Default<double>(input, paramName, message);
+    }
+}
