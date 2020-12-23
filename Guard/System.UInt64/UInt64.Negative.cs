e87fe5fdf711632439af3d3ce0d8939fcd7136f8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:32 +0330

    Create UInt64.Negative.cs

---
+++ Guard/System.UInt64/UInt64.Negative.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static ulong Negative(this IGuard _, ulong input)
+    {
+        return Guard.Against.Negative(input, null, null);
+    }
+
+    public static ulong Negative(this IGuard _, ulong input, string paramName)
+    {
+        return Guard.Against.Negative(input, paramName, null);
+    }
+
+    public static ulong Negative(this IGuard _, ulong input, string paramName, string message)
+    {
+        return Guard.Against.Negative<ulong>(input, paramName, message);
+    }
+}
