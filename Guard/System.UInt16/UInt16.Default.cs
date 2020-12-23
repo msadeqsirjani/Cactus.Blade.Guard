063fd2ade7a3fca9c157a9e8aa6106cd758d150a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:23:49 +0330

    Create UInt16.Default.cs

---
+++ Guard/System.UInt16/UInt16.Default.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static ushort Default(this IGuard _, ushort input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static ushort Default(this IGuard _, ushort input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static ushort Default(this IGuard _, ushort input, string paramName, string message)
+    {
+        return Guard.Against.Default<ushort>(input, paramName, message);
+    }
+}
