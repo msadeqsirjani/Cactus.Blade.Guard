0354f6735f7a225438f9f4ef5ed1d94f9242b4b2
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:14:30 +0330

    Create Int64.NotZero.cs

---
+++ Guard/System.Int64/Int64.NotZero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static long NotZero(this IGuard _, long input)
+    {
+        return Guard.Against.NotZero(input, null, null);
+    }
+
+    public static long NotZero(this IGuard _, long input, string paramName)
+    {
+        return Guard.Against.NotZero(input, paramName, null);
+    }
+
+
+    public static long NotZero(this IGuard _, long input, string paramName, string message)
+    {
+        return Guard.Against.NotZero<long>(input, paramName, message);
+    }
+}
