e3aed26b45b984a6d37887919b1b8d0b38c31932
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:18:16 +0330

    Create Decimal.Between.cs

---
+++ Guard/System.Decimal/CodeObject/Decimal.Between.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static decimal Between(this IGuard _, decimal value, decimal minimum, decimal maximum)
+    {
+        return Guard.Against.Between(value, null, null, minimum, maximum);
+    }
+
+    public static decimal Between(this IGuard _, decimal value, string paramName, decimal minimum, decimal maximum)
+    {
+        return Guard.Against.Between(value, paramName, null, minimum, maximum);
+    }
+
+    public static decimal Between(this IGuard _, decimal value, string paramName, string message, decimal minimum, decimal maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.Between(value, minimum, maximum);
+
+        if (value.Between(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
