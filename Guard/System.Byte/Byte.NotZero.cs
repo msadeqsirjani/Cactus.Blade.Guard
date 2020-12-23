277d4b7c66722b002a0cc84f7a35a1858b4de568
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:13:52 +0330

    Create Byte.NotZero.cs

---
+++ Guard/System.Byte/Byte.NotZero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static byte NotZero(this IGuard _, byte input)
+    {
+        return Guard.Against.NotZero(input, null, null);
+    }
+
+    public static byte NotZero(this IGuard _, byte input, string paramName)
+    {
+        return Guard.Against.NotZero(input, paramName, null);
+    }
+
+
+    public static byte NotZero(this IGuard _, byte input, string paramName, string message)
+    {
+        return Guard.Against.NotZero<byte>(input, paramName, message);
+    }
+}
