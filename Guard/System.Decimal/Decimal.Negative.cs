fca87fb3721dbb3f86f714602bdb0db9f4b39aea
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:17:13 +0330

    Create Decimal.Negative.cs

---
+++ Guard/System.Decimal/Decimal.Negative.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static decimal Negative(this IGuard _, decimal input)
+    {
+        return Guard.Against.Negative(input, null, null);
+    }
+
+    public static decimal Negative(this IGuard _, decimal input, string paramName)
+    {
+        return Guard.Against.Negative(input, paramName, null);
+    }
+
+    public static decimal Negative(this IGuard _, decimal input, string paramName, string message)
+    {
+        return Guard.Against.Negative<decimal>(input, paramName, message);
+    }
+}
