c3a4602992262573c35fc9d342083c6bc181d3b1
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:16:20 +0330

    Create DateTimeOffset.NotIn.cs

---
+++ Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.NotIn.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static DateTimeOffset NotIn(this IGuard _, DateTimeOffset value, params DateTimeOffset[] values)
+    {
+        return Guard.Against.NotIn(value, null, null, values);
+    }
+
+    public static DateTimeOffset NotIn(this IGuard _, DateTimeOffset value, string paramName, params DateTimeOffset[] values)
+    {
+        return Guard.Against.NotIn(value, paramName, null, values);
+    }
+
+    public static DateTimeOffset NotIn(this IGuard _, DateTimeOffset value, string paramName, string message, params DateTimeOffset[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotIn(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.NotIn(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
