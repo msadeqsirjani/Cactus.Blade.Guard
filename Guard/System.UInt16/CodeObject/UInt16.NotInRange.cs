4ab2fd80f6ba2d0564326082e959b3122144ba82
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:23:43 +0330

    Create UInt16.NotInRange.cs

---
+++ Guard/System.UInt16/CodeObject/UInt16.NotInRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static ushort NotInRange(this IGuard _, ushort value, ushort minimum, ushort maximum)
+    {
+        return Guard.Against.NotInRange(value, null, null, minimum, maximum);
+    }
+
+    public static ushort NotInRange(this IGuard _, ushort value, string paramName, ushort minimum, ushort maximum)
+    {
+        return Guard.Against.NotInRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static ushort NotInRange(this IGuard _, ushort value, string paramName, string message, ushort minimum, ushort maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotInRange(value, minimum, maximum);
+
+        if (value.NotInRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
