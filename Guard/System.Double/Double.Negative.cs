355a0a956d3dbc809723449f716be4463120efd7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:48 +0330

    Create Double.Negative.cs

---
+++ Guard/System.Double/Double.Negative.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static double Negative(this IGuard _, double input)
+    {
+        return Guard.Against.Negative(input, null, null);
+    }
+
+    public static double Negative(this IGuard _, double input, string paramName)
+    {
+        return Guard.Against.Negative(input, paramName, null);
+    }
+
+    public static double Negative(this IGuard _, double input, string paramName, string message)
+    {
+        return Guard.Against.Negative<double>(input, paramName, message);
+    }
+}
