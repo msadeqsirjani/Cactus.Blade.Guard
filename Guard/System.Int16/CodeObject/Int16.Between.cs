aeba72c9a6b3943c4a2837aa64c8731c45428e12
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:17:12 +0330

    Create Int16.Between.cs

---
+++ Guard/System.Int16/CodeObject/Int16.Between.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static short Between(this IGuard _, short value, short minimum, short maximum)
+    {
+        return Guard.Against.Between(value, null, null, minimum, maximum);
+    }
+
+    public static short Between(this IGuard _, short value, string paramName, short minimum, short maximum)
+    {
+        return Guard.Against.Between(value, paramName, null, minimum, maximum);
+    }
+
+    public static short Between(this IGuard _, short value, string paramName, string message, short minimum, short maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.Between(value, minimum, maximum);
+
+        if (value.Between(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
