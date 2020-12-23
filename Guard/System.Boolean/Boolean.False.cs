cc5393437bd01b63d567b2d17b1fa5519a5aead9
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:14:42 +0330

    Create Boolean.False.cs

---
+++ Guard/System.Boolean/Boolean.False.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static bool False(this IGuard _, bool input)
+    {
+        return Guard.Against.True(input, null, null);
+    }
+
+    public static bool False(this IGuard _, bool input, string paramName)
+    {
+        return Guard.Against.True(input, paramName, null);
+    }
+
+    public static bool False(this IGuard _, bool input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= Message.False(input);
+
+        if (!input) return false;
+
+        throw new ArgumentException(message, paramName);
+    }
+}
