c4bab42b0823a0299791fc24e792ba2f2e42ef2e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:18:10 +0330

    Create Byte.OutOfRange.cs

---
+++ Guard/System.Byte/Byte.OutOfRange.cs
@@ -0,0 +1,50 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="input"/> is less than <paramref name="rangeFrom"/> or greater than <paramref name="rangeTo"/>.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="rangeFrom"></param>
+    /// <param name="rangeTo"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static byte OutOfRange(this IGuard _, byte input, byte rangeFrom, byte rangeTo)
+    {
+        return Guard.Against.OutOfRange<byte>(input, rangeFrom, rangeTo);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="input"/> is less than <paramref name="rangeFrom"/> or greater than <paramref name="rangeTo"/>.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="rangeFrom"></param>
+    /// <param name="rangeTo"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static byte OutOfRange(this IGuard _, byte input, string paramName, byte rangeFrom, byte rangeTo)
+    {
+        return Guard.Against.OutOfRange<byte>(input, paramName, rangeFrom, rangeTo);
+    }
+
+    /// <summary>
+    /// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="input"/> is less than <paramref name="rangeFrom"/> or greater than <paramref name="rangeTo"/>.
+    /// </summary>
+    /// <param name="_"></param>
+    /// <param name="input"></param>
+    /// <param name="paramName"></param>
+    /// <param name="message"></param>
+    /// <param name="rangeFrom"></param>
+    /// <param name="rangeTo"></param>
+    /// <returns><paramref name="input" /> if the value is not out of range.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static byte OutOfRange(this IGuard _, byte input, string paramName, string message, byte rangeFrom, byte rangeTo)
+    {
+        return Guard.Against.OutOfRange<byte>(input, paramName, message, rangeFrom, rangeTo);
+    }
+}
