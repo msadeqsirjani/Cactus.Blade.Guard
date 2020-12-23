9afdeb5b81fcdbcc0c79ca22805ce61c327a372c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:21:50 +0330

    Create UInt64.Between.cs

---
+++ Guard/System.UInt64/CodeObject/UInt64.Between.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static ulong Between(this IGuard _, ulong value, ulong minimum, ulong maximum)
+    {
+        return Guard.Against.Between(value, null, null, minimum, maximum);
+    }
+
+    public static ulong Between(this IGuard _, ulong value, string paramName, ulong minimum, ulong maximum)
+    {
+        return Guard.Against.Between(value, paramName, null, minimum, maximum);
+    }
+
+    public static ulong Between(this IGuard _, ulong value, string paramName, string message, ulong minimum, ulong maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.Between(value, minimum, maximum);
+
+        if (value.Between(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
