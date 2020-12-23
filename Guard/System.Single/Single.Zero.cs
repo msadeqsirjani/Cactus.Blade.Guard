8c7a2ce720bcc1e74ba56246ade66a9593aa53d4
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:15:37 +0330

    Create Single.Zero.cs

---
+++ Guard/System.Single/Single.Zero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static float Zero(this IGuard _, float input)
+    {
+        return Guard.Against.Zero(input, null, null);
+    }
+
+    public static float Zero(this IGuard _, float input, string paramName)
+    {
+        return Guard.Against.Zero(input, paramName, null);
+    }
+
+
+    public static float Zero(this IGuard _, float input, string paramName, string message)
+    {
+        return Guard.Against.Zero<float>(input, paramName, message);
+    }
+}
