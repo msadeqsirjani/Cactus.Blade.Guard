8191d63b8e7a4d57897ae85056fcb7f7f9b9524b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:13:48 +0330

    Create Byte.Negative.cs

---
+++ Guard/System.Byte/Byte.Negative.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static byte Negative(this IGuard _, byte input)
+    {
+        return Guard.Against.Negative(input, null, null);
+    }
+
+    public static byte Negative(this IGuard _, byte input, string paramName)
+    {
+        return Guard.Against.Negative(input, paramName, null);
+    }
+
+    public static byte Negative(this IGuard _, byte input, string paramName, string message)
+    {
+        return Guard.Against.Negative<byte>(input, paramName, message);
+    }
+}
