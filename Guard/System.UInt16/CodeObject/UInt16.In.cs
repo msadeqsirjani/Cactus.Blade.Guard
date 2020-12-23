1527a176c7f1f820f4195cd69660624628b5ee2e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:22:53 +0330

    Create UInt16.In.cs

---
+++ Guard/System.UInt16/CodeObject/UInt16.In.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static ushort In(this IGuard _, ushort value, params ushort[] values)
+    {
+        return Guard.Against.In(value, null, null, values);
+    }
+
+    public static ushort In(this IGuard _, ushort value, string paramName, params ushort[] values)
+    {
+        return Guard.Against.In(value, paramName, null, values);
+    }
+
+    public static ushort In(this IGuard _, ushort value, string paramName, string message, params ushort[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.In(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.In(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
