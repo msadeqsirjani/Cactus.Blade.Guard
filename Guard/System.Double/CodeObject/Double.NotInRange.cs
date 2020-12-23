60b88171424e779cdb86b6027da67ce39e4cb542
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:16:55 +0330

    Create Double.NotInRange.cs

---
+++ Guard/System.Double/CodeObject/Double.NotInRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static double NotInRange(this IGuard _, double value, double minimum, double maximum)
+    {
+        return Guard.Against.NotInRange(value, null, null, minimum, maximum);
+    }
+
+    public static double NotInRange(this IGuard _, double value, string paramName, double minimum, double maximum)
+    {
+        return Guard.Against.NotInRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static double NotInRange(this IGuard _, double value, string paramName, string message, double minimum, double maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotInRange(value, minimum, maximum);
+
+        if (value.NotInRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
