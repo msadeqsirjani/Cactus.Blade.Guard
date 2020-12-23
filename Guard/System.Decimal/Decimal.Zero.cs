e4f668f480afdb9b6d753224234494dd7c8c30e4
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:42 +0330

    Create Decimal.Zero.cs

---
+++ Guard/System.Decimal/Decimal.Zero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static decimal Zero(this IGuard _, decimal input)
+    {
+        return Guard.Against.Zero(input, null, null);
+    }
+
+    public static decimal Zero(this IGuard _, decimal input, string paramName)
+    {
+        return Guard.Against.Zero(input, paramName, null);
+    }
+
+
+    public static decimal Zero(this IGuard _, decimal input, string paramName, string message)
+    {
+        return Guard.Against.Zero<decimal>(input, paramName, message);
+    }
+}
