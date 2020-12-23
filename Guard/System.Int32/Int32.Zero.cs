5b6d323e54e080d4c68f0efa16c11ab032b4a1a8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:14:19 +0330

    Create Int32.Zero.cs

---
+++ Guard/System.Int32/Int32.Zero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static int Zero(this IGuard _, int input)
+    {
+        return Guard.Against.Zero(input, null, null);
+    }
+
+    public static int Zero(this IGuard _, int input, string paramName)
+    {
+        return Guard.Against.Zero(input, paramName, null);
+    }
+
+
+    public static int Zero(this IGuard _, int input, string paramName, string message)
+    {
+        return Guard.Against.Zero<int>(input, paramName, message);
+    }
+}
