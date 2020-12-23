3c7699f36beceed9f05d6f84f2ec062f92b9faaf
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:22:50 +0330

    Create UInt16.Between.cs

---
+++ Guard/System.UInt16/CodeObject/UInt16.Between.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static ushort Between(this IGuard _, ushort value, ushort minimum, ushort maximum)
+    {
+        return Guard.Against.Between(value, null, null, minimum, maximum);
+    }
+
+    public static ushort Between(this IGuard _, ushort value, string paramName, ushort minimum, ushort maximum)
+    {
+        return Guard.Against.Between(value, paramName, null, minimum, maximum);
+    }
+
+    public static ushort Between(this IGuard _, ushort value, string paramName, string message, ushort minimum, ushort maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.Between(value, minimum, maximum);
+
+        if (value.Between(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
