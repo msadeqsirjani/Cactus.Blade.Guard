bdad53e99e9dc05e3aa16b19285dbca4c0ca0c37
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:20:30 +0330

    Create Int64.Between.cs

---
+++ Guard/System.Int64/CodeObject/Int64.Between.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static long Between(this IGuard _, long value, long minimum, long maximum)
+    {
+        return Guard.Against.Between(value, null, null, minimum, maximum);
+    }
+
+    public static long Between(this IGuard _, long value, string paramName, long minimum, long maximum)
+    {
+        return Guard.Against.Between(value, paramName, null, minimum, maximum);
+    }
+
+    public static long Between(this IGuard _, long value, string paramName, string message, long minimum, long maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.Between(value, minimum, maximum);
+
+        if (value.Between(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
