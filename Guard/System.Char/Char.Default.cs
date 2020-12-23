77c40786eb2837a6125ca4804932a20140de255c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:15:25 +0330

    Create Char.Default.cs

---
+++ Guard/System.Char/Char.Default.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static char Default(this IGuard _, char input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static char Default(this IGuard _, char input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static char Default(this IGuard _, char input, string paramName, string message)
+    {
+        return Guard.Against.Default<char>(input, paramName, message);
+    }
+}
