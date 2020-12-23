006641dd8e1e709edd1b1b242439e7f58156a3cb
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:15:56 +0330

    Create UInt16.Negative.cs

---
+++ Guard/System.UInt16/UInt16.Negative.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static ushort Negative(this IGuard _, ushort input)
+    {
+        return Guard.Against.Negative(input, null, null);
+    }
+
+    public static ushort Negative(this IGuard _, ushort input, string paramName)
+    {
+        return Guard.Against.Negative(input, paramName, null);
+    }
+
+    public static ushort Negative(this IGuard _, ushort input, string paramName, string message)
+    {
+        return Guard.Against.Negative<ushort>(input, paramName, message);
+    }
+}
