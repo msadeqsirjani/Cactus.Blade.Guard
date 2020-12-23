415ac873a2059dd8fdb1242bbee89c9af422e7a1
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:15:21 +0330

    Create Byte.NotInRange.cs

---
+++ Guard/System.Byte/CodeObject/Byte.NotInRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static byte NotInRange(this IGuard _, byte value, byte minimum, byte maximum)
+    {
+        return Guard.Against.NotInRange(value, null, null, minimum, maximum);
+    }
+
+    public static byte NotInRange(this IGuard _, byte value, string paramName, byte minimum, byte maximum)
+    {
+        return Guard.Against.NotInRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static byte NotInRange(this IGuard _, byte value, string paramName, string message, byte minimum, byte maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotInRange(value, minimum, maximum);
+
+        if (((short)value).NotInRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
