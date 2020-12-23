e45686c1f4ea278a03bf64b82034184b6b9b46b8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:19:09 +0330

    Create Int32.NotInRange.cs

---
+++ Guard/System.Int32/CodeObject/Int32.NotInRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static int NotInRange(this IGuard _, int value, int minimum, int maximum)
+    {
+        return Guard.Against.NotInRange(value, null, null, minimum, maximum);
+    }
+
+    public static int NotInRange(this IGuard _, int value, string paramName, int minimum, int maximum)
+    {
+        return Guard.Against.NotInRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static int NotInRange(this IGuard _, int value, string paramName, string message, int minimum, int maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotInRange(value, minimum, maximum);
+
+        if (value.NotInRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
