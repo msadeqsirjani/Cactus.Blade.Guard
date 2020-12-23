40139ea87e176bfe637727ee730d269ee5c07cfc
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:15:22 +0330

    Create Int32.NotZero.cs

---
+++ Guard/System.Int32/Int32.NotZero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static int NotZero(this IGuard _, int input)
+    {
+        return Guard.Against.NotZero(input, null, null);
+    }
+
+    public static int NotZero(this IGuard _, int input, string paramName)
+    {
+        return Guard.Against.NotZero(input, paramName, null);
+    }
+
+
+    public static int NotZero(this IGuard _, int input, string paramName, string message)
+    {
+        return Guard.Against.NotZero<int>(input, paramName, message);
+    }
+}
