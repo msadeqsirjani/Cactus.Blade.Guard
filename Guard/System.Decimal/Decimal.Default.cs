53624dcf63fb811de27d3aba4f36e0702af2aec6
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:18:30 +0330

    Create Decimal.Default.cs

---
+++ Guard/System.Decimal/Decimal.Default.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static decimal Default(this IGuard _, decimal input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static decimal Default(this IGuard _, decimal input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static decimal Default(this IGuard _, decimal input, string paramName, string message)
+    {
+        return Guard.Against.Default<decimal>(input, paramName, message);
+    }
+}
