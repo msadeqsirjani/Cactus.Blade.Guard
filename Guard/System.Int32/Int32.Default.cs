c2b53f0700f755bae69069a53df612aa7f9724e3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:19:19 +0330

    Create Int32.Default.cs

---
+++ Guard/System.Int32/Int32.Default.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static int Default(this IGuard _, int input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static int Default(this IGuard _, int input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static int Default(this IGuard _, int input, string paramName, string message)
+    {
+        return Guard.Against.Default<int>(input, paramName, message);
+    }
+}
