50a7ead3f5b1e06f6f5bc1637e77039dae6ac364
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:16:17 +0330

    Create DateTimeOffset.InRange.cs

---
+++ Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.InRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static DateTimeOffset InRange(this IGuard _, DateTimeOffset value, DateTimeOffset minimum, DateTimeOffset maximum)
+    {
+        return Guard.Against.InRange(value, null, null, minimum, maximum);
+    }
+
+    public static DateTimeOffset InRange(this IGuard _, DateTimeOffset value, string paramName, DateTimeOffset minimum, DateTimeOffset maximum)
+    {
+        return Guard.Against.InRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static DateTimeOffset InRange(this IGuard _, DateTimeOffset value, string paramName, string message, DateTimeOffset minimum, DateTimeOffset maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.InRange(value, minimum, maximum);
+
+        if (value.InRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
