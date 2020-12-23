3765ebb41a2990a857a504058be662ebd90f4f69
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:20:41 +0330

    Create Int64.Default.cs

---
+++ Guard/System.Int64/Int64.Default.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static long Default(this IGuard _, long input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static long Default(this IGuard _, long input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static long Default(this IGuard _, long input, string paramName, string message)
+    {
+        return Guard.Against.Default<long>(input, paramName, message);
+    }
+}
