9770c74aa8014d9ed405499951799c3a6d95e03a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:17:31 +0330

    Create DateTimeOffset.Default.cs

---
+++ Guard/System.DateTimeOffset/DateTimeOffset.Default.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    public static DateTimeOffset Default(this IGuard _, DateTimeOffset input)
+    {
+        return Guard.Against.Default(input, null, null);
+    }
+
+    public static DateTimeOffset Default(this IGuard _, DateTimeOffset input, string paramName)
+    {
+        return Guard.Against.Default(input, paramName, null);
+    }
+
+    public static DateTimeOffset Default(this IGuard _, DateTimeOffset input, string paramName, string message)
+    {
+        return Guard.Against.Default<DateTimeOffset>(input, paramName, message);
+    }
+}
