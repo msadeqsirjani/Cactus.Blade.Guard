38a3a459604e3a9430fead8d862949bcdbed3cad
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:14:26 +0330

    Create Int64.Negative.cs

---
+++ Guard/System.Int64/Int64.Negative.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static long Negative(this IGuard _, long input)
+    {
+        return Guard.Against.Negative(input, null, null);
+    }
+
+    public static long Negative(this IGuard _, long input, string paramName)
+    {
+        return Guard.Against.Negative(input, paramName, null);
+    }
+
+    public static long Negative(this IGuard _, long input, string paramName, string message)
+    {
+        return Guard.Against.Negative<long>(input, paramName, message);
+    }
+}
