ee9fd97c028502fe951d75ac71ebf16333d4b2cf
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:22:11 +0330

    Create UInt32.InRange.cs

---
+++ Guard/System.UInt32/CodeObject/UInt32.InRange.cs
@@ -0,0 +1,26 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static uint InRange(this IGuard _, uint value, uint minimum, uint maximum)
+    {
+        return Guard.Against.InRange(value, null, null, minimum, maximum);
+    }
+
+    public static uint InRange(this IGuard _, uint value, string paramName, uint minimum, uint maximum)
+    {
+        return Guard.Against.InRange(value, paramName, null, minimum, maximum);
+    }
+
+    public static uint InRange(this IGuard _, uint value, string paramName, string message, uint minimum, uint maximum)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.InRange(value, minimum, maximum);
+
+        if (value.InRange(minimum, maximum)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
