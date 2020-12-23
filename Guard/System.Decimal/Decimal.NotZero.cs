3d053766538d22af0fec2a5750fcead807cc1a93
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:40 +0330

    Create Decimal.NotZero.cs

---
+++ Guard/System.Decimal/Decimal.NotZero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static decimal NotZero(this IGuard _, decimal input)
+    {
+        return Guard.Against.NotZero(input, null, null);
+    }
+
+    public static decimal NotZero(this IGuard _, decimal input, string paramName)
+    {
+        return Guard.Against.NotZero(input, paramName, null);
+    }
+
+
+    public static decimal NotZero(this IGuard _, decimal input, string paramName, string message)
+    {
+        return Guard.Against.NotZero<decimal>(input, paramName, message);
+    }
+}
