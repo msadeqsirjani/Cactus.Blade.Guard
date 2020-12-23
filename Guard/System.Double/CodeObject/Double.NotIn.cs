5c08b3751a1808bc8c634888542482bf70bb35ee
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:16:52 +0330

    Create Double.NotIn.cs

---
+++ Guard/System.Double/CodeObject/Double.NotIn.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static double NotIn(this IGuard _, double value, params double[] values)
+    {
+        return Guard.Against.NotIn(value, null, null, values);
+    }
+
+    public static double NotIn(this IGuard _, double value, string paramName, params double[] values)
+    {
+        return Guard.Against.NotIn(value, paramName, null, values);
+    }
+
+    public static double NotIn(this IGuard _, double value, string paramName, string message, params double[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotIn(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.NotIn(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
