8e08431cca8ed2660657aa3e1df3dd56bf3a3e1c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:15:43 +0330

    Create DateTime.In.cs

---
+++ Guard/System.DateTime/CodeObject/DateTime.In.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static DateTime NotIn(this IGuard _, DateTime value, params DateTime[] values)
+    {
+        return Guard.Against.NotIn(value, null, null, values);
+    }
+
+    public static DateTime NotIn(this IGuard _, DateTime value, string paramName, params DateTime[] values)
+    {
+        return Guard.Against.NotIn(value, paramName, null, values);
+    }
+
+    public static DateTime NotIn(this IGuard _, DateTime value, string paramName, string message, params DateTime[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotIn(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.NotIn(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
