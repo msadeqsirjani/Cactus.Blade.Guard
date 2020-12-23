c85ad69454eda991b093b82b91020a85b734c12a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:18:20 +0330

    Create Decimal.In.cs

---
+++ Guard/System.Decimal/CodeObject/Decimal.In.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static decimal In(this IGuard _, decimal value, params decimal[] values)
+    {
+        return Guard.Against.In(value, null, null, values);
+    }
+
+    public static decimal In(this IGuard _, decimal value, string paramName, params decimal[] values)
+    {
+        return Guard.Against.In(value, paramName, null, values);
+    }
+
+    public static decimal In(this IGuard _, decimal value, string paramName, string message, params decimal[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.In(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.In(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
