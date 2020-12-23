52014c909a45e2ba5e4afa06cec5472d1081fb3b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:15:23 +0330

    Create Single.Negative.cs

---
+++ Guard/System.Single/Single.Negative.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static float Negative(this IGuard _, float input)
+    {
+        return Guard.Against.Negative(input, null, null);
+    }
+
+    public static float Negative(this IGuard _, float input, string paramName)
+    {
+        return Guard.Against.Negative(input, paramName, null);
+    }
+
+    public static float Negative(this IGuard _, float input, string paramName, string message)
+    {
+        return Guard.Against.Negative<float>(input, paramName, message);
+    }
+}
