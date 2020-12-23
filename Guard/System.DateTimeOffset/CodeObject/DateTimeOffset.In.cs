730cd52a195d7b1a44903b8e7379ec6cc0300f44
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:16:15 +0330

    Create DateTimeOffset.In.cs

---
+++ Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.In.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static DateTimeOffset In(this IGuard _, DateTimeOffset value, params DateTimeOffset[] values)
+    {
+        return Guard.Against.In(value, null, null, values);
+    }
+
+    public static DateTimeOffset In(this IGuard _, DateTimeOffset value, string paramName, params DateTimeOffset[] values)
+    {
+        return Guard.Against.In(value, paramName, null, values);
+    }
+
+    public static DateTimeOffset In(this IGuard _, DateTimeOffset value, string paramName, string message, params DateTimeOffset[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.In(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.In(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
