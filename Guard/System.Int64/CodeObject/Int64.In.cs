a66857db2baf965a79853d35c0f632f57e01b8d1
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 16:20:32 +0330

    Create Int64.In.cs

---
+++ Guard/System.Int64/CodeObject/Int64.In.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static long In(this IGuard _, long value, params long[] values)
+    {
+        return Guard.Against.In(value, null, null, values);
+    }
+
+    public static long In(this IGuard _, long value, string paramName, params long[] values)
+    {
+        return Guard.Against.In(value, paramName, null, values);
+    }
+
+    public static long In(this IGuard _, long value, string paramName, string message, params long[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.In(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.In(values)) return value;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
