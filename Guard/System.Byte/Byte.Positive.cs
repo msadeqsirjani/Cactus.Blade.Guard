867c2d25cb4c1523c4f21d12c8a91239379a8327
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:57 +0330

    Create Byte.Positive.cs

---
+++ Guard/System.Byte/Byte.Positive.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static byte Positive(this IGuard _, byte input)
+    {
+        return Guard.Against.Positive(input, null, null);
+    }
+
+    public static byte Positive(this IGuard _, byte input, string paramName)
+    {
+        return Guard.Against.Positive(input, paramName, null);
+    }
+
+    public static byte Positive(this IGuard _, byte input, string paramName, string message)
+    {
+        return Guard.Against.Positive<byte>(input, paramName, message);
+    }
+}
