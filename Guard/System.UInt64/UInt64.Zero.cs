d8f703b1f85b33b9b7fd941416c474da8f991d2c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-23 19:16:30 +0330

    Create UInt64.Zero.cs

---
+++ Guard/System.UInt64/UInt64.Zero.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static ulong Zero(this IGuard _, ulong input)
+    {
+        return Guard.Against.Zero(input, null, null);
+    }
+
+    public static ulong Zero(this IGuard _, ulong input, string paramName)
+    {
+        return Guard.Against.Zero(input, paramName, null);
+    }
+
+
+    public static ulong Zero(this IGuard _, ulong input, string paramName, string message)
+    {
+        return Guard.Against.Zero<ulong>(input, paramName, message);
+    }
+}
