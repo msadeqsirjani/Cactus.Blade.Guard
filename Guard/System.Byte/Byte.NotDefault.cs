fa189a37e4e1ee84a7b76186be99af0e5a6fccff
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:52:46 +0330

    Create Byte.NotDefault.cs

---
+++ Guard/System.Byte/Byte.NotDefault.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static byte NotDefault(this IGuard _, byte input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static byte NotDefault(this IGuard _, byte input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static byte NotDefault(this IGuard _, byte input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<byte>(input, paramName, message);
+    }
+}
