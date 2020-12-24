0c1eead9e3eda2c9fe6d9dde770317d52965617c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:02:00 +0330

    Update Byte.NotZero.cs

--- Guard/System.Byte/Byte.NotZero.cs
+++ Guard/System.Byte/Byte.NotZero.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static byte NotZero(this IGuard _, byte input)
+    public static IGuard NotZero(this IGuard _, byte input)
     {
         return Guard.MustBe.NotZero(input, null, null);
     }
 
-    public static byte NotZero(this IGuard _, byte input, string paramName)
+    public static IGuard NotZero(this IGuard _, byte input, string paramName)
     {
         return Guard.MustBe.NotZero(input, paramName, null);
     }
 
 
-    public static byte NotZero(this IGuard _, byte input, string paramName, string message)
+    public static IGuard NotZero(this IGuard _, byte input, string paramName, string message)
     {
         return Guard.MustBe.NotZero<byte>(input, paramName, message);
     }
