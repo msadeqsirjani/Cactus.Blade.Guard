110093a2ae9c72a85d6821d5a20059aa2c368f0d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:53:22 +0330

    Create TimeSpan.NotDefault.cs

---
+++ Guard/System.TimeSpan/TimeSpan.NotDefault.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    public static TimeSpan NotDefault(this IGuard _, TimeSpan input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static TimeSpan NotDefault(this IGuard _, TimeSpan input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static TimeSpan NotDefault(this IGuard _, TimeSpan input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<TimeSpan>(input, paramName, message);
+    }
+}
