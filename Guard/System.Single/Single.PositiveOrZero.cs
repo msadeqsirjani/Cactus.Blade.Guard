fdb8accb956356d0639d97dba754121e85e44192
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:34:59 +0330

    Create Single.PositiveOrZero.cs

---
+++ Guard/System.Single/Single.PositiveOrZero.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static float PositiveOrZero(this IGuard _, float input)
+    {
+        return Guard.MustBe.PositiveOrZero(input, null, null);
+    }
+
+    public static float PositiveOrZero(this IGuard _, float input, string paramName)
+    {
+        return Guard.MustBe.PositiveOrZero(input, paramName, null);
+    }
+
+    public static float PositiveOrZero(this IGuard _, float input, string paramName, string message)
+    {
+        return Guard.MustBe.PositiveOrZero<float>(input, paramName, message);
+    }
+}
