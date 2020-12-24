2c055dacc70bedfa43e69a444c4454dadffd3190
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:33:41 +0330

    Update Program.cs

--- Guard.Sample/Program.cs
+++ Guard.Sample/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Guard.Sample
+﻿using System;
+
+namespace Sample
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Operation is starting ...");
+
+            IExecutable executable = new Executable();
+
+            executable.Execute();
+
+            Console.WriteLine("Operation is stopping ...");
         }
     }
 }
