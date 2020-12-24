18cd1450bd2d64994ab4ccc9d34f1a3882d32062
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:02:23 +0330

    Update T.Positive.cs

--- Guard/System.T/T.Positive.cs
+++ Guard/System.T/T.Positive.cs
@@ -4,19 +4,19 @@ using System;
 
 public static partial class GuardExtension
 {
-    private static T Positive<T>(this IGuard _, T input)
+    private static IGuard Positive<T>(this IGuard _, T input)
         where T : struct, IComparable
     {
-        return Guard.MustBe.Negative(input, null, null);
+        return Guard.MustBe.Positive(input, null, null);
     }
 
-    private static T Positive<T>(this IGuard _, T input, string paramName)
+    private static IGuard Positive<T>(this IGuard _, T input, string paramName)
         where T : struct, IComparable
     {
-        return Guard.MustBe.Negative(input, paramName, null);
+        return Guard.MustBe.Positive(input, paramName, null);
     }
 
-    private static T Positive<T>(this IGuard _, T input, string paramName, string message)
+    private static IGuard Positive<T>(this IGuard guard, T input, string paramName, string message)
         where T : struct, IComparable
     {
         paramName ??= nameof(input);
@@ -25,6 +25,6 @@ public static partial class GuardExtension
         if (input.CompareTo(default(T)) > 0)
             throw new ArgumentException(message, paramName);
 
-        return input;
+        return guard;
     }
 }
