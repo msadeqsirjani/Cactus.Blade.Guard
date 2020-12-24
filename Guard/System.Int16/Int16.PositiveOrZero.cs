83f634c12000f236b8f761373c28ab051d855e1c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:34:43 +0330

    Create Int16.PositiveOrZero.cs

---
+++ Guard/System.Int16/Int16.PositiveOrZero.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static short PositiveOrZero(this IGuard _, short input)
+    {
+        return Guard.MustBe.PositiveOrZero(input, null, null);
+    }
+
+    public static short PositiveOrZero(this IGuard _, short input, string paramName)
+    {
+        return Guard.MustBe.PositiveOrZero(input, paramName, null);
+    }
+
+    public static short PositiveOrZero(this IGuard _, short input, string paramName, string message)
+    {
+        return Guard.MustBe.PositiveOrZero<short>(input, paramName, message);
+    }
+}
