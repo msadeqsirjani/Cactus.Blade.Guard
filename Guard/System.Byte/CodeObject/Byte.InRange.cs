6d3e2210149db965192dcc3292df3a50ed6b3d7d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:45:49 +0330

    Update Byte.InRange.cs

--- Guard/System.Byte/CodeObject/Byte.InRange.cs
+++ Guard/System.Byte/CodeObject/Byte.InRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static byte InRange(this IGuard _, byte value, byte minimum, byte maximum)
+    public static IGuard InRange(this IGuard _, byte value, byte minimum, byte maximum)
     {
         return Guard.MustBe.InRange(value, null, null, minimum, maximum);
     }
 
-    public static byte InRange(this IGuard _, byte value, string paramName, byte minimum, byte maximum)
+    public static IGuard InRange(this IGuard _, byte value, string paramName, byte minimum, byte maximum)
     {
         return Guard.MustBe.InRange(value, paramName, null, minimum, maximum);
     }
 
-    public static byte InRange(this IGuard _, byte value, string paramName, string message, byte minimum, byte maximum)
+    public static IGuard InRange(this IGuard guard, byte value, string paramName, string message, byte minimum, byte maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.InRange(value, minimum, maximum);
 
-        if (value.InRange(minimum, maximum)) return value;
+        if (value.InRange(minimum, maximum)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
