d1c955028251c80d1407c8c552e78566ab65c698
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:34:55 +0330

    Create Single.Positive.cs

---
+++ Guard/System.Single/Single.Positive.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static float Positive(this IGuard _, float input)
+    {
+        return Guard.MustBe.Positive(input, null, null);
+    }
+
+    public static float Positive(this IGuard _, float input, string paramName)
+    {
+        return Guard.MustBe.Positive(input, paramName, null);
+    }
+
+    public static float Positive(this IGuard _, float input, string paramName, string message)
+    {
+        return Guard.MustBe.Positive<float>(input, paramName, message);
+    }
+}
