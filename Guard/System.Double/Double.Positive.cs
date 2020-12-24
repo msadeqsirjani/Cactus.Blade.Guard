b01138ea93d5db234e77852aed05e2e4c677e913
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:34:34 +0330

    Create Double.Positive.cs

---
+++ Guard/System.Double/Double.Positive.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static short Positive(this IGuard _, short input)
+    {
+        return Guard.MustBe.Positive(input, null, null);
+    }
+
+    public static short Positive(this IGuard _, short input, string paramName)
+    {
+        return Guard.MustBe.Positive(input, paramName, null);
+    }
+
+    public static short Positive(this IGuard _, short input, string paramName, string message)
+    {
+        return Guard.MustBe.Positive<short>(input, paramName, message);
+    }
+}
