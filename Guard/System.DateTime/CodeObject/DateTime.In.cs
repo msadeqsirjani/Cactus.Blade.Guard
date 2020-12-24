d7bfffec14ee7866c9103016bddd3bc87bf3f4e3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:05:29 +0330

    Update DateTime.In.cs

--- Guard/System.DateTime/CodeObject/DateTime.In.cs
+++ Guard/System.DateTime/CodeObject/DateTime.In.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static DateTime NotIn(this IGuard _, DateTime value, params DateTime[] values)
+    public static IGuard NotIn(this IGuard _, DateTime value, params DateTime[] values)
     {
         return Guard.MustBe.NotIn(value, null, null, values);
     }
 
-    public static DateTime NotIn(this IGuard _, DateTime value, string paramName, params DateTime[] values)
+    public static IGuard NotIn(this IGuard _, DateTime value, string paramName, params DateTime[] values)
     {
         return Guard.MustBe.NotIn(value, paramName, null, values);
     }
 
-    public static DateTime NotIn(this IGuard _, DateTime value, string paramName, string message, params DateTime[] values)
+    public static IGuard NotIn(this IGuard guard, DateTime value, string paramName, string message, params DateTime[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.NotIn(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.NotIn(values)) return value;
+        if (value.NotIn(values)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
