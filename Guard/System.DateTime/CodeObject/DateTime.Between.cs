e9d721e836ce14a97178d880e5dfdf05f4b81bb4
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:05:28 +0330

    Update DateTime.Between.cs

--- Guard/System.DateTime/CodeObject/DateTime.Between.cs
+++ Guard/System.DateTime/CodeObject/DateTime.Between.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static DateTime Between(this IGuard _, DateTime value, DateTime minimum, DateTime maximum)
+    public static IGuard Between(this IGuard _, DateTime value, DateTime minimum, DateTime maximum)
     {
         return Guard.MustBe.Between(value, null, null, minimum, maximum);
     }
 
-    public static DateTime Between(this IGuard _, DateTime value, string paramName, DateTime minimum, DateTime maximum)
+    public static IGuard Between(this IGuard _, DateTime value, string paramName, DateTime minimum, DateTime maximum)
     {
         return Guard.MustBe.Between(value, paramName, null, minimum, maximum);
     }
 
-    public static DateTime Between(this IGuard _, DateTime value, string paramName, string message, DateTime minimum, DateTime maximum)
+    public static IGuard Between(this IGuard guard, DateTime value, string paramName, string message, DateTime minimum, DateTime maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.Between(value, minimum, maximum);
 
-        if (value.Between(minimum, maximum)) return value;
+        if (value.Between(minimum, maximum)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
