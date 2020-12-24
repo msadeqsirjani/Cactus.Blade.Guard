8ddb279ff8ac625b56299295ec107bb14a8a28d3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:07:45 +0330

    Update DateTimeOffset.InRange.cs

--- Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.InRange.cs
+++ Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.InRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static DateTimeOffset InRange(this IGuard _, DateTimeOffset value, DateTimeOffset minimum, DateTimeOffset maximum)
+    public static IGuard InRange(this IGuard _, DateTimeOffset value, DateTimeOffset minimum, DateTimeOffset maximum)
     {
         return Guard.MustBe.InRange(value, null, null, minimum, maximum);
     }
 
-    public static DateTimeOffset InRange(this IGuard _, DateTimeOffset value, string paramName, DateTimeOffset minimum, DateTimeOffset maximum)
+    public static IGuard InRange(this IGuard _, DateTimeOffset value, string paramName, DateTimeOffset minimum, DateTimeOffset maximum)
     {
         return Guard.MustBe.InRange(value, paramName, null, minimum, maximum);
     }
 
-    public static DateTimeOffset InRange(this IGuard _, DateTimeOffset value, string paramName, string message, DateTimeOffset minimum, DateTimeOffset maximum)
+    public static IGuard InRange(this IGuard guard, DateTimeOffset value, string paramName, string message, DateTimeOffset minimum, DateTimeOffset maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.InRange(value, minimum, maximum);
 
-        if (value.InRange(minimum, maximum)) return value;
+        if (value.InRange(minimum, maximum)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
