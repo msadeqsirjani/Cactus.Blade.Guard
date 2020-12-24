cda7f0987e8a1e74744bc01c2841786658b2e24f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:33:39 +0330

    Update Boolean.False.cs

--- Guard.Sample/System.Boolean/Boolean.False.cs
+++ Guard.Sample/System.Boolean/Boolean.False.cs
@@ -1,10 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Cactus.Blade.Guard;
+using System;
 
-namespace Guard.Sample.System.Boolean
+namespace Sample.System.Boolean
 {
-    class Boolean
+    public class FalseExecutable : IExecutable
     {
+        public void Execute()
+        {
+            const bool @false = false;
+            const bool @true = true;
+            const string paramName = "Value";
+            const string message = "Value must be false";
+
+
+            False(@false);
+            False(@true);
+            False(@false, paramName);
+            False(@true, paramName);
+            False(@false, null, message);
+            False(@true, null, message);
+            False(@false, paramName, message);
+            False(@true, paramName, message);
+        }
+
+        private static void False(bool input)
+        {
+            try
+            {
+                Guard.MustBe.False(input);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private static void False(bool input, string paramName)
+        {
+            try
+            {
+                Guard.MustBe.False(input, paramName);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        private static void False(bool input, string paramName, string message)
+        {
+            try
+            {
+                Guard.MustBe.False(input, paramName, message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
     }
 }
