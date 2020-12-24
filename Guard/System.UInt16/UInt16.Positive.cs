e31ac649a7efb4ce217595580254da699f852288
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:33:41 +0330

    Create UInt16.Positive.cs

---
+++ Guard/System.UInt16/UInt16.Positive.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static ushort Positive(this IGuard _, ushort input)
+    {
+        return Guard.MustBe.Positive(input, null, null);
+    }
+
+    public static ushort Positive(this IGuard _, ushort input, string paramName)
+    {
+        return Guard.MustBe.Positive(input, paramName, null);
+    }
+
+    public static ushort Positive(this IGuard _, ushort input, string paramName, string message)
+    {
+        return Guard.MustBe.Positive<ushort>(input, paramName, message);
+    }
+}
