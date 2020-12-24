c135e1465cfd514d6ba7c24fa3a9f1908054245b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:33:57 +0330

    Create T.Default.cs

---
+++ Guard/System.T/T.Default.cs
@@ -0,0 +1,28 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Collections.Generic;
+
+public static partial class GuardExtension
+{
+    public static T Default<T>(this IGuard _, T input)
+    {
+        return Guard.MustBe.Default<T>(input, null, null);
+    }
+
+    public static T Default<T>(this IGuard _, T input, string paramName)
+    {
+        return Guard.MustBe.Default<T>(input, paramName, null);
+    }
+
+    public static T Default<T>(this IGuard _, T input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= Message.Default(paramName);
+
+        if (EqualityComparer<T>.Default.Equals(input, default!) || input is null)
+            throw new ArgumentException(message, paramName);
+
+        return input;
+    }
+}
