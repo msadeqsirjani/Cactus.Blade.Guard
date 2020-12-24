00da600291cc9fec51b16a970185511627cdb587
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:34:03 +0330

    Create T.NegativeOrZero.cs

---
+++ Guard/System.T/T.NegativeOrZero.cs
@@ -0,0 +1,27 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    private static T NegativeOrZero<T>(this IGuard _, T input) where T : struct, IComparable
+    {
+        return Guard.MustBe.NegativeOrZero(input, null, null);
+    }
+
+    private static T NegativeOrZero<T>(this IGuard _, T input, string paramName) where T : struct, IComparable
+    {
+        return Guard.MustBe.NegativeOrZero(input, paramName, null);
+    }
+
+    private static T NegativeOrZero<T>(this IGuard _, T input, string paramName, string message) where T : struct, IComparable
+    {
+        paramName ??= nameof(input);
+        message ??= Message.NegativeOrZero(paramName);
+
+        if (input.CompareTo(default(T)) <= 0)
+            throw new ArgumentException(message, paramName);
+
+        return input;
+    }
+}
