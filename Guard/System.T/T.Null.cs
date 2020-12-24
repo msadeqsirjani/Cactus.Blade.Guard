6b1d3f084d4deea0f1a7b0d741d2f5cc715941d7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:34:13 +0330

    Create T.Null.cs

---
+++ Guard/System.T/T.Null.cs
@@ -0,0 +1,27 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static T Null<T>(this IGuard _, T input)
+    {
+        return Guard.MustBe.Null<T>(input, null, null);
+    }
+
+    public static T Null<T>(this IGuard _, T input, string parameter)
+    {
+        return Guard.MustBe.Null(input, parameter, null);
+    }
+
+    public static T Null<T>(this IGuard _, T input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= Message.Null(paramName);
+
+        if (input is null)
+            throw new ArgumentNullException(paramName, message);
+
+        return input;
+    }
+}
