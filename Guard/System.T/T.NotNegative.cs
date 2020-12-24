cc028dc27a784edcc582601f7017f6ade5f5ba47
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:02:11 +0330

    Update T.NotNegative.cs

--- Guard/System.T/T.NotNegative.cs
+++ Guard/System.T/T.NotNegative.cs
@@ -4,19 +4,19 @@ using System;
 
 public static partial class GuardExtension
 {
-    private static T NotNegative<T>(this IGuard _, T input)
+    private static IGuard NotNegative<T>(this IGuard _, T input)
         where T : struct, IComparable
     {
         return Guard.MustBe.NotNegative(input, null, null);
     }
 
-    private static T NotNegative<T>(this IGuard _, T input, string paramName)
+    private static IGuard NotNegative<T>(this IGuard _, T input, string paramName)
         where T : struct, IComparable
     {
         return Guard.MustBe.NotNegative(input, paramName, null);
     }
 
-    private static T NotNegative<T>(this IGuard _, T input, string paramName, string message)
+    private static IGuard NotNegative<T>(this IGuard guard, T input, string paramName, string message)
         where T : struct, IComparable
     {
         paramName ??= nameof(input);
@@ -25,6 +25,6 @@ public static partial class GuardExtension
         if (input.CompareTo(default(T)) >= 0)
             throw new ArgumentException(message, paramName);
 
-        return input;
+        return guard;
     }
 }
