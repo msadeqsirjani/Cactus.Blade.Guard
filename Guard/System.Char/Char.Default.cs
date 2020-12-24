11d64cbf1bb3e7ff1f5e223ba3152b6f3f380c48
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:03:44 +0330

    Update Char.Default.cs

--- Guard/System.Char/Char.Default.cs
+++ Guard/System.Char/Char.Default.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static char Default(this IGuard _, char input)
+    public static IGuard Default(this IGuard _, char input)
     {
         return Guard.MustBe.Default(input, null, null);
     }
 
-    public static char Default(this IGuard _, char input, string paramName)
+    public static IGuard Default(this IGuard _, char input, string paramName)
     {
         return Guard.MustBe.Default(input, paramName, null);
     }
 
-    public static char Default(this IGuard _, char input, string paramName, string message)
+    public static IGuard Default(this IGuard _, char input, string paramName, string message)
     {
         return Guard.MustBe.Default<char>(input, paramName, message);
     }
