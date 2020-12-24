79104feffaf78d4cb56c2635c05b26558562ec13
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:07:47 +0330

    Update DateTimeOffset.NotIn.cs

--- Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.NotIn.cs
+++ Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.NotIn.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static DateTimeOffset NotIn(this IGuard _, DateTimeOffset value, params DateTimeOffset[] values)
+    public static IGuard NotIn(this IGuard _, DateTimeOffset value, params DateTimeOffset[] values)
     {
         return Guard.MustBe.NotIn(value, null, null, values);
     }
 
-    public static DateTimeOffset NotIn(this IGuard _, DateTimeOffset value, string paramName, params DateTimeOffset[] values)
+    public static IGuard NotIn(this IGuard _, DateTimeOffset value, string paramName, params DateTimeOffset[] values)
     {
         return Guard.MustBe.NotIn(value, paramName, null, values);
     }
 
-    public static DateTimeOffset NotIn(this IGuard _, DateTimeOffset value, string paramName, string message, params DateTimeOffset[] values)
+    public static IGuard NotIn(this IGuard guard, DateTimeOffset value, string paramName, string message, params DateTimeOffset[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.NotIn(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.NotIn(values)) return value;
+        if (value.NotIn(values)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
