876b10b61587c0ebaa70b8d4f4b2f8a2fb226257
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:53:30 +0330

    Create UInt64.NotDefault.cs

---
+++ Guard/System.UInt64/UInt64.NotDefault.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static ulong NotDefault(this IGuard _, ulong input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static ulong NotDefault(this IGuard _, ulong input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static ulong NotDefault(this IGuard _, ulong input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<ulong>(input, paramName, message);
+    }
+}
