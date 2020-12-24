20bbe5c652866e29e9e4ab0c2d5ce7d441eed706
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:52:57 +0330

    Create Char.NotDefault.cs

---
+++ Guard/System.Char/Char.NotDefault.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static char NotDefault(this IGuard _, char input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static char NotDefault(this IGuard _, char input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static char NotDefault(this IGuard _, char input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<char>(input, paramName, message);
+    }
+}
