757014123a98d9378c78d9cc39b362e5eb53acea
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:45:45 +0330

    Update Byte.Between.cs

--- Guard/System.Byte/CodeObject/Byte.Between.cs
+++ Guard/System.Byte/CodeObject/Byte.Between.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static byte Between(this IGuard _, byte value, byte minimum, byte maximum)
+    public static IGuard Between(this IGuard _, byte value, byte minimum, byte maximum)
     {
         return Guard.MustBe.Between(value, null, null, minimum, maximum);
     }
 
-    public static byte Between(this IGuard _, byte value, string paramName, byte minimum, byte maximum)
+    public static IGuard Between(this IGuard _, byte value, string paramName, byte minimum, byte maximum)
     {
         return Guard.MustBe.Between(value, paramName, null, minimum, maximum);
     }
 
-    public static byte Between(this IGuard _, byte value, string paramName, string message, byte minimum, byte maximum)
+    public static IGuard Between(this IGuard guard, byte value, string paramName, string message, byte minimum, byte maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.Between(value, minimum, maximum);
 
-        if (value.Between(minimum, maximum)) return value;
+        if (value.Between(minimum, maximum)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
