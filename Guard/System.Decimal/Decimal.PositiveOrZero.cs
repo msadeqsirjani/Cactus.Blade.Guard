6cd04f46c9af709f32cf0e34d6883ec08f547229
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:34:31 +0330

    Create Decimal.PositiveOrZero.cs

---
+++ Guard/System.Decimal/Decimal.PositiveOrZero.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static decimal PositiveOrZero(this IGuard _, decimal input)
+    {
+        return Guard.MustBe.PositiveOrZero(input, null, null);
+    }
+
+    public static decimal PositiveOrZero(this IGuard _, decimal input, string paramName)
+    {
+        return Guard.MustBe.PositiveOrZero(input, paramName, null);
+    }
+
+    public static decimal PositiveOrZero(this IGuard _, decimal input, string paramName, string message)
+    {
+        return Guard.MustBe.PositiveOrZero<decimal>(input, paramName, message);
+    }
+}
