88f2ad9c7064b6d06cf3c0f8315b279ed9414a8f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:05:38 +0330

    Update DateTime.NotInRange.cs

--- Guard/System.DateTime/CodeObject/DateTime.NotInRange.cs
+++ Guard/System.DateTime/CodeObject/DateTime.NotInRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static DateTime NotInRange(this IGuard _, DateTime value, DateTime minimum, DateTime maximum)
+    public static IGuard NotInRange(this IGuard _, DateTime value, DateTime minimum, DateTime maximum)
     {
         return Guard.MustBe.NotInRange(value, null, null, minimum, maximum);
     }
 
-    public static DateTime NotInRange(this IGuard _, DateTime value, string paramName, DateTime minimum, DateTime maximum)
+    public static IGuard NotInRange(this IGuard _, DateTime value, string paramName, DateTime minimum, DateTime maximum)
     {
         return Guard.MustBe.NotInRange(value, paramName, null, minimum, maximum);
     }
 
-    public static DateTime NotInRange(this IGuard _, DateTime value, string paramName, string message, DateTime minimum, DateTime maximum)
+    public static IGuard NotInRange(this IGuard guard, DateTime value, string paramName, string message, DateTime minimum, DateTime maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.NotInRange(value, minimum, maximum);
 
-        if (value.NotInRange(minimum, maximum)) return value;
+        if (value.NotInRange(minimum, maximum)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
