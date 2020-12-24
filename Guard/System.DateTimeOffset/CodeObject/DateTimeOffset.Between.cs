7fcda99c7c1be9b0dac01f7b173a0e3a9bcf4a0d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:07:40 +0330

    Update DateTimeOffset.Between.cs

--- Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.Between.cs
+++ Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.Between.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static DateTimeOffset Between(this IGuard _, DateTimeOffset value, DateTimeOffset minimum, DateTimeOffset maximum)
+    public static IGuard Between(this IGuard _, DateTimeOffset value, DateTimeOffset minimum, DateTimeOffset maximum)
     {
         return Guard.MustBe.Between(value, null, null, minimum, maximum);
     }
 
-    public static DateTimeOffset Between(this IGuard _, DateTimeOffset value, string paramName, DateTimeOffset minimum, DateTimeOffset maximum)
+    public static IGuard Between(this IGuard _, DateTimeOffset value, string paramName, DateTimeOffset minimum, DateTimeOffset maximum)
     {
         return Guard.MustBe.Between(value, paramName, null, minimum, maximum);
     }
 
-    public static DateTimeOffset Between(this IGuard _, DateTimeOffset value, string paramName, string message, DateTimeOffset minimum, DateTimeOffset maximum)
+    public static IGuard Between(this IGuard guard, DateTimeOffset value, string paramName, string message, DateTimeOffset minimum, DateTimeOffset maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.Between(value, minimum, maximum);
 
-        if (value.Between(minimum, maximum)) return value;
+        if (value.Between(minimum, maximum)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
