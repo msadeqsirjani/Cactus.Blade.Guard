3a5cc5cdada552ce39c57e7dc7cc4d62dec087c8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:53:08 +0330

    Create Int16.NotDefault.cs

---
+++ Guard/System.Int16/Int16.NotDefault.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static short NotDefault(this IGuard _, short input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static short NotDefault(this IGuard _, short input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static short NotDefault(this IGuard _, short input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<short>(input, paramName, message);
+    }
+}
