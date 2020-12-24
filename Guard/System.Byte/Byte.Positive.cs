9accc2b55385741198603ab11489ac5882440dea
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:02:02 +0330

    Update Byte.Positive.cs

--- Guard/System.Byte/Byte.Positive.cs
+++ Guard/System.Byte/Byte.Positive.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static byte Positive(this IGuard _, byte input)
+    public static IGuard Positive(this IGuard _, byte input)
     {
         return Guard.MustBe.Positive(input, null, null);
     }
 
-    public static byte Positive(this IGuard _, byte input, string paramName)
+    public static IGuard Positive(this IGuard _, byte input, string paramName)
     {
         return Guard.MustBe.Positive(input, paramName, null);
     }
 
-    public static byte Positive(this IGuard _, byte input, string paramName, string message)
+    public static IGuard Positive(this IGuard _, byte input, string paramName, string message)
     {
         return Guard.MustBe.Positive<byte>(input, paramName, message);
     }
