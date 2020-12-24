55cc8b2c612e16b4c2a4320e10a21c29806d9154
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:34:05 +0330

    Create T.NotDefault.cs

---
+++ Guard/System.T/T.NotDefault.cs
@@ -0,0 +1,28 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Collections.Generic;
+
+public static partial class GuardExtension
+{
+    public static T NotDefault<T>(this IGuard _, T input)
+    {
+        return Guard.MustBe.NotDefault<T>(input, null, null);
+    }
+
+    public static T NotDefault<T>(this IGuard _, T input, string paramName)
+    {
+        return Guard.MustBe.NotDefault<T>(input, paramName, null);
+    }
+
+    public static T NotDefault<T>(this IGuard _, T input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= Message.NotDefault(paramName);
+
+        if (!(EqualityComparer<T>.Default.Equals(input, default!) || input is null))
+            throw new ArgumentException(message, paramName);
+
+        return input;
+    }
+}
