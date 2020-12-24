ec6357876edd84cb4c53564210054fd2ffa894d0
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:35:06 +0330

    Create UInt32.PositiveOrZero.cs

---
+++ Guard/System.UInt32/UInt32.PositiveOrZero.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static uint PositiveOrZero(this IGuard _, uint input)
+    {
+        return Guard.MustBe.PositiveOrZero(input, null, null);
+    }
+
+    public static uint PositiveOrZero(this IGuard _, uint input, string paramName)
+    {
+        return Guard.MustBe.PositiveOrZero(input, paramName, null);
+    }
+
+    public static uint PositiveOrZero(this IGuard _, uint input, string paramName, string message)
+    {
+        return Guard.MustBe.PositiveOrZero<uint>(input, paramName, message);
+    }
+}
