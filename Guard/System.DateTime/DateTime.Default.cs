e442501dc8b48b549deca112fc7d525b937596ae
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:05:42 +0330

    Update DateTime.Default.cs

--- Guard/System.DateTime/DateTime.Default.cs
+++ Guard/System.DateTime/DateTime.Default.cs
@@ -3,17 +3,17 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static DateTime Default(this IGuard _, DateTime input)
+    public static IGuard Default(this IGuard _, DateTime input)
     {
         return Guard.MustBe.Default(input, null, null);
     }
 
-    public static DateTime Default(this IGuard _, DateTime input, string paramName)
+    public static IGuard Default(this IGuard _, DateTime input, string paramName)
     {
         return Guard.MustBe.Default(input, paramName, null);
     }
 
-    public static DateTime Default(this IGuard _, DateTime input, string paramName, string message)
+    public static IGuard Default(this IGuard _, DateTime input, string paramName, string message)
     {
         return Guard.MustBe.Default<DateTime>(input, paramName, message);
     }
