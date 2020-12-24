7aec822965aafa60728e43f418285940922f283b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:53:27 +0330

    Create UInt32.NotDefault.cs

---
+++ Guard/System.UInt32/UInt32.NotDefault.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static uint NotDefault(this IGuard _, uint input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static uint NotDefault(this IGuard _, uint input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static uint NotDefault(this IGuard _, uint input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<uint>(input, paramName, message);
+    }
+}
