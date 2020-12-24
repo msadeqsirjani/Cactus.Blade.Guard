4e8d906cf3c3747352f5d9d886cccf5d72b4060a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:53:13 +0330

    Create Int64.NotDefault.cs

---
+++ Guard/System.Int64/Int64.NotDefault.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static long NotDefault(this IGuard _, long input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static long NotDefault(this IGuard _, long input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static long NotDefault(this IGuard _, long input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<long>(input, paramName, message);
+    }
+}
