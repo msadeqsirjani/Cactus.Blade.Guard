4438a48170303757358943f5ca26b61c4e008667
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:01:34 +0330

    Update Byte.Default.cs

--- Guard/System.Byte/Byte.Default.cs
+++ Guard/System.Byte/Byte.Default.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static byte Default(this IGuard _, byte input)
+    public static IGuard Default(this IGuard _, byte input)
     {
         return Guard.MustBe.Default(input, null, null);
     }
 
-    public static byte Default(this IGuard _, byte input, string paramName)
+    public static IGuard Default(this IGuard _, byte input, string paramName)
     {
         return Guard.MustBe.Default(input, paramName, null);
     }
 
-    public static byte Default(this IGuard _, byte input, string paramName, string message)
+    public static IGuard Default(this IGuard _, byte input, string paramName, string message)
     {
         return Guard.MustBe.Default<byte>(input, paramName, message);
     }
