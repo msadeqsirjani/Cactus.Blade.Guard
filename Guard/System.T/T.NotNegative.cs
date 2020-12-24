494ac86bd937ea4d4a4efaac21dfe541b63b3d62
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:34:07 +0330

    Create T.NotNegative.cs

---
+++ Guard/System.T/T.NotNegative.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    private static T NotNegative<T>(this IGuard _, T input)
+        where T : struct, IComparable
+    {
+        return Guard.MustBe.NotNegative(input, null, null);
+    }
+
+    private static T NotNegative<T>(this IGuard _, T input, string paramName)
+        where T : struct, IComparable
+    {
+        return Guard.MustBe.NotNegative(input, paramName, null);
+    }
+
+    private static T NotNegative<T>(this IGuard _, T input, string paramName, string message)
+        where T : struct, IComparable
+    {
+        paramName ??= nameof(input);
+        message ??= Message.NotNegative(paramName);
+
+        if (input.CompareTo(default(T)) >= 0)
+            throw new ArgumentException(message, paramName);
+
+        return input;
+    }
+}
