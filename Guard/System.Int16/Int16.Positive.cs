af43da962652ad99d0b187664db755fe4ac075fc
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:34:40 +0330

    Create Int16.Positive.cs

---
+++ Guard/System.Int16/Int16.Positive.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static double Positive(this IGuard _, double input)
+    {
+        return Guard.MustBe.Positive(input, null, null);
+    }
+
+    public static double Positive(this IGuard _, double input, string paramName)
+    {
+        return Guard.MustBe.Positive(input, paramName, null);
+    }
+
+    public static double Positive(this IGuard _, double input, string paramName, string message)
+    {
+        return Guard.MustBe.Positive<double>(input, paramName, message);
+    }
+}
