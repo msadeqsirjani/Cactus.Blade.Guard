9ab127067947b6f1ca605358e5e34719e8bd2b44
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 12:33:49 +0330

    Create Executable.cs

---
+++ Guard.Sample/Executable.cs
@@ -0,0 +1,23 @@
+﻿using Sample.System.Boolean;
+
+namespace Sample
+{
+    public class Executable : IExecutable
+    {
+        public void Execute()
+        {
+            Boolean();
+        }
+
+        private static void Boolean()
+        {
+            IExecutable boolean = new FalseExecutable();
+
+            boolean.Execute();
+
+            boolean = new TrueExecutable();
+
+            boolean.Execute();
+        }
+    }
+}
