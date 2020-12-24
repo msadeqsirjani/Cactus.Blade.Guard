d6530d495e6d2461a293fad3766cc8d11368ca10
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:03:47 +0330

    Update Char.In.cs

--- Guard/System.Char/Char.In.cs
+++ Guard/System.Char/Char.In.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static char NotIn(this IGuard _, char value, params char[] values)
+    public static IGuard NotIn(this IGuard _, char value, params char[] values)
     {
         return Guard.MustBe.NotIn(value, null, null, values);
     }
 
-    public static char NotIn(this IGuard _, char value, string paramName, params char[] values)
+    public static IGuard NotIn(this IGuard _, char value, string paramName, params char[] values)
     {
         return Guard.MustBe.NotIn(value, paramName, null, values);
     }
 
-    public static char NotIn(this IGuard _, char value, string paramName, string message, params char[] values)
+    public static IGuard NotIn(this IGuard guard, char value, string paramName, string message, params char[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.NotIn(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.NotIn(values)) return value;
+        if (value.NotIn(values)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
