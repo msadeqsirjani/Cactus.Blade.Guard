eb8566c5ad28e6dfdd2f00d5d553013052c8645e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:53:20 +0330

    Create String.NotDefault.cs

---
+++ Guard/System.String/String.NotDefault.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static string NotDefault(this IGuard _, string input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static string NotDefault(this IGuard _, string input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static string NotDefault(this IGuard _, string input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<string>(input, paramName, message);
+    }
+}
