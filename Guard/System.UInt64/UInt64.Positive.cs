ea556430617aa5cd854b318099ac866812508476
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:35:08 +0330

    Create UInt64.Positive.cs

---
+++ Guard/System.UInt64/UInt64.Positive.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static ulong Positive(this IGuard _, ulong input)
+    {
+        return Guard.MustBe.Positive(input, null, null);
+    }
+
+    public static ulong Positive(this IGuard _, ulong input, string paramName)
+    {
+        return Guard.MustBe.Positive(input, paramName, null);
+    }
+
+    public static ulong Positive(this IGuard _, ulong input, string paramName, string message)
+    {
+        return Guard.MustBe.Positive<ulong>(input, paramName, message);
+    }
+}
