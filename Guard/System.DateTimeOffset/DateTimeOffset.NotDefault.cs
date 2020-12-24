76592c7820963522ab0cd78b916572c7f2f1cf1b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:07:52 +0330

    Update DateTimeOffset.NotDefault.cs

--- Guard/System.DateTimeOffset/DateTimeOffset.NotDefault.cs
+++ Guard/System.DateTimeOffset/DateTimeOffset.NotDefault.cs
@@ -3,17 +3,17 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static DateTimeOffset NotDefault(this IGuard _, DateTimeOffset input)
+    public static IGuard NotDefault(this IGuard _, DateTimeOffset input)
     {
         return Guard.MustBe.NotDefault(input, null, null);
     }
 
-    public static DateTimeOffset NotDefault(this IGuard _, DateTimeOffset input, string paramName)
+    public static IGuard NotDefault(this IGuard _, DateTimeOffset input, string paramName)
     {
         return Guard.MustBe.NotDefault(input, paramName, null);
     }
 
-    public static DateTimeOffset NotDefault(this IGuard _, DateTimeOffset input, string paramName, string message)
+    public static IGuard NotDefault(this IGuard _, DateTimeOffset input, string paramName, string message)
     {
         return Guard.MustBe.NotDefault<DateTimeOffset>(input, paramName, message);
     }
