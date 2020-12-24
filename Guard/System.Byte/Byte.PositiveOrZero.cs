1b439cfa2934632db0e85246259d6cf127f078aa
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:34:25 +0330

    Create Byte.PositiveOrZero.cs

---
+++ Guard/System.Byte/Byte.PositiveOrZero.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static byte PositiveOrZero(this IGuard _, byte input)
+    {
+        return Guard.MustBe.PositiveOrZero(input, null, null);
+    }
+
+    public static byte PositiveOrZero(this IGuard _, byte input, string paramName)
+    {
+        return Guard.MustBe.PositiveOrZero(input, paramName, null);
+    }
+
+    public static byte PositiveOrZero(this IGuard _, byte input, string paramName, string message)
+    {
+        return Guard.MustBe.PositiveOrZero<byte>(input, paramName, message);
+    }
+}
