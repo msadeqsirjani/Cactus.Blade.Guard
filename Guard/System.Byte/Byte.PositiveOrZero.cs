6588d7e9676b6718c70e5a2204c0e19d3b7b27d3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:02:04 +0330

    Update Byte.PositiveOrZero.cs

--- Guard/System.Byte/Byte.PositiveOrZero.cs
+++ Guard/System.Byte/Byte.PositiveOrZero.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static byte PositiveOrZero(this IGuard _, byte input)
+    public static IGuard PositiveOrZero(this IGuard _, byte input)
     {
         return Guard.MustBe.PositiveOrZero(input, null, null);
     }
 
-    public static byte PositiveOrZero(this IGuard _, byte input, string paramName)
+    public static IGuard PositiveOrZero(this IGuard _, byte input, string paramName)
     {
         return Guard.MustBe.PositiveOrZero(input, paramName, null);
     }
 
-    public static byte PositiveOrZero(this IGuard _, byte input, string paramName, string message)
+    public static IGuard PositiveOrZero(this IGuard _, byte input, string paramName, string message)
     {
         return Guard.MustBe.PositiveOrZero<byte>(input, paramName, message);
     }
