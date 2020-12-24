15ea242b54d27b1a520f85ec230247b17a2aa965
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:53:03 +0330

    Create Decimal.NotDefault.cs

---
+++ Guard/System.Decimal/Decimal.NotDefault.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static decimal NotDefault(this IGuard _, decimal input)
+    {
+        return Guard.MustBe.NotDefault(input, null, null);
+    }
+
+    public static decimal NotDefault(this IGuard _, decimal input, string paramName)
+    {
+        return Guard.MustBe.NotDefault(input, paramName, null);
+    }
+
+    public static decimal NotDefault(this IGuard _, decimal input, string paramName, string message)
+    {
+        return Guard.MustBe.NotDefault<decimal>(input, paramName, message);
+    }
+}
