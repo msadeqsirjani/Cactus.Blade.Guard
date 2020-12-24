efc471e44145f68003e657bbb161802f59162bdd
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 11:34:27 +0330

    Create Decimal.Positive.cs

---
+++ Guard/System.Decimal/Decimal.Positive.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static decimal Positive(this IGuard _, decimal input)
+    {
+        return Guard.MustBe.Positive(input, null, null);
+    }
+
+    public static decimal Positive(this IGuard _, decimal input, string paramName)
+    {
+        return Guard.MustBe.Positive(input, paramName, null);
+    }
+
+    public static decimal Positive(this IGuard _, decimal input, string paramName, string message)
+    {
+        return Guard.MustBe.Positive<decimal>(input, paramName, message);
+    }
+}
