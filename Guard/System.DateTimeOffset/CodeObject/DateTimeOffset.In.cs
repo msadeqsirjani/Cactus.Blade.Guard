b0c709a3cf6246734da3311f169bb4cb6f432280
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-24 13:07:43 +0330

    Update DateTimeOffset.In.cs

--- Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.In.cs
+++ Guard/System.DateTimeOffset/CodeObject/DateTimeOffset.In.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static DateTimeOffset In(this IGuard _, DateTimeOffset value, params DateTimeOffset[] values)
+    public static IGuard In(this IGuard _, DateTimeOffset value, params DateTimeOffset[] values)
     {
         return Guard.MustBe.In(value, null, null, values);
     }
 
-    public static DateTimeOffset In(this IGuard _, DateTimeOffset value, string paramName, params DateTimeOffset[] values)
+    public static IGuard In(this IGuard _, DateTimeOffset value, string paramName, params DateTimeOffset[] values)
     {
         return Guard.MustBe.In(value, paramName, null, values);
     }
 
-    public static DateTimeOffset In(this IGuard _, DateTimeOffset value, string paramName, string message, params DateTimeOffset[] values)
+    public static IGuard In(this IGuard guard, DateTimeOffset value, string paramName, string message, params DateTimeOffset[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.In(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.In(values)) return value;
+        if (value.In(values)) return guard;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
