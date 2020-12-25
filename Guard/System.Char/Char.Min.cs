462081492cb943aaf9f8225a31a6ce875e3ff977
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:55:16 +0330

    Create Char.Min.cs

---
+++ Guard/System.Char/Char.Min.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Min(char input, char minimum)
+    {
+        return Guard.MustBe.Min(input, minimum);
+    }
+
+    public static IGuard Min(char input, char minimum, string paramName)
+    {
+        return Guard.MustBe.Min(input, minimum, paramName);
+    }
+
+    public static IGuard Min(char input, char minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Min(input, minimum, paramName, message);
+    }
+}
