5e20da1ea4564bfab3099e4b762f411ae12ca830
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:34:27 +0330

    Update Single.NotDefault.cs

--- Guard/System.Single/Single.NotDefault.cs
+++ Guard/System.Single/Single.NotDefault.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotDefault(this IGuard _, float input)
     {
-        return Guard.MustBe.NotDefault(input, null, null);
+        return Guard.MustBe.NotDefault<float>(input);
     }
 
     public static IGuard NotDefault(this IGuard _, float input, string paramName)
     {
-        return Guard.MustBe.NotDefault(input, paramName, null);
+        return Guard.MustBe.NotDefault<float>(input, paramName);
     }
 
     public static IGuard NotDefault(this IGuard _, float input, string paramName, string message)
