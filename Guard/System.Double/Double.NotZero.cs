2db734b18be2370b485136eccc2e6f3e86cdd23f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:02 +0330

    Update Double.NotZero.cs

--- Guard/System.Double/Double.NotZero.cs
+++ Guard/System.Double/Double.NotZero.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static double NotZero(this IGuard _, double input)
+    public static IGuard NotZero(this IGuard _, double input)
     {
         return Guard.MustBe.NotZero(input, null, null);
     }
 
-    public static double NotZero(this IGuard _, double input, string paramName)
+    public static IGuard NotZero(this IGuard _, double input, string paramName)
     {
         return Guard.MustBe.NotZero(input, paramName, null);
     }
 
 
-    public static double NotZero(this IGuard _, double input, string paramName, string message)
+    public static IGuard NotZero(this IGuard _, double input, string paramName, string message)
     {
         return Guard.MustBe.NotZero<double>(input, paramName, message);
     }
