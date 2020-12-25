635954839b9ba576b08d737775f603dec14b9845
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:59 +0330

    Update Single.Between.cs

--- Guard/System.Single/CodeObject/Single.Between.cs
+++ Guard/System.Single/CodeObject/Single.Between.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static float Between(this IGuard _, float value, float minimum, float maximum)
+    public static IGuard Between(this IGuard _, float value, float minimum, float maximum)
     {
         return Guard.MustBe.Between(value, null, null, minimum, maximum);
     }
 
-    public static float Between(this IGuard _, float value, string paramName, float minimum, float maximum)
+    public static IGuard Between(this IGuard _, float value, string paramName, float minimum, float maximum)
     {
         return Guard.MustBe.Between(value, paramName, null, minimum, maximum);
     }
 
-    public static float Between(this IGuard _, float value, string paramName, string message, float minimum, float maximum)
+    public static IGuard Between(this IGuard _, float value, string paramName, string message, float minimum, float maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.Between(value, minimum, maximum);
 
-        if (value.Between(minimum, maximum)) return value;
+        if (value.Between(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
