5d56e8adcc22ce4afc36f56890d6d9a9fc0afd7b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 13:01:24 +0330

    Create TimeSpan.Max.cs

---
+++ Guard/System.TimeSpan/TimeSpan.Max.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    public static IGuard Max(TimeSpan input, TimeSpan minimum)
+    {
+        return Guard.MustBe.Max(input, minimum);
+    }
+
+    public static IGuard Max(TimeSpan input, TimeSpan minimum, string paramName)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName);
+    }
+
+    public static IGuard Max(TimeSpan input, TimeSpan minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName, message);
+    }
+}
