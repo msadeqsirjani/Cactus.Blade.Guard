79dfc3ea41c456a7196f75dda808dfd7013f247f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:48:13 +0330

    Update UInt32.NotDefault.cs

--- Guard/System.UInt32/UInt32.NotDefault.cs
+++ Guard/System.UInt32/UInt32.NotDefault.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotDefault(this IGuard _, uint input)
     {
-        return Guard.MustBe.NotDefault(input, null, null);
+        return Guard.MustBe.NotDefault<uint>(input);
     }
 
     public static IGuard NotDefault(this IGuard _, uint input, string paramName)
     {
-        return Guard.MustBe.NotDefault(input, paramName, null);
+        return Guard.MustBe.NotDefault<uint>(input, paramName);
     }
 
     public static IGuard NotDefault(this IGuard _, uint input, string paramName, string message)
