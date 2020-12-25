d19eb5858fbf6a61ec9a02c87dc06001243070b7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:59 +0330

    Update Int16.NotIn.cs

--- Guard/System.Int16/CodeObject/Int16.NotIn.cs
+++ Guard/System.Int16/CodeObject/Int16.NotIn.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static short NotIn(this IGuard _, short value, params short[] values)
+    public static IGuard NotIn(this IGuard _, short value, params short[] values)
     {
         return Guard.MustBe.NotIn(value, null, null, values);
     }
 
-    public static short NotIn(this IGuard _, short value, string paramName, params short[] values)
+    public static IGuard NotIn(this IGuard _, short value, string paramName, params short[] values)
     {
         return Guard.MustBe.NotIn(value, paramName, null, values);
     }
 
-    public static short NotIn(this IGuard _, short value, string paramName, string message, params short[] values)
+    public static IGuard NotIn(this IGuard _, short value, string paramName, string message, params short[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.NotIn(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.NotIn(values)) return value;
+        if (value.NotIn(values)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
