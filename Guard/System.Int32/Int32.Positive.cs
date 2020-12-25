4367cb33578364aab407d729108e5bfb15a65214
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:52 +0330

    Update Int32.Positive.cs

--- Guard/System.Int32/Int32.Positive.cs
+++ Guard/System.Int32/Int32.Positive.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static int Positive(this IGuard _, int input)
+    public static IGuard Positive(this IGuard _, int input)
     {
         return Guard.MustBe.Positive(input, null, null);
     }
 
-    public static int Positive(this IGuard _, int input, string paramName)
+    public static IGuard Positive(this IGuard _, int input, string paramName)
     {
         return Guard.MustBe.Positive(input, paramName, null);
     }
 
-    public static int Positive(this IGuard _, int input, string paramName, string message)
+    public static IGuard Positive(this IGuard _, int input, string paramName, string message)
     {
         return Guard.MustBe.Positive<int>(input, paramName, message);
     }
