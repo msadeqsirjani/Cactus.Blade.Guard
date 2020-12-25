29e0c27682685bd1e11d240dd86a16d53df7ca56
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 13:01:06 +0330

    Create Double.Max.cs

---
+++ Guard/System.Double/Double.Max.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Min(double input, double minimum)
+    {
+        return Guard.MustBe.Min(input, minimum);
+    }
+
+    public static IGuard Min(double input, double minimum, string paramName)
+    {
+        return Guard.MustBe.Min(input, minimum, paramName);
+    }
+
+    public static IGuard Min(double input, double minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Min(input, minimum, paramName, message);
+    }
+}
