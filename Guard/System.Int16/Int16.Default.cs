bb9f010dd24de6c54dadf422dd59e73a759bb8c9
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:07:00 +0330

    Update Int16.Default.cs

--- Guard/System.Int16/Int16.Default.cs
+++ Guard/System.Int16/Int16.Default.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard Default(this IGuard _, short input)
     {
-        return Guard.MustBe.Default(input, null, null);
+        return Guard.MustBe.Default<short>(input);
     }
 
     public static IGuard Default(this IGuard _, short input, string paramName)
     {
-        return Guard.MustBe.Default(input, paramName, null);
+        return Guard.MustBe.Default<short>(input, paramName);
     }
 
     public static IGuard Default(this IGuard _, short input, string paramName, string message)
