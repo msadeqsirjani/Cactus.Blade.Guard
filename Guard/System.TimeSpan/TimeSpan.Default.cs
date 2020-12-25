08fb3b46f55036d9f3c223a9f1f352331f155bab
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:28:04 +0330

    Update TimeSpan.Default.cs

--- Guard/System.TimeSpan/TimeSpan.Default.cs
+++ Guard/System.TimeSpan/TimeSpan.Default.cs
@@ -3,17 +3,17 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static TimeSpan Default(this IGuard _, TimeSpan input)
+    public static IGuard Default(this IGuard _, TimeSpan input)
     {
         return Guard.MustBe.Default(input, null, null);
     }
 
-    public static TimeSpan Default(this IGuard _, TimeSpan input, string paramName)
+    public static IGuard Default(this IGuard _, TimeSpan input, string paramName)
     {
         return Guard.MustBe.Default(input, paramName, null);
     }
 
-    public static TimeSpan Default(this IGuard _, TimeSpan input, string paramName, string message)
+    public static IGuard Default(this IGuard _, TimeSpan input, string paramName, string message)
     {
         return Guard.MustBe.Default<TimeSpan>(input, paramName, message);
     }
