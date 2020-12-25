391bda66c4179f364c4d6167bc029d47c3d47fa7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:24:50 +0330

    Update Double.Default.cs

--- Guard/System.Double/Double.Default.cs
+++ Guard/System.Double/Double.Default.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static double Default(this IGuard _, double input)
+    public static IGuard Default(this IGuard _, double input)
     {
         return Guard.MustBe.Default(input, null, null);
     }
 
-    public static double Default(this IGuard _, double input, string paramName)
+    public static IGuard Default(this IGuard _, double input, string paramName)
     {
         return Guard.MustBe.Default(input, paramName, null);
     }
 
-    public static double Default(this IGuard _, double input, string paramName, string message)
+    public static IGuard Default(this IGuard _, double input, string paramName, string message)
     {
         return Guard.MustBe.Default<double>(input, paramName, message);
     }
