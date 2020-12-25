b211509044fd4761311b13a558962e38c0e7adb5
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:22:27 +0330

    Update Int64.NegativeOrZero.cs

--- Guard/System.Int64/Int64.NegativeOrZero.cs
+++ Guard/System.Int64/Int64.NegativeOrZero.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NegativeOrZero(this IGuard _, long input)
     {
-        return Guard.MustBe.NegativeOrZero(input, null, null);
+        return Guard.MustBe.NegativeOrZero<long>(input);
     }
 
     public static IGuard NegativeOrZero(this IGuard _, long input, string paramName)
     {
-        return Guard.MustBe.NegativeOrZero(input, paramName, null);
+        return Guard.MustBe.NegativeOrZero<long>(input, paramName);
     }
 
     public static IGuard NegativeOrZero(this IGuard _, long input, string paramName, string message)
