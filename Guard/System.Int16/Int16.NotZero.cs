0e7e711e85f66872e8b75e8fb40abd059e9ad6df
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:07:12 +0330

    Update Int16.NotZero.cs

--- Guard/System.Int16/Int16.NotZero.cs
+++ Guard/System.Int16/Int16.NotZero.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotZero(this IGuard _, short input)
     {
-        return Guard.MustBe.NotZero(input, null, null);
+        return Guard.MustBe.NotZero<double>(input);
     }
 
     public static IGuard NotZero(this IGuard _, short input, string paramName)
     {
-        return Guard.MustBe.NotZero(input, paramName, null);
+        return Guard.MustBe.NotZero<double>(input, paramName);
     }
 
 
