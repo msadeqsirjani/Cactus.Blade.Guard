e1913d72398f33ce9d4500934f2a00e4f8a8ac3d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:02 +0330

    Update Decimal.Positive.cs

--- Guard/System.Decimal/Decimal.Positive.cs
+++ Guard/System.Decimal/Decimal.Positive.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static decimal Positive(this IGuard _, decimal input)
+    public static IGuard Positive(this IGuard _, decimal input)
     {
         return Guard.MustBe.Positive(input, null, null);
     }
 
-    public static decimal Positive(this IGuard _, decimal input, string paramName)
+    public static IGuard Positive(this IGuard _, decimal input, string paramName)
     {
         return Guard.MustBe.Positive(input, paramName, null);
     }
 
-    public static decimal Positive(this IGuard _, decimal input, string paramName, string message)
+    public static IGuard Positive(this IGuard _, decimal input, string paramName, string message)
     {
         return Guard.MustBe.Positive<decimal>(input, paramName, message);
     }
