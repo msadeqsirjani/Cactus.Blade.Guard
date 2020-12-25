61aa5140a289537c73341eeb888b6721b1e74c7d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:24:55 +0330

    Update Double.NegativeOrZero.cs

--- Guard/System.Double/Double.NegativeOrZero.cs
+++ Guard/System.Double/Double.NegativeOrZero.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static double NegativeOrZero(this IGuard _, double input)
+    public static IGuard NegativeOrZero(this IGuard _, double input)
     {
         return Guard.MustBe.NegativeOrZero(input, null, null);
     }
 
-    public static double NegativeOrZero(this IGuard _, double input, string paramName)
+    public static IGuard NegativeOrZero(this IGuard _, double input, string paramName)
     {
         return Guard.MustBe.NegativeOrZero(input, paramName, null);
     }
 
-    public static double NegativeOrZero(this IGuard _, double input, string paramName, string message)
+    public static IGuard NegativeOrZero(this IGuard _, double input, string paramName, string message)
     {
         return Guard.MustBe.NegativeOrZero<double>(input, paramName, message);
     }
