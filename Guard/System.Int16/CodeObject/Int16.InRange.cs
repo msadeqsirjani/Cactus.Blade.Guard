c83837ad27ae716c1621e93f590bbf0eb76106f3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:56 +0330

    Update Int16.InRange.cs

--- Guard/System.Int16/CodeObject/Int16.InRange.cs
+++ Guard/System.Int16/CodeObject/Int16.InRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static short InRange(this IGuard _, short value, short minimum, short maximum)
+    public static IGuard InRange(this IGuard _, short value, short minimum, short maximum)
     {
         return Guard.MustBe.InRange(value, null, null, minimum, maximum);
     }
 
-    public static short InRange(this IGuard _, short value, string paramName, short minimum, short maximum)
+    public static IGuard InRange(this IGuard _, short value, string paramName, short minimum, short maximum)
     {
         return Guard.MustBe.InRange(value, paramName, null, minimum, maximum);
     }
 
-    public static short InRange(this IGuard _, short value, string paramName, string message, short minimum, short maximum)
+    public static IGuard InRange(this IGuard _, short value, string paramName, string message, short minimum, short maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.InRange(value, minimum, maximum);
 
-        if (value.InRange(minimum, maximum)) return value;
+        if (value.InRange(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
