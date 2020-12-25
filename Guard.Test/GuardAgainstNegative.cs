80a20239a575935712d850f01cdc5636e6c68be8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:21:15 +0330

    Update GuardAgainstNegative.cs

--- Guard.Test/GuardAgainstNegative.cs
+++ Guard.Test/GuardAgainstNegative.cs
@@ -4,91 +4,91 @@ using Xunit;
 
 namespace Guard.Test
 {
-    public class GuardAgainstNegative
+    public class GuardMustBeNegative
     {
         [Fact]
         public void DoesNothingGivenZeroValue()
         {
-            Cactus.Blade.Guard.Guard.Against.Negative(0, "intZero");
-            Cactus.Blade.Guard.Guard.Against.Negative(0L, "longZero");
-            Cactus.Blade.Guard.Guard.Against.Negative(0.0M, "decimalZero");
-            Cactus.Blade.Guard.Guard.Against.Negative(0.0f, "floatZero");
-            Cactus.Blade.Guard.Guard.Against.Negative(0.0, "doubleZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(0, "intZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(0L, "longZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(0.0M, "decimalZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(0.0f, "floatZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(0.0, "doubleZero");
         }
 
         [Fact]
         public void DoesNothingGivenPositiveValue()
         {
-            Cactus.Blade.Guard.Guard.Against.Negative(1, "intZero");
-            Cactus.Blade.Guard.Guard.Against.Negative(1L, "longZero");
-            Cactus.Blade.Guard.Guard.Against.Negative(1.0M, "decimalZero");
-            Cactus.Blade.Guard.Guard.Against.Negative(1.0f, "floatZero");
-            Cactus.Blade.Guard.Guard.Against.Negative(1.0, "doubleZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(1, "intZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(1L, "longZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(1.0M, "decimalZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(1.0f, "floatZero");
+            Cactus.Blade.Guard.Guard.MustBe.Negative(1.0, "doubleZero");
         }
 
         [Fact]
         public void ThrowsGivenNegativeIntValue()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Negative(-1, "negative"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Negative(-1, "negative"));
         }
 
         [Fact]
         public void ThrowsGivenNegativeLongValue()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Negative(-1L, "negative"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Negative(-1L, "negative"));
         }
 
         [Fact]
         public void ThrowsGivenNegativeDecimalValue()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Negative(-1.0M, "negative"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Negative(-1.0M, "negative"));
         }
 
         [Fact]
         public void ThrowsGivenNegativeFloatValue()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Negative(-1.0f, "negative"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Negative(-1.0f, "negative"));
         }
 
         [Fact]
         public void ThrowsGivenNegativeDoubleValue()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Negative(-1.0, "negative"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Negative(-1.0, "negative"));
         }
 
         [Fact]
         public void ReturnsExpectedValueGivenNonNegativeIntValue()
         {
-            Assert.Equal(0, Cactus.Blade.Guard.Guard.Against.Negative(0, "intZero"));
-            Assert.Equal(1, Cactus.Blade.Guard.Guard.Against.Negative(1, "intOne"));
+            Assert.Equal(0, Cactus.Blade.Guard.Guard.MustBe.Negative(0, "intZero"));
+            Assert.Equal(1, Cactus.Blade.Guard.Guard.MustBe.Negative(1, "intOne"));
         }
 
         [Fact]
         public void ReturnsExpectedValueGivenNonNegativeLongValue()
         {
-            Assert.Equal(0L, Cactus.Blade.Guard.Guard.Against.Negative(0L, "longZero"));
-            Assert.Equal(1L, Cactus.Blade.Guard.Guard.Against.Negative(1L, "longOne"));
+            Assert.Equal(0L, Cactus.Blade.Guard.Guard.MustBe.Negative(0L, "longZero"));
+            Assert.Equal(1L, Cactus.Blade.Guard.Guard.MustBe.Negative(1L, "longOne"));
         }
 
         [Fact]
         public void ReturnsExpectedValueGivenNonNegativeDecimalValue()
         {
-            Assert.Equal(0.0M, Cactus.Blade.Guard.Guard.Against.Negative(0.0M, "decimalZero"));
-            Assert.Equal(1.0M, Cactus.Blade.Guard.Guard.Against.Negative(1.0M, "decimalOne"));
+            Assert.Equal(0.0M, Cactus.Blade.Guard.Guard.MustBe.Negative(0.0M, "decimalZero"));
+            Assert.Equal(1.0M, Cactus.Blade.Guard.Guard.MustBe.Negative(1.0M, "decimalOne"));
         }
 
         [Fact]
         public void ReturnsExpectedValueGivenNonNegativeFloatValue()
         {
-            Assert.Equal(0.0f, Cactus.Blade.Guard.Guard.Against.Negative(0.0f, "floatZero"));
-            Assert.Equal(1.0f, Cactus.Blade.Guard.Guard.Against.Negative(1.0f, "floatOne"));
+            Assert.Equal(0.0f, Cactus.Blade.Guard.Guard.MustBe.Negative(0.0f, "floatZero"));
+            Assert.Equal(1.0f, Cactus.Blade.Guard.Guard.MustBe.Negative(1.0f, "floatOne"));
         }
 
         [Fact]
         public void ReturnsExpectedValueGivenNonNegativeDoubleValue()
         {
-            Assert.Equal(0.0, Cactus.Blade.Guard.Guard.Against.Negative(0.0, "doubleZero"));
-            Assert.Equal(1.0, Cactus.Blade.Guard.Guard.Against.Negative(1.0, "doubleOne"));
+            Assert.Equal(0.0, Cactus.Blade.Guard.Guard.MustBe.Negative(0.0, "doubleZero"));
+            Assert.Equal(1.0, Cactus.Blade.Guard.Guard.MustBe.Negative(1.0, "doubleOne"));
         }
     }
 }
