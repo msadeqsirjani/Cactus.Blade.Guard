072bfe852b36d1733b2d8f1d19073a81fc14f4bd
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:22:38 +0330

    Update Int64.NotZero.cs

--- Guard/System.Int64/Int64.NotZero.cs
+++ Guard/System.Int64/Int64.NotZero.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotZero(this IGuard _, long input)
     {
-        return Guard.MustBe.NotZero(input, null, null);
+        return Guard.MustBe.NotZero<long>(input);
     }
 
     public static IGuard NotZero(this IGuard _, long input, string paramName)
     {
-        return Guard.MustBe.NotZero(input, paramName, null);
+        return Guard.MustBe.NotZero<long>(input, paramName);
     }
 
 
