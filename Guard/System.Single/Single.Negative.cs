6a608f6eda0808734f267a94dab0036bf91a4d22
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:34:21 +0330

    Update Single.Negative.cs

--- Guard/System.Single/Single.Negative.cs
+++ Guard/System.Single/Single.Negative.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard Negative(this IGuard _, float input)
     {
-        return Guard.MustBe.Negative(input, null, null);
+        return Guard.MustBe.Negative<float>(input);
     }
 
     public static IGuard Negative(this IGuard _, float input, string paramName)
     {
-        return Guard.MustBe.Negative(input, paramName, null);
+        return Guard.MustBe.Negative<float>(input, paramName);
     }
 
     public static IGuard Negative(this IGuard _, float input, string paramName, string message)
