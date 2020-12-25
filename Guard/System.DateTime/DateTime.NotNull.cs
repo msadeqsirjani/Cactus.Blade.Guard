9ac15ece8ccfa70e931791f4ea40c7b7d0b2043c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:16:39 +0330

    Create DateTime.NotNull.cs

---
+++ Guard/System.DateTime/DateTime.NotNull.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    public static IGuard NotNull(DateTime? input)
+    {
+        return Guard.MustBe.NotNull(input, null, null);
+    }
+
+    public static IGuard NotNull(DateTime? input, string paramName)
+    {
+        return Guard.MustBe.NotNull(input, paramName, null);
+    }
+
+    public static IGuard NotNull(DateTime? input, string paramName, string message)
+    {
+        return Guard.MustBe.NotNull(input, paramName, message);
+    }
+}
