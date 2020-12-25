949860e6a075a85504dffe4609c14f0481704888
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:00 +0330

    Update Decimal.NotZero.cs

--- Guard/System.Decimal/Decimal.NotZero.cs
+++ Guard/System.Decimal/Decimal.NotZero.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static decimal NotZero(this IGuard _, decimal input)
+    public static IGuard NotZero(this IGuard _, decimal input)
     {
         return Guard.MustBe.NotZero(input, null, null);
     }
 
-    public static decimal NotZero(this IGuard _, decimal input, string paramName)
+    public static IGuard NotZero(this IGuard _, decimal input, string paramName)
     {
         return Guard.MustBe.NotZero(input, paramName, null);
     }
 
 
-    public static decimal NotZero(this IGuard _, decimal input, string paramName, string message)
+    public static IGuard NotZero(this IGuard _, decimal input, string paramName, string message)
     {
         return Guard.MustBe.NotZero<decimal>(input, paramName, message);
     }
