70749f78e84154b6faaf61ebad1ed38a879efb74
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:21:21 +0330

    Update GuardAgainstNull.cs

--- Guard.Test/GuardAgainstNull.cs
+++ Guard.Test/GuardAgainstNull.cs
@@ -4,39 +4,39 @@ using Xunit;
 
 namespace Guard.Test
 {
-    public class GuardAgainstNull
+    public class GuardMustBeNull
     {
         [Fact]
         public void DoesNothingGivenNonNullValue()
         {
-            Cactus.Blade.Guard.Guard.Against.Null("", "string");
-            Cactus.Blade.Guard.Guard.Against.Null(1, "int");
-            Cactus.Blade.Guard.Guard.Against.Null(Guid.Empty, "guid");
-            Cactus.Blade.Guard.Guard.Against.Null(DateTime.Now, "datetime");
-            Cactus.Blade.Guard.Guard.Against.Null(new object(), "object");
+            Cactus.Blade.Guard.Guard.MustBe.Null("", "string");
+            Cactus.Blade.Guard.Guard.MustBe.Null(1, "int");
+            Cactus.Blade.Guard.Guard.MustBe.Null(Guid.Empty, "guid");
+            Cactus.Blade.Guard.Guard.MustBe.Null(DateTime.Now, "datetime");
+            Cactus.Blade.Guard.Guard.MustBe.Null(new object(), "object");
         }
 
         [Fact]
         public void ThrowsGivenNullValue()
         {
             object obj = null!;
-            Assert.Throws<ArgumentNullException>(() => Cactus.Blade.Guard.Guard.Against.Null(obj, "null"));
+            Assert.Throws<ArgumentNullException>(() => Cactus.Blade.Guard.Guard.MustBe.Null(obj, "null"));
         }
 
         [Fact]
         public void ReturnsExpectedValueWhenGivenNonNullValue()
         {
-            Assert.Equal("", Cactus.Blade.Guard.Guard.Against.Null("", "string"));
-            Assert.Equal(1, Cactus.Blade.Guard.Guard.Against.Null(1, "int"));
+            Assert.Equal("", Cactus.Blade.Guard.Guard.MustBe.Null("", "string"));
+            Assert.Equal(1, Cactus.Blade.Guard.Guard.MustBe.Null(1, "int"));
 
             var guid = Guid.Empty;
-            Assert.Equal(guid, Cactus.Blade.Guard.Guard.Against.Null(guid, "guid"));
+            Assert.Equal(guid, Cactus.Blade.Guard.Guard.MustBe.Null(guid, "guid"));
 
             var now = DateTime.Now;
-            Assert.Equal(now, Cactus.Blade.Guard.Guard.Against.Null(now, "datetime"));
+            Assert.Equal(now, Cactus.Blade.Guard.Guard.MustBe.Null(now, "datetime"));
 
             var obj = new object();
-            Assert.Equal(obj, Cactus.Blade.Guard.Guard.Against.Null(obj, "object"));
+            Assert.Equal(obj, Cactus.Blade.Guard.Guard.MustBe.Null(obj, "object"));
         }
     }
 }
