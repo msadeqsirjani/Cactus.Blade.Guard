1ffa8f3bb418dc3ca7e04110ccb7c17ec236342f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:21:10 +0330

    Update GuardAgainstDefault.cs

--- Guard.Test/GuardAgainstDefault.cs
+++ Guard.Test/GuardAgainstDefault.cs
@@ -5,33 +5,33 @@ using Xunit;
 
 namespace Guard.Test
 {
-    public class GuardAgainstDefault
+    public class GuardMustBeDefault
     {
         [Fact]
         public void DoesNothingGivenNonDefaultValue()
         {
-            Cactus.Blade.Guard.Guard.Against.Default("", "string");
-            Cactus.Blade.Guard.Guard.Against.Default(1, "int");
-            Cactus.Blade.Guard.Guard.Against.Default(Guid.NewGuid(), "guid");
-            Cactus.Blade.Guard.Guard.Against.Default(DateTime.Now, "datetime");
-            Cactus.Blade.Guard.Guard.Against.Default(new object(), "object");
+            Cactus.Blade.Guard.Guard.MustBe.Default("", "string");
+            Cactus.Blade.Guard.Guard.MustBe.Default(1, "int");
+            Cactus.Blade.Guard.Guard.MustBe.Default(Guid.NewGuid(), "guid");
+            Cactus.Blade.Guard.Guard.MustBe.Default(DateTime.Now, "datetime");
+            Cactus.Blade.Guard.Guard.MustBe.Default(new object(), "object");
         }
 
         [Fact]
         public void ThrowsGivenDefaultValue()
         {
-            Assert.Throws<ArgumentException>("string", () => Cactus.Blade.Guard.Guard.Against.Default(default(string), "string"));
-            Assert.Throws<ArgumentException>("int", () => Cactus.Blade.Guard.Guard.Against.Default(default(int), "int"));
-            Assert.Throws<ArgumentException>("guid", () => Cactus.Blade.Guard.Guard.Against.Default(default(Guid), "guid"));
-            Assert.Throws<ArgumentException>("datetime", () => Cactus.Blade.Guard.Guard.Against.Default(default(DateTime), "datetime"));
-            Assert.Throws<ArgumentException>("object", () => Cactus.Blade.Guard.Guard.Against.Default(default(object), "object"));
+            Assert.Throws<ArgumentException>("string", () => Cactus.Blade.Guard.Guard.MustBe.Default(default(string), "string"));
+            Assert.Throws<ArgumentException>("int", () => Cactus.Blade.Guard.Guard.MustBe.Default(default(int), "int"));
+            Assert.Throws<ArgumentException>("guid", () => Cactus.Blade.Guard.Guard.MustBe.Default(default(Guid), "guid"));
+            Assert.Throws<ArgumentException>("datetime", () => Cactus.Blade.Guard.Guard.MustBe.Default(default(DateTime), "datetime"));
+            Assert.Throws<ArgumentException>("object", () => Cactus.Blade.Guard.Guard.MustBe.Default(default(object), "object"));
         }
 
         [Theory]
         [MemberData(nameof(GetNonDefaultTestVectors))]
         public void ReturnsExpectedValueWhenGivenNonDefaultValue(object input, string name, object expected)
         {
-            var actual = Cactus.Blade.Guard.Guard.Against.Default(input, name);
+            var actual = Cactus.Blade.Guard.Guard.MustBe.Default(input, name);
             Assert.Equal(expected, actual);
         }
 
