002c461752a59a38503b6e866e36b1c0e9892e4e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:40 +0330

    Update UInt64.Positive.cs

--- Guard/System.UInt64/UInt64.Positive.cs
+++ Guard/System.UInt64/UInt64.Positive.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static ulong Positive(this IGuard _, ulong input)
+    public static IGuard Positive(this IGuard _, ulong input)
     {
         return Guard.MustBe.Positive(input, null, null);
     }
 
-    public static ulong Positive(this IGuard _, ulong input, string paramName)
+    public static IGuard Positive(this IGuard _, ulong input, string paramName)
     {
         return Guard.MustBe.Positive(input, paramName, null);
     }
 
-    public static ulong Positive(this IGuard _, ulong input, string paramName, string message)
+    public static IGuard Positive(this IGuard _, ulong input, string paramName, string message)
     {
         return Guard.MustBe.Positive<ulong>(input, paramName, message);
     }
