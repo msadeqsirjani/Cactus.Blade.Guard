8b40068488c47960fb59a4fa9d1b8bae5185e1a1
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:16:13 +0330

    Create Object.NotNull.cs

---
+++ Guard/System.Object/Object.NotNull.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard NotNull(object input)
+    {
+        return Guard.MustBe.NotNull(input, null, null);
+    }
+
+    public static IGuard NotNull(object input, string paramName)
+    {
+        return Guard.MustBe.NotNull(input, paramName, null);
+    }
+
+    public static IGuard NotNull(object input, string paramName, string message)
+    {
+        return Guard.MustBe.NotNull(input, paramName, message);
+    }
+}
