ae036e9b0dda4e7beca433a95074bedad476fe08
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 13:01:14 +0330

    Create Int64.Max.cs

---
+++ Guard/System.Int64/Int64.Max.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Max(long input, long minimum)
+    {
+        return Guard.MustBe.Max(input, minimum);
+    }
+
+    public static IGuard Max(long input, long minimum, string paramName)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName);
+    }
+
+    public static IGuard Max(long input, long minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName, message);
+    }
+}
