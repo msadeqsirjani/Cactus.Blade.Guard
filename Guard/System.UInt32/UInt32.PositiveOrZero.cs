5e6f3da3473ca07d2d0dba294cc014640e9553a3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:12 +0330

    Update UInt32.PositiveOrZero.cs

--- Guard/System.UInt32/UInt32.PositiveOrZero.cs
+++ Guard/System.UInt32/UInt32.PositiveOrZero.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static uint PositiveOrZero(this IGuard _, uint input)
+    public static IGuard PositiveOrZero(this IGuard _, uint input)
     {
         return Guard.MustBe.PositiveOrZero(input, null, null);
     }
 
-    public static uint PositiveOrZero(this IGuard _, uint input, string paramName)
+    public static IGuard PositiveOrZero(this IGuard _, uint input, string paramName)
     {
         return Guard.MustBe.PositiveOrZero(input, paramName, null);
     }
 
-    public static uint PositiveOrZero(this IGuard _, uint input, string paramName, string message)
+    public static IGuard PositiveOrZero(this IGuard _, uint input, string paramName, string message)
     {
         return Guard.MustBe.PositiveOrZero<uint>(input, paramName, message);
     }
