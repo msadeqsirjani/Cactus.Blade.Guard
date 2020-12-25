803c717e04a46d2fa9d9aa5fa9c52f9b0bfa8cfe
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:26:35 +0330

    Update UInt16.NotZero.cs

--- Guard/System.UInt16/UInt16.NotZero.cs
+++ Guard/System.UInt16/UInt16.NotZero.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static ushort NotZero(this IGuard _, ushort input)
+    public static IGuard NotZero(this IGuard _, ushort input)
     {
         return Guard.MustBe.NotZero(input, null, null);
     }
 
-    public static ushort NotZero(this IGuard _, ushort input, string paramName)
+    public static IGuard NotZero(this IGuard _, ushort input, string paramName)
     {
         return Guard.MustBe.NotZero(input, paramName, null);
     }
 
 
-    public static ushort NotZero(this IGuard _, ushort input, string paramName, string message)
+    public static IGuard NotZero(this IGuard _, ushort input, string paramName, string message)
     {
         return Guard.MustBe.NotZero<ushort>(input, paramName, message);
     }
