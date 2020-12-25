94d6c1454f0529810c299fb2214cad423da1b525
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:39 +0330

    Update UInt64.NotZero.cs

--- Guard/System.UInt64/UInt64.NotZero.cs
+++ Guard/System.UInt64/UInt64.NotZero.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static ulong NotZero(this IGuard _, ulong input)
+    public static IGuard NotZero(this IGuard _, ulong input)
     {
         return Guard.MustBe.NotZero(input, null, null);
     }
 
-    public static ulong NotZero(this IGuard _, ulong input, string paramName)
+    public static IGuard NotZero(this IGuard _, ulong input, string paramName)
     {
         return Guard.MustBe.NotZero(input, paramName, null);
     }
 
 
-    public static ulong NotZero(this IGuard _, ulong input, string paramName, string message)
+    public static IGuard NotZero(this IGuard _, ulong input, string paramName, string message)
     {
         return Guard.MustBe.NotZero<ulong>(input, paramName, message);
     }
