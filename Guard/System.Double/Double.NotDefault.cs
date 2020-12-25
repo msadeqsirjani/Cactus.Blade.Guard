b5c16231ac9d0ffc1f6278295530a1d2674110df
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:03:50 +0330

    Update Double.NotDefault.cs

--- Guard/System.Double/Double.NotDefault.cs
+++ Guard/System.Double/Double.NotDefault.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotDefault(this IGuard _, double input)
     {
-        return Guard.MustBe.NotDefault(input, null, null);
+        return Guard.MustBe.NotDefault<double>(input);
     }
 
     public static IGuard NotDefault(this IGuard _, double input, string paramName)
     {
-        return Guard.MustBe.NotDefault(input, paramName, null);
+        return Guard.MustBe.NotDefault<double>(input, paramName);
     }
 
     public static IGuard NotDefault(this IGuard _, double input, string paramName, string message)
