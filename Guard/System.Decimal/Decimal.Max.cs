9d069f0995e197d5201c7f1abc3c2cd8121ecb84
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 13:01:04 +0330

    Create Decimal.Max.cs

---
+++ Guard/System.Decimal/Decimal.Max.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Max(decimal input, decimal minimum)
+    {
+        return Guard.MustBe.Max(input, minimum);
+    }
+
+    public static IGuard Max(decimal input, decimal minimum, string paramName)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName);
+    }
+
+    public static IGuard Max(decimal input, decimal minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName, message);
+    }
+}
