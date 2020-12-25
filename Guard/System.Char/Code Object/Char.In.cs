89141f80a0e14289561f783f5853823b2a65c464
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:48:23 +0330

    Create Char.In.cs

---
+++ Guard/System.Char/Code Object/Char.In.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+using System.Linq;
+
+public static partial class GuardExtension
+{
+    public static IGuard NotIn(this IGuard _, char value, params char[] values)
+    {
+        return Guard.MustBe.NotIn(value, null, null, values);
+    }
+
+    public static IGuard NotIn(this IGuard _, char value, string paramName, params char[] values)
+    {
+        return Guard.MustBe.NotIn(value, paramName, null, values);
+    }
+
+    public static IGuard NotIn(this IGuard guard, char value, string paramName, string message, params char[] values)
+    {
+        paramName ??= nameof(value);
+        message ??= Message.NotIn(value, values);
+
+        if (!values.Any())
+            throw new ArgumentNullException(Message.Null(values), nameof(values));
+
+        if (value.NotIn(values)) return guard;
+
+        throw new ArgumentOutOfRangeException(message, paramName);
+    }
+}
