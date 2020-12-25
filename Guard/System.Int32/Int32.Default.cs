64eb8eaa85ffb8eb2fc93eea8047aa56e160207d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:42 +0330

    Update Int32.Default.cs

--- Guard/System.Int32/Int32.Default.cs
+++ Guard/System.Int32/Int32.Default.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static int Default(this IGuard _, int input)
+    public static IGuard Default(this IGuard _, int input)
     {
         return Guard.MustBe.Default(input, null, null);
     }
 
-    public static int Default(this IGuard _, int input, string paramName)
+    public static IGuard Default(this IGuard _, int input, string paramName)
     {
         return Guard.MustBe.Default(input, paramName, null);
     }
 
-    public static int Default(this IGuard _, int input, string paramName, string message)
+    public static IGuard Default(this IGuard _, int input, string paramName, string message)
     {
         return Guard.MustBe.Default<int>(input, paramName, message);
     }
