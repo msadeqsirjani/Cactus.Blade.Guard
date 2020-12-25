1306de326c3db87457b9728cf312104493cdcec7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:43:00 +0330

    Update UInt16.Positive.cs

--- Guard/System.UInt16/UInt16.Positive.cs
+++ Guard/System.UInt16/UInt16.Positive.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard Positive(this IGuard _, ushort input)
     {
-        return Guard.MustBe.Positive(input, null, null);
+        return Guard.MustBe.Positive<ushort>(input);
     }
 
     public static IGuard Positive(this IGuard _, ushort input, string paramName)
     {
-        return Guard.MustBe.Positive(input, paramName, null);
+        return Guard.MustBe.Positive<ushort>(input, paramName);
     }
 
     public static IGuard Positive(this IGuard _, ushort input, string paramName, string message)
