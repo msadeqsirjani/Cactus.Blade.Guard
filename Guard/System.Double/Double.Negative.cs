15cf66684a93340af6641cf01f9292e2956e7415
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:03:44 +0330

    Update Double.Negative.cs

--- Guard/System.Double/Double.Negative.cs
+++ Guard/System.Double/Double.Negative.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard Negative(this IGuard _, double input)
     {
-        return Guard.MustBe.Negative(input, null, null);
+        return Guard.MustBe.Negative<double>(input);
     }
 
     public static IGuard Negative(this IGuard _, double input, string paramName)
     {
-        return Guard.MustBe.Negative(input, paramName, null);
+        return Guard.MustBe.Negative<double>(input, paramName);
     }
 
     public static IGuard Negative(this IGuard _, double input, string paramName, string message)
