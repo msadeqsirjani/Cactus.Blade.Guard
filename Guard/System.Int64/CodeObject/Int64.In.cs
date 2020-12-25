76ff6279113ffb7a50022513bc97cba88874698c
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:24 +0330

    Update Int64.In.cs

--- Guard/System.Int64/CodeObject/Int64.In.cs
+++ Guard/System.Int64/CodeObject/Int64.In.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static long In(this IGuard _, long value, params long[] values)
+    public static IGuard In(this IGuard _, long value, params long[] values)
     {
         return Guard.MustBe.In(value, null, null, values);
     }
 
-    public static long In(this IGuard _, long value, string paramName, params long[] values)
+    public static IGuard In(this IGuard _, long value, string paramName, params long[] values)
     {
         return Guard.MustBe.In(value, paramName, null, values);
     }
 
-    public static long In(this IGuard _, long value, string paramName, string message, params long[] values)
+    public static IGuard In(this IGuard _, long value, string paramName, string message, params long[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.In(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.In(values)) return value;
+        if (value.In(values)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
