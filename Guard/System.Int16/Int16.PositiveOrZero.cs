39c5cc9daae5b87e1e945e4c52afe571a5450a49
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:20 +0330

    Update Int16.PositiveOrZero.cs

--- Guard/System.Int16/Int16.PositiveOrZero.cs
+++ Guard/System.Int16/Int16.PositiveOrZero.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static short PositiveOrZero(this IGuard _, short input)
+    public static IGuard PositiveOrZero(this IGuard _, short input)
     {
         return Guard.MustBe.PositiveOrZero(input, null, null);
     }
 
-    public static short PositiveOrZero(this IGuard _, short input, string paramName)
+    public static IGuard PositiveOrZero(this IGuard _, short input, string paramName)
     {
         return Guard.MustBe.PositiveOrZero(input, paramName, null);
     }
 
-    public static short PositiveOrZero(this IGuard _, short input, string paramName, string message)
+    public static IGuard PositiveOrZero(this IGuard _, short input, string paramName, string message)
     {
         return Guard.MustBe.PositiveOrZero<short>(input, paramName, message);
     }
