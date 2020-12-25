e185caa5404b0c033809559776da3b7fb47f53b2
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:26:05 +0330

    Update Single.InRange.cs

--- Guard/System.Single/CodeObject/Single.InRange.cs
+++ Guard/System.Single/CodeObject/Single.InRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static float InRange(this IGuard _, float value, float minimum, float maximum)
+    public static IGuard InRange(this IGuard _, float value, float minimum, float maximum)
     {
         return Guard.MustBe.InRange(value, null, null, minimum, maximum);
     }
 
-    public static float InRange(this IGuard _, float value, string paramName, float minimum, float maximum)
+    public static IGuard InRange(this IGuard _, float value, string paramName, float minimum, float maximum)
     {
         return Guard.MustBe.InRange(value, paramName, null, minimum, maximum);
     }
 
-    public static float InRange(this IGuard _, float value, string paramName, string message, float minimum, float maximum)
+    public static IGuard InRange(this IGuard _, float value, string paramName, string message, float minimum, float maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.InRange(value, minimum, maximum);
 
-        if (value.InRange(minimum, maximum)) return value;
+        if (value.InRange(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
