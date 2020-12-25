5612a8d12cdb2830dc27dbf6d01531f3f2201810
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:04 +0330

    Update Double.Positive.cs

--- Guard/System.Double/Double.Positive.cs
+++ Guard/System.Double/Double.Positive.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static short Positive(this IGuard _, short input)
+    public static IGuard Positive(this IGuard _, double input)
     {
         return Guard.MustBe.Positive(input, null, null);
     }
 
-    public static short Positive(this IGuard _, short input, string paramName)
+    public static IGuard Positive(this IGuard _, double input, string paramName)
     {
         return Guard.MustBe.Positive(input, paramName, null);
     }
 
-    public static short Positive(this IGuard _, short input, string paramName, string message)
+    public static IGuard Positive(this IGuard _, double input, string paramName, string message)
     {
-        return Guard.MustBe.Positive<short>(input, paramName, message);
+        return Guard.MustBe.Positive<double>(input, paramName, message);
     }
 }
