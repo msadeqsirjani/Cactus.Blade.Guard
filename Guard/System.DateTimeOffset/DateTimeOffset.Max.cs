0909208edb18277a1d29e78659419c4e58226ffa
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 13:01:02 +0330

    Create DateTimeOffset.Max.cs

---
+++ Guard/System.DateTimeOffset/DateTimeOffset.Max.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    public static IGuard Max(DateTimeOffset input, DateTimeOffset minimum)
+    {
+        return Guard.MustBe.Max(input, minimum);
+    }
+
+    public static IGuard Max(DateTimeOffset input, DateTimeOffset minimum, string paramName)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName);
+    }
+
+    public static IGuard Max(DateTimeOffset input, DateTimeOffset minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName, message);
+    }
+}
