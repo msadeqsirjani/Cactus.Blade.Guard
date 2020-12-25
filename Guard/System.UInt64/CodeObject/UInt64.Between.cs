efaaa4443985fccf5497261d433c5d1edcd96638
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:18 +0330

    Update UInt64.Between.cs

--- Guard/System.UInt64/CodeObject/UInt64.Between.cs
+++ Guard/System.UInt64/CodeObject/UInt64.Between.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static ulong Between(this IGuard _, ulong value, ulong minimum, ulong maximum)
+    public static IGuard Between(this IGuard _, ulong value, ulong minimum, ulong maximum)
     {
         return Guard.MustBe.Between(value, null, null, minimum, maximum);
     }
 
-    public static ulong Between(this IGuard _, ulong value, string paramName, ulong minimum, ulong maximum)
+    public static IGuard Between(this IGuard _, ulong value, string paramName, ulong minimum, ulong maximum)
     {
         return Guard.MustBe.Between(value, paramName, null, minimum, maximum);
     }
 
-    public static ulong Between(this IGuard _, ulong value, string paramName, string message, ulong minimum, ulong maximum)
+    public static IGuard Between(this IGuard _, ulong value, string paramName, string message, ulong minimum, ulong maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.Between(value, minimum, maximum);
 
-        if (value.Between(minimum, maximum)) return value;
+        if (value.Between(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
