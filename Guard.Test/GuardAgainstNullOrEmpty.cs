f3031b8698955ce3d6dc026273f82aeac7880186
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:21:28 +0330

    Update GuardAgainstNullOrEmpty.cs

--- Guard.Test/GuardAgainstNullOrEmpty.cs
+++ Guard.Test/GuardAgainstNullOrEmpty.cs
@@ -5,26 +5,26 @@ using Xunit;
 
 namespace Guard.Test
 {
-    public class GuardAgainstNullOrEmpty
+    public class GuardMustBeNullOrEmpty
     {
         [Fact]
         public void DoesNothingGivenNonEmptyStringValue()
         {
-            Cactus.Blade.Guard.Guard.Against.NullOrEmpty("a", "string");
-            Cactus.Blade.Guard.Guard.Against.NullOrEmpty("1", "aNumericString");
+            Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty("a", "string");
+            Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty("1", "aNumericString");
         }
 
         [Fact]
         public void DoesNothingGivenNonEmptyGuidValue()
         {
-            Cactus.Blade.Guard.Guard.Against.NullOrEmpty(Guid.NewGuid(), "guid");
+            Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(Guid.NewGuid(), "guid");
         }
 
         [Fact]
         public void DoesNothingGivenNonEmptyEnumerable()
         {
-            Cactus.Blade.Guard.Guard.Against.NullOrEmpty(new[] { "foo", "bar" }, "stringArray");
-            Cactus.Blade.Guard.Guard.Against.NullOrEmpty(new[] { 1, 2 }, "intArray");
+            Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(new[] { "foo", "bar" }, "stringArray");
+            Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(new[] { 1, 2 }, "intArray");
         }
 
         [Fact]
@@ -32,13 +32,13 @@ namespace Guard.Test
         {
             string? nullString = null;
             Assert.Throws<ArgumentNullException>(() =>
-                Cactus.Blade.Guard.Guard.Against.NullOrEmpty(nullString, "nullString"));
+                Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(nullString, "nullString"));
         }
 
         [Fact]
         public void ThrowsGivenEmptyString()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.NullOrEmpty("", "emptyString"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty("", "emptyString"));
         }
 
         [Fact]
@@ -46,34 +46,34 @@ namespace Guard.Test
         {
             Guid? nullGuid = null;
             Assert.Throws<ArgumentNullException>(() =>
-                Cactus.Blade.Guard.Guard.Against.NullOrEmpty(nullGuid, "nullGuid"));
+                Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(nullGuid, "nullGuid"));
         }
 
         [Fact]
         public void ThrowsGivenEmptyGuid()
         {
             Assert.Throws<ArgumentException>(
-                () => Cactus.Blade.Guard.Guard.Against.NullOrEmpty(Guid.Empty, "emptyGuid"));
+                () => Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(Guid.Empty, "emptyGuid"));
         }
 
         [Fact]
         public void ThrowsGivenEmptyEnumerable()
         {
             Assert.Throws<ArgumentException>(() =>
-                Cactus.Blade.Guard.Guard.Against.NullOrEmpty(Enumerable.Empty<string>(), "emptyStringEnumerable"));
+                Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(Enumerable.Empty<string>(), "emptyStringEnumerable"));
         }
 
         [Fact]
         public void ReturnsExpectedValueWhenGivenValidValue()
         {
-            Assert.Equal("a", Cactus.Blade.Guard.Guard.Against.NullOrEmpty("a", "string"));
-            Assert.Equal("1", Cactus.Blade.Guard.Guard.Against.NullOrEmpty("1", "aNumericString"));
+            Assert.Equal("a", Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty("a", "string"));
+            Assert.Equal("1", Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty("1", "aNumericString"));
 
             var collection1 = new[] { "foo", "bar" };
-            Assert.Equal(collection1, Cactus.Blade.Guard.Guard.Against.NullOrEmpty(collection1, "stringArray"));
+            Assert.Equal(collection1, Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(collection1, "stringArray"));
 
             var collection2 = new[] { 1, 2 };
-            Assert.Equal(collection2, Cactus.Blade.Guard.Guard.Against.NullOrEmpty(collection2, "intArray"));
+            Assert.Equal(collection2, Cactus.Blade.Guard.Guard.MustBe.NullOrEmpty(collection2, "intArray"));
         }
     }
 }
