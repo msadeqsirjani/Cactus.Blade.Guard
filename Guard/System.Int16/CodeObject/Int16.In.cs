c95ad28a7ca5ef30520e5ce626d6b6c2ba6acbcd
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:14 +0330

    Update Int16.In.cs

--- Guard/System.Int16/CodeObject/Int16.In.cs
+++ Guard/System.Int16/CodeObject/Int16.In.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static short In(this IGuard _, short value, params short[] values)
+    public static IGuard In(this IGuard _, short value, params short[] values)
     {
         return Guard.MustBe.In(value, null, null, values);
     }
 
-    public static short In(this IGuard _, short value, string paramName, params short[] values)
+    public static IGuard In(this IGuard _, short value, string paramName, params short[] values)
     {
         return Guard.MustBe.In(value, paramName, null, values);
     }
 
-    public static short In(this IGuard _, short value, string paramName, string message, params short[] values)
+    public static IGuard In(this IGuard _, short value, string paramName, string message, params short[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.In(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.In(values)) return value;
+        if (value.In(values)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
