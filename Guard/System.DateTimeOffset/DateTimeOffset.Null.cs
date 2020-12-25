225831f214ecb6c64addb8382056a5dc74512f72
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:57:51 +0330

    Update DateTimeOffset.Null.cs

--- Guard/System.DateTimeOffset/DateTimeOffset.Null.cs
+++ Guard/System.DateTimeOffset/DateTimeOffset.Null.cs
@@ -5,12 +5,12 @@ public static partial class GuardExtension
 {
     public static IGuard Null(DateTimeOffset? input)
     {
-        return Guard.MustBe.Null(input, null, null);
+        return Guard.MustBe.Null(input);
     }
 
     public static IGuard Null(DateTimeOffset? input, string paramName)
     {
-        return Guard.MustBe.Null(input, paramName, null);
+        return Guard.MustBe.Null(input, paramName);
     }
 
     public static IGuard Null(DateTimeOffset? input, string paramName, string message)
