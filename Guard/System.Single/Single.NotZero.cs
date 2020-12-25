87162f76c6b099e3b0ab8125892b32603f6377d6
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:34:31 +0330

    Update Single.NotZero.cs

--- Guard/System.Single/Single.NotZero.cs
+++ Guard/System.Single/Single.NotZero.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotZero(this IGuard _, float input)
     {
-        return Guard.MustBe.NotZero(input, null, null);
+        return Guard.MustBe.NotZero<float>(input);
     }
 
     public static IGuard NotZero(this IGuard _, float input, string paramName)
     {
-        return Guard.MustBe.NotZero(input, paramName, null);
+        return Guard.MustBe.NotZero<float>(input, paramName);
     }
 
 
