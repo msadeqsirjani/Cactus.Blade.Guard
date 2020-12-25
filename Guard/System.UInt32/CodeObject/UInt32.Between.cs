2ab73312c9ee2de19942921c59e943706bb76f0f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:26:50 +0330

    Update UInt32.Between.cs

--- Guard/System.UInt32/CodeObject/UInt32.Between.cs
+++ Guard/System.UInt32/CodeObject/UInt32.Between.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static uint Between(this IGuard _, uint value, uint minimum, uint maximum)
+    public static IGuard Between(this IGuard _, uint value, uint minimum, uint maximum)
     {
         return Guard.MustBe.Between(value, null, null, minimum, maximum);
     }
 
-    public static uint Between(this IGuard _, uint value, string paramName, uint minimum, uint maximum)
+    public static IGuard Between(this IGuard _, uint value, string paramName, uint minimum, uint maximum)
     {
         return Guard.MustBe.Between(value, paramName, null, minimum, maximum);
     }
 
-    public static uint Between(this IGuard _, uint value, string paramName, string message, uint minimum, uint maximum)
+    public static IGuard Between(this IGuard _, uint value, string paramName, string message, uint minimum, uint maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.Between(value, minimum, maximum);
 
-        if (value.Between(minimum, maximum)) return value;
+        if (value.Between(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
