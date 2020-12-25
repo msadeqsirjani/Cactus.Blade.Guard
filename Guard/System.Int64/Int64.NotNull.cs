1067ef5165456c1d6de7ef4ab4c48f5cafd5caa4
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:22:33 +0330

    Update Int64.NotNull.cs

--- Guard/System.Int64/Int64.NotNull.cs
+++ Guard/System.Int64/Int64.NotNull.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotNull(long? input)
     {
-        return Guard.MustBe.NotNull(input, null, null);
+        return Guard.MustBe.NotNull(input);
     }
 
     public static IGuard NotNull(long? input, string paramName)
     {
-        return Guard.MustBe.NotNull(input, paramName, null);
+        return Guard.MustBe.NotNull(input, paramName);
     }
 
     public static IGuard NotNull(long? input, string paramName, string message)
