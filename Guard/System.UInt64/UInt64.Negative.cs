94aecd9938986157c46a19370add23eb72194df2
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:31 +0330

    Update UInt64.Negative.cs

--- Guard/System.UInt64/UInt64.Negative.cs
+++ Guard/System.UInt64/UInt64.Negative.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static ulong Negative(this IGuard _, ulong input)
+    public static IGuard Negative(this IGuard _, ulong input)
     {
         return Guard.MustBe.Negative(input, null, null);
     }
 
-    public static ulong Negative(this IGuard _, ulong input, string paramName)
+    public static IGuard Negative(this IGuard _, ulong input, string paramName)
     {
         return Guard.MustBe.Negative(input, paramName, null);
     }
 
-    public static ulong Negative(this IGuard _, ulong input, string paramName, string message)
+    public static IGuard Negative(this IGuard _, ulong input, string paramName, string message)
     {
         return Guard.MustBe.Negative<ulong>(input, paramName, message);
     }
