5207c8593796f2e1b88999442338aa73fc09fb85
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:24:44 +0330

    Update Double.NotIn.cs

--- Guard/System.Double/CodeObject/Double.NotIn.cs
+++ Guard/System.Double/CodeObject/Double.NotIn.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static double NotIn(this IGuard _, double value, params double[] values)
+    public static IGuard NotIn(this IGuard _, double value, params double[] values)
     {
         return Guard.MustBe.NotIn(value, null, null, values);
     }
 
-    public static double NotIn(this IGuard _, double value, string paramName, params double[] values)
+    public static IGuard NotIn(this IGuard _, double value, string paramName, params double[] values)
     {
         return Guard.MustBe.NotIn(value, paramName, null, values);
     }
 
-    public static double NotIn(this IGuard _, double value, string paramName, string message, params double[] values)
+    public static IGuard NotIn(this IGuard _, double value, string paramName, string message, params double[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.NotIn(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.NotIn(values)) return value;
+        if (value.NotIn(values)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
