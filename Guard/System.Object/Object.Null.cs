2ed7cacde5aeff2b505ffe179b2cba1c5ad817f0
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:15:42 +0330

    Create Object.Null.cs

---
+++ Guard/System.Object/Object.Null.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Null(object input)
+    {
+        return Guard.MustBe.Null(input, null, null);
+    }
+
+    public static IGuard Null(object input, string paramName)
+    {
+        return Guard.MustBe.Null(input, paramName, null);
+    }
+
+    public static IGuard Null(object input, string paramName, string message)
+    {
+        return Guard.MustBe.Null(input, paramName, message);
+    }
+}
