896dbe7bfefaa785d11abecbbc686f0e89cfb125
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:26:18 +0330

    Update UInt16.Negative.cs

--- Guard/System.UInt16/UInt16.Negative.cs
+++ Guard/System.UInt16/UInt16.Negative.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static ushort Negative(this IGuard _, ushort input)
+    public static IGuard Negative(this IGuard _, ushort input)
     {
         return Guard.MustBe.Negative(input, null, null);
     }
 
-    public static ushort Negative(this IGuard _, ushort input, string paramName)
+    public static IGuard Negative(this IGuard _, ushort input, string paramName)
     {
         return Guard.MustBe.Negative(input, paramName, null);
     }
 
-    public static ushort Negative(this IGuard _, ushort input, string paramName, string message)
+    public static IGuard Negative(this IGuard _, ushort input, string paramName, string message)
     {
         return Guard.MustBe.Negative<ushort>(input, paramName, message);
     }
