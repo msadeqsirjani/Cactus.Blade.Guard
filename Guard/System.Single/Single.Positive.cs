f29d6cd61950eb6969e379debf1545e8a0295e39
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:28:28 +0330

    Update Single.Positive.cs

--- Guard/System.Single/Single.Positive.cs
+++ Guard/System.Single/Single.Positive.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static float Positive(this IGuard _, float input)
+    public static IGuard Positive(this IGuard _, float input)
     {
         return Guard.MustBe.Positive(input, null, null);
     }
 
-    public static float Positive(this IGuard _, float input, string paramName)
+    public static IGuard Positive(this IGuard _, float input, string paramName)
     {
         return Guard.MustBe.Positive(input, paramName, null);
     }
 
-    public static float Positive(this IGuard _, float input, string paramName, string message)
+    public static IGuard Positive(this IGuard _, float input, string paramName, string message)
     {
         return Guard.MustBe.Positive<float>(input, paramName, message);
     }
