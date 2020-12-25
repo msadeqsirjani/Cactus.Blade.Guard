ccba827d17f77f536bf0e89a4cf959fd8542da64
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:48 +0330

    Update Int64.Positive.cs

--- Guard/System.Int64/Int64.Positive.cs
+++ Guard/System.Int64/Int64.Positive.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static long Positive(this IGuard _, long input)
+    public static IGuard Positive(this IGuard _, long input)
     {
         return Guard.MustBe.Positive(input, null, null);
     }
 
-    public static long Positive(this IGuard _, long input, string paramName)
+    public static IGuard Positive(this IGuard _, long input, string paramName)
     {
         return Guard.MustBe.Positive(input, paramName, null);
     }
 
-    public static long Positive(this IGuard _, long input, string paramName, string message)
+    public static IGuard Positive(this IGuard _, long input, string paramName, string message)
     {
         return Guard.MustBe.Positive<long>(input, paramName, message);
     }
