ad9f9b47a0b6598e641c5d72644b2093fe893f27
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:34:33 +0330

    Update Single.Null.cs

--- Guard/System.Single/Single.Null.cs
+++ Guard/System.Single/Single.Null.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard Null(float? input)
     {
-        return Guard.MustBe.Null(input, null, null);
+        return Guard.MustBe.Null(input);
     }
 
     public static IGuard Null(float? input, string paramName)
     {
-        return Guard.MustBe.Null(input, paramName, null);
+        return Guard.MustBe.Null(input, paramName);
     }
 
     public static IGuard Null(float? input, string paramName, string message)
