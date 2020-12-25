d578cad0107f1e407d0033fe48861963bc0cfb16
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:16:21 +0330

    Create UInt32.NotNull.cs

---
+++ Guard/System.UInt32/UInt32.NotNull.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard NotNull(uint? input)
+    {
+        return Guard.MustBe.NotNull(input, null, null);
+    }
+
+    public static IGuard NotNull(uint? input, string paramName)
+    {
+        return Guard.MustBe.NotNull(input, paramName, null);
+    }
+
+    public static IGuard NotNull(uint? input, string paramName, string message)
+    {
+        return Guard.MustBe.NotNull(input, paramName, message);
+    }
+}
