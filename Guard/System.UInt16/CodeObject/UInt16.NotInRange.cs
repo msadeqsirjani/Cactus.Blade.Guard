22ecc646cd1f901fbba850b61ff06010e10f2a13
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:26:25 +0330

    Update UInt16.NotInRange.cs

--- Guard/System.UInt16/CodeObject/UInt16.NotInRange.cs
+++ Guard/System.UInt16/CodeObject/UInt16.NotInRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static ushort NotInRange(this IGuard _, ushort value, ushort minimum, ushort maximum)
+    public static IGuard NotInRange(this IGuard _, ushort value, ushort minimum, ushort maximum)
     {
         return Guard.MustBe.NotInRange(value, null, null, minimum, maximum);
     }
 
-    public static ushort NotInRange(this IGuard _, ushort value, string paramName, ushort minimum, ushort maximum)
+    public static IGuard NotInRange(this IGuard _, ushort value, string paramName, ushort minimum, ushort maximum)
     {
         return Guard.MustBe.NotInRange(value, paramName, null, minimum, maximum);
     }
 
-    public static ushort NotInRange(this IGuard _, ushort value, string paramName, string message, ushort minimum, ushort maximum)
+    public static IGuard NotInRange(this IGuard _, ushort value, string paramName, string message, ushort minimum, ushort maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.NotInRange(value, minimum, maximum);
 
-        if (value.NotInRange(minimum, maximum)) return value;
+        if (value.NotInRange(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
