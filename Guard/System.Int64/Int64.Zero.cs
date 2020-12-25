cf229049e77bd289150abfeccef805f89c2bf31e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:22:55 +0330

    Update Int64.Zero.cs

--- Guard/System.Int64/Int64.Zero.cs
+++ Guard/System.Int64/Int64.Zero.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard Zero(this IGuard _, long input)
     {
-        return Guard.MustBe.Zero(input, null, null);
+        return Guard.MustBe.Zero<long>(input);
     }
 
     public static IGuard Zero(this IGuard _, long input, string paramName)
     {
-        return Guard.MustBe.Zero(input, paramName, null);
+        return Guard.MustBe.Zero<long>(input, paramName);
     }
 
 
