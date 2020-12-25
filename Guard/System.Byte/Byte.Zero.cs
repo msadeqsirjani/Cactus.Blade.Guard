a86f257cd8451074b895f3bee420bd5002828048
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:41:36 +0330

    Update Byte.Zero.cs

--- Guard/System.Byte/Byte.Zero.cs
+++ Guard/System.Byte/Byte.Zero.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard Zero(this IGuard _, byte input)
     {
-        return Guard.MustBe.Zero(input, null, null);
+        return Guard.MustBe.Zero<byte>(input);
     }
 
     public static IGuard Zero(this IGuard _, byte input, string paramName)
     {
-        return Guard.MustBe.Zero(input, paramName, null);
+        return Guard.MustBe.Zero<byte>(input, paramName);
     }
 
 
