c4c171b882c8bc5701b17e92bede2911c3709611
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:24:05 +0330

    Update Double.InRange.cs

--- Guard/System.Double/CodeObject/Double.InRange.cs
+++ Guard/System.Double/CodeObject/Double.InRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static double InRange(this IGuard _, double value, double minimum, double maximum)
+    public static IGuard InRange(this IGuard _, double value, double minimum, double maximum)
     {
         return Guard.MustBe.InRange(value, null, null, minimum, maximum);
     }
 
-    public static double InRange(this IGuard _, double value, string paramName, double minimum, double maximum)
+    public static IGuard InRange(this IGuard _, double value, string paramName, double minimum, double maximum)
     {
         return Guard.MustBe.InRange(value, paramName, null, minimum, maximum);
     }
 
-    public static double InRange(this IGuard _, double value, string paramName, string message, double minimum, double maximum)
+    public static IGuard InRange(this IGuard _, double value, string paramName, string message, double minimum, double maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.InRange(value, minimum, maximum);
 
-        if (value.InRange(minimum, maximum)) return value;
+        if (value.InRange(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
