6dfecc3d18256edf85eda1a20322ff97a799630e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:41:32 +0330

    Create T.Min.cs

---
+++ Guard/System.T/T.Min.cs
@@ -0,0 +1,30 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    private static IGuard Min<T>(this IGuard _, T input, T minimum)
+        where T : struct, IComparable
+    {
+        return Guard.MustBe.Min(input, minimum, null, null);
+    }
+
+    private static IGuard Min<T>(this IGuard _, T input, T minimum, string paramName)
+        where T : struct, IComparable
+    {
+        return Guard.MustBe.Min(input, minimum, paramName, null);
+    }
+
+    private static IGuard Min<T>(this IGuard guard, T input, T minimum, string paramName, string message)
+        where T : struct, IComparable
+    {
+        paramName ??= nameof(input);
+        message ??= Message.Min(paramName, nameof(minimum));
+
+        if (input.CompareTo(minimum) > 0)
+            throw new ArgumentException(message, paramName);
+
+        return guard;
+    }
+}
