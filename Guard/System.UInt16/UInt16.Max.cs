dd5e2e37ace56ce711f6fc95d1725f0f4c657583
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 13:01:26 +0330

    Create UInt16.Max.cs

---
+++ Guard/System.UInt16/UInt16.Max.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Max(ushort input, ushort minimum)
+    {
+        return Guard.MustBe.Max(input, minimum);
+    }
+
+    public static IGuard Max(ushort input, ushort minimum, string paramName)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName);
+    }
+
+    public static IGuard Max(ushort input, ushort minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName, message);
+    }
+}
