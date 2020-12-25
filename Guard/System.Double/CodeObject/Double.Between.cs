d4ae6cb79a4fa8ffb148d5adebb2b99c44c8fb2e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:24:00 +0330

    Update Double.Between.cs

--- Guard/System.Double/CodeObject/Double.Between.cs
+++ Guard/System.Double/CodeObject/Double.Between.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static double Between(this IGuard _, double value, double minimum, double maximum)
+    public static IGuard Between(this IGuard _, double value, double minimum, double maximum)
     {
         return Guard.MustBe.Between(value, null, null, minimum, maximum);
     }
 
-    public static double Between(this IGuard _, double value, string paramName, double minimum, double maximum)
+    public static IGuard Between(this IGuard _, double value, string paramName, double minimum, double maximum)
     {
         return Guard.MustBe.Between(value, paramName, null, minimum, maximum);
     }
 
-    public static double Between(this IGuard _, double value, string paramName, string message, double minimum, double maximum)
+    public static IGuard Between(this IGuard _, double value, string paramName, string message, double minimum, double maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.Between(value, minimum, maximum);
 
-        if (value.Between(minimum, maximum)) return value;
+        if (value.Between(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
