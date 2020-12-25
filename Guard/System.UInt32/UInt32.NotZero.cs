74e1f8ee5e1ca999b3d1526b32fd743118863ab7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:06 +0330

    Update UInt32.NotZero.cs

--- Guard/System.UInt32/UInt32.NotZero.cs
+++ Guard/System.UInt32/UInt32.NotZero.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static uint NotZero(this IGuard _, uint input)
+    public static IGuard NotZero(this IGuard _, uint input)
     {
         return Guard.MustBe.NotZero(input, null, null);
     }
 
-    public static uint NotZero(this IGuard _, uint input, string paramName)
+    public static IGuard NotZero(this IGuard _, uint input, string paramName)
     {
         return Guard.MustBe.NotZero(input, paramName, null);
     }
 
 
-    public static uint NotZero(this IGuard _, uint input, string paramName, string message)
+    public static IGuard NotZero(this IGuard _, uint input, string paramName, string message)
     {
         return Guard.MustBe.NotZero<uint>(input, paramName, message);
     }
