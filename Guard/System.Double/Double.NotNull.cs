9f326af542cffe9eac7cb138811f28c279daff5a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:03:53 +0330

    Update Double.NotNull.cs

--- Guard/System.Double/Double.NotNull.cs
+++ Guard/System.Double/Double.NotNull.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotNull(double? input)
     {
-        return Guard.MustBe.NotNull(input, null, null);
+        return Guard.MustBe.NotNull(input);
     }
 
     public static IGuard NotNull(double? input, string paramName)
     {
-        return Guard.MustBe.NotNull(input, paramName, null);
+        return Guard.MustBe.NotNull(input, paramName);
     }
 
     public static IGuard NotNull(double? input, string paramName, string message)
