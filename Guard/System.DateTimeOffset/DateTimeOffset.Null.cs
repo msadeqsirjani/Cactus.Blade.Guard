ad49ae72985727e97f136acc21286579418286ba
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:16:32 +0330

    Create DateTimeOffset.Null.cs

---
+++ Guard/System.DateTimeOffset/DateTimeOffset.Null.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    public static IGuard Null(DateTimeOffset? input)
+    {
+        return Guard.MustBe.Null(input, null, null);
+    }
+
+    public static IGuard Null(DateTimeOffset? input, string paramName)
+    {
+        return Guard.MustBe.Null(input, paramName, null);
+    }
+
+    public static IGuard Null(DateTimeOffset? input, string paramName, string message)
+    {
+        return Guard.MustBe.Null(input, paramName, message);
+    }
+}
