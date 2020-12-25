fc49326dd0f00314ab0aa801f841fc5c74520a43
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:26:53 +0330

    Update UInt32.InRange.cs

--- Guard/System.UInt32/CodeObject/UInt32.InRange.cs
+++ Guard/System.UInt32/CodeObject/UInt32.InRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static uint InRange(this IGuard _, uint value, uint minimum, uint maximum)
+    public static IGuard InRange(this IGuard _, uint value, uint minimum, uint maximum)
     {
         return Guard.MustBe.InRange(value, null, null, minimum, maximum);
     }
 
-    public static uint InRange(this IGuard _, uint value, string paramName, uint minimum, uint maximum)
+    public static IGuard InRange(this IGuard _, uint value, string paramName, uint minimum, uint maximum)
     {
         return Guard.MustBe.InRange(value, paramName, null, minimum, maximum);
     }
 
-    public static uint InRange(this IGuard _, uint value, string paramName, string message, uint minimum, uint maximum)
+    public static IGuard InRange(this IGuard _, uint value, string paramName, string message, uint minimum, uint maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.InRange(value, minimum, maximum);
 
-        if (value.InRange(minimum, maximum)) return value;
+        if (value.InRange(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
