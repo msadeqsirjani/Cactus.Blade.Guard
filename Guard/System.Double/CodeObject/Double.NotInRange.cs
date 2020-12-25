bd450504a3f0ec142c44b7d911d523f765dd710f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:24:47 +0330

    Update Double.NotInRange.cs

--- Guard/System.Double/CodeObject/Double.NotInRange.cs
+++ Guard/System.Double/CodeObject/Double.NotInRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static double NotInRange(this IGuard _, double value, double minimum, double maximum)
+    public static IGuard NotInRange(this IGuard _, double value, double minimum, double maximum)
     {
         return Guard.MustBe.NotInRange(value, null, null, minimum, maximum);
     }
 
-    public static double NotInRange(this IGuard _, double value, string paramName, double minimum, double maximum)
+    public static IGuard NotInRange(this IGuard _, double value, string paramName, double minimum, double maximum)
     {
         return Guard.MustBe.NotInRange(value, paramName, null, minimum, maximum);
     }
 
-    public static double NotInRange(this IGuard _, double value, string paramName, string message, double minimum, double maximum)
+    public static IGuard NotInRange(this IGuard _, double value, string paramName, string message, double minimum, double maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.NotInRange(value, minimum, maximum);
 
-        if (value.NotInRange(minimum, maximum)) return value;
+        if (value.NotInRange(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
