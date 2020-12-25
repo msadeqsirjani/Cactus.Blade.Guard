670084c170aca59a8df4bc6e25e16185ac474bda
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:20 +0330

    Update Int32.Zero.cs

--- Guard/System.Int32/Int32.Zero.cs
+++ Guard/System.Int32/Int32.Zero.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static int Zero(this IGuard _, int input)
+    public static IGuard Zero(this IGuard _, int input)
     {
         return Guard.MustBe.Zero(input, null, null);
     }
 
-    public static int Zero(this IGuard _, int input, string paramName)
+    public static IGuard Zero(this IGuard _, int input, string paramName)
     {
         return Guard.MustBe.Zero(input, paramName, null);
     }
 
 
-    public static int Zero(this IGuard _, int input, string paramName, string message)
+    public static IGuard Zero(this IGuard _, int input, string paramName, string message)
     {
         return Guard.MustBe.Zero<int>(input, paramName, message);
     }
