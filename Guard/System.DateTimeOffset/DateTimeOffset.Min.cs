54816b81273e4a5258f95d7d0ea3ddff451f3ff7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:55:21 +0330

    Create DateTimeOffset.Min.cs

---
+++ Guard/System.DateTimeOffset/DateTimeOffset.Min.cs
@@ -0,0 +1,20 @@
+﻿using Cactus.Blade.Guard;
+using System;
+
+public static partial class GuardExtension
+{
+    public static IGuard Min(DateTimeOffset input, DateTimeOffset minimum)
+    {
+        return Guard.MustBe.Min(input, minimum);
+    }
+
+    public static IGuard Min(DateTimeOffset input, DateTimeOffset minimum, string paramName)
+    {
+        return Guard.MustBe.Min(input, minimum, paramName);
+    }
+
+    public static IGuard Min(DateTimeOffset input, DateTimeOffset minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Min(input, minimum, paramName, message);
+    }
+}
