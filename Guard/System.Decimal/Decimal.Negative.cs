0761258449f2d671863c625fec196965631f2acd
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:21:54 +0330

    Update Decimal.Negative.cs

--- Guard/System.Decimal/Decimal.Negative.cs
+++ Guard/System.Decimal/Decimal.Negative.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static decimal Negative(this IGuard _, decimal input)
+    public static IGuard Negative(this IGuard _, decimal input)
     {
         return Guard.MustBe.Negative(input, null, null);
     }
 
-    public static decimal Negative(this IGuard _, decimal input, string paramName)
+    public static IGuard Negative(this IGuard _, decimal input, string paramName)
     {
         return Guard.MustBe.Negative(input, paramName, null);
     }
 
-    public static decimal Negative(this IGuard _, decimal input, string paramName, string message)
+    public static IGuard Negative(this IGuard _, decimal input, string paramName, string message)
     {
         return Guard.MustBe.Negative<decimal>(input, paramName, message);
     }
