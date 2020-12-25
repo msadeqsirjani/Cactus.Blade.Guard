9ced1782937e9bf2e4c27586107de8695cac1d79
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:42 +0330

    Update UInt64.PositiveOrZero.cs

--- Guard/System.UInt64/UInt64.PositiveOrZero.cs
+++ Guard/System.UInt64/UInt64.PositiveOrZero.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static ulong PositiveOrZero(this IGuard _, ulong input)
+    public static IGuard PositiveOrZero(this IGuard _, ulong input)
     {
         return Guard.MustBe.PositiveOrZero(input, null, null);
     }
 
-    public static ulong PositiveOrZero(this IGuard _, ulong input, string paramName)
+    public static IGuard PositiveOrZero(this IGuard _, ulong input, string paramName)
     {
         return Guard.MustBe.PositiveOrZero(input, paramName, null);
     }
 
-    public static ulong PositiveOrZero(this IGuard _, ulong input, string paramName, string message)
+    public static IGuard PositiveOrZero(this IGuard _, ulong input, string paramName, string message)
     {
         return Guard.MustBe.PositiveOrZero<ulong>(input, paramName, message);
     }
