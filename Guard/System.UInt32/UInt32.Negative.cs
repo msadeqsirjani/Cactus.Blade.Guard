01c3d89d76993eabe2669f81825e91d8940d6703
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:51 +0330

    Update UInt32.Negative.cs

--- Guard/System.UInt32/UInt32.Negative.cs
+++ Guard/System.UInt32/UInt32.Negative.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static uint Negative(this IGuard _, uint input)
+    public static IGuard Negative(this IGuard _, uint input)
     {
         return Guard.MustBe.Negative(input, null, null);
     }
 
-    public static uint Negative(this IGuard _, uint input, string paramName)
+    public static IGuard Negative(this IGuard _, uint input, string paramName)
     {
         return Guard.MustBe.Negative(input, paramName, null);
     }
 
-    public static uint Negative(this IGuard _, uint input, string paramName, string message)
+    public static IGuard Negative(this IGuard _, uint input, string paramName, string message)
     {
         return Guard.MustBe.Negative<uint>(input, paramName, message);
     }
