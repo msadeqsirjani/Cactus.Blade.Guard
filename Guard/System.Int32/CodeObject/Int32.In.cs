0f085ea9650a6581d934c8540d78ccb553106d87
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:30 +0330

    Update Int32.In.cs

--- Guard/System.Int32/CodeObject/Int32.In.cs
+++ Guard/System.Int32/CodeObject/Int32.In.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static int In(this IGuard _, int value, params int[] values)
+    public static IGuard In(this IGuard _, int value, params int[] values)
     {
         return Guard.MustBe.In(value, null, null, values);
     }
 
-    public static int In(this IGuard _, int value, string paramName, params int[] values)
+    public static IGuard In(this IGuard _, int value, string paramName, params int[] values)
     {
         return Guard.MustBe.In(value, paramName, null, values);
     }
 
-    public static int In(this IGuard _, int value, string paramName, string message, params int[] values)
+    public static IGuard In(this IGuard _, int value, string paramName, string message, params int[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.In(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.In(values)) return value;
+        if (value.In(values)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
