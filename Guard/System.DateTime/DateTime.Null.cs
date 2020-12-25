eac9f2b1651dd5c0d3e31ff76f6717d92f529cb7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:50:41 +0330

    Update DateTime.Null.cs

--- Guard/System.DateTime/DateTime.Null.cs
+++ Guard/System.DateTime/DateTime.Null.cs
@@ -5,12 +5,12 @@ public static partial class GuardExtension
 {
     public static IGuard Null(DateTime? input)
     {
-        return Guard.MustBe.Null(input, null, null);
+        return Guard.MustBe.Null(input);
     }
 
     public static IGuard Null(DateTime? input, string paramName)
     {
-        return Guard.MustBe.Null(input, paramName, null);
+        return Guard.MustBe.Null(input, paramName);
     }
 
     public static IGuard Null(DateTime? input, string paramName, string message)
