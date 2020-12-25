dadbe79281e8eda03f6ed32c825a3782642e00de
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:05 +0330

    Update Decimal.PositiveOrZero.cs

--- Guard/System.Decimal/Decimal.PositiveOrZero.cs
+++ Guard/System.Decimal/Decimal.PositiveOrZero.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static decimal PositiveOrZero(this IGuard _, decimal input)
+    public static IGuard PositiveOrZero(this IGuard _, decimal input)
     {
         return Guard.MustBe.PositiveOrZero(input, null, null);
     }
 
-    public static decimal PositiveOrZero(this IGuard _, decimal input, string paramName)
+    public static IGuard PositiveOrZero(this IGuard _, decimal input, string paramName)
     {
         return Guard.MustBe.PositiveOrZero(input, paramName, null);
     }
 
-    public static decimal PositiveOrZero(this IGuard _, decimal input, string paramName, string message)
+    public static IGuard PositiveOrZero(this IGuard _, decimal input, string paramName, string message)
     {
         return Guard.MustBe.PositiveOrZero<decimal>(input, paramName, message);
     }
