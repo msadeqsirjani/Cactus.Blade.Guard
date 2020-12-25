7a25f34f5c68fcaeb42fcaf33900ebc0f9513504
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:26 +0330

    Update Int64.InRange.cs

--- Guard/System.Int64/CodeObject/Int64.InRange.cs
+++ Guard/System.Int64/CodeObject/Int64.InRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static long InRange(this IGuard _, long value, long minimum, long maximum)
+    public static IGuard InRange(this IGuard _, long value, long minimum, long maximum)
     {
         return Guard.MustBe.InRange(value, null, null, minimum, maximum);
     }
 
-    public static long InRange(this IGuard _, long value, string paramName, long minimum, long maximum)
+    public static IGuard InRange(this IGuard _, long value, string paramName, long minimum, long maximum)
     {
         return Guard.MustBe.InRange(value, paramName, null, minimum, maximum);
     }
 
-    public static long InRange(this IGuard _, long value, string paramName, string message, long minimum, long maximum)
+    public static IGuard InRange(this IGuard _, long value, string paramName, string message, long minimum, long maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.InRange(value, minimum, maximum);
 
-        if (value.InRange(minimum, maximum)) return value;
+        if (value.InRange(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
