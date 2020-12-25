04efb4dbe45e9795872c6fb8c4a0740dd2632f94
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:43 +0330

    Update Int64.NotDefault.cs

--- Guard/System.Int64/Int64.NotDefault.cs
+++ Guard/System.Int64/Int64.NotDefault.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static long NotDefault(this IGuard _, long input)
+    public static IGuard NotDefault(this IGuard _, long input)
     {
         return Guard.MustBe.NotDefault(input, null, null);
     }
 
-    public static long NotDefault(this IGuard _, long input, string paramName)
+    public static IGuard NotDefault(this IGuard _, long input, string paramName)
     {
         return Guard.MustBe.NotDefault(input, paramName, null);
     }
 
-    public static long NotDefault(this IGuard _, long input, string paramName, string message)
+    public static IGuard NotDefault(this IGuard _, long input, string paramName, string message)
     {
         return Guard.MustBe.NotDefault<long>(input, paramName, message);
     }
