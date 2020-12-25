ffbd972ddceefa0fe6bd50de68c78f8d7a803f2d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:28 +0330

    Update UInt64.NotInRange.cs

--- Guard/System.UInt64/CodeObject/UInt64.NotInRange.cs
+++ Guard/System.UInt64/CodeObject/UInt64.NotInRange.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static ulong NotInRange(this IGuard _, ulong value, ulong minimum, ulong maximum)
+    public static IGuard NotInRange(this IGuard _, ulong value, ulong minimum, ulong maximum)
     {
         return Guard.MustBe.NotInRange(value, null, null, minimum, maximum);
     }
 
-    public static ulong NotInRange(this IGuard _, ulong value, string paramName, ulong minimum, ulong maximum)
+    public static IGuard NotInRange(this IGuard _, ulong value, string paramName, ulong minimum, ulong maximum)
     {
         return Guard.MustBe.NotInRange(value, paramName, null, minimum, maximum);
     }
 
-    public static ulong NotInRange(this IGuard _, ulong value, string paramName, string message, ulong minimum, ulong maximum)
+    public static IGuard NotInRange(this IGuard _, ulong value, string paramName, string message, ulong minimum, ulong maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.NotInRange(value, minimum, maximum);
 
-        if (value.NotInRange(minimum, maximum)) return value;
+        if (value.NotInRange(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
