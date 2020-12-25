897e25f0505a2be02e7d96260aae433f3f4fd66f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:42:48 +0330

    Update UInt16.NotDefault.cs

--- Guard/System.UInt16/UInt16.NotDefault.cs
+++ Guard/System.UInt16/UInt16.NotDefault.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard NotDefault(this IGuard _, ushort input)
     {
-        return Guard.MustBe.NotDefault(input, null, null);
+        return Guard.MustBe.NotDefault<ushort>(input);
     }
 
     public static IGuard NotDefault(this IGuard _, ushort input, string paramName)
     {
-        return Guard.MustBe.NotDefault(input, paramName, null);
+        return Guard.MustBe.NotDefault<ushort>(input, paramName);
     }
 
     public static IGuard NotDefault(this IGuard _, ushort input, string paramName, string message)
