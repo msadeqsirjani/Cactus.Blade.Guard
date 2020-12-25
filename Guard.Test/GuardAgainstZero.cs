b52adb5f6c4245ec48d63095558451b2b1cd9717
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:21:50 +0330

    Update GuardAgainstZero.cs

--- Guard.Test/GuardAgainstZero.cs
+++ Guard.Test/GuardAgainstZero.cs
@@ -4,53 +4,53 @@ using Xunit;
 
 namespace Guard.Test
 {
-    public class GuardAgainstZero
+    public class GuardMustBeZero
     {
         [Fact]
         public void DoesNothingGivenNonZeroValue()
         {
-            Cactus.Blade.Guard.Guard.Against.Zero(-1, "minusOne");
-            Cactus.Blade.Guard.Guard.Against.Zero(1, "plusOne");
-            Cactus.Blade.Guard.Guard.Against.Zero(int.MinValue, "int.MinValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(int.MaxValue, "int.MaxValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(long.MinValue, "long.MinValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(long.MaxValue, "long.MaxValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(decimal.MinValue, "decimal.MinValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(decimal.MaxValue, "decimal.MaxValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(float.MinValue, "float.MinValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(float.MaxValue, "float.MaxValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(double.MinValue, "double.MinValue");
-            Cactus.Blade.Guard.Guard.Against.Zero(double.MaxValue, "double.MaxValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(-1, "minusOne");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(1, "plusOne");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(int.MinValue, "int.MinValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(int.MaxValue, "int.MaxValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(long.MinValue, "long.MinValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(long.MaxValue, "long.MaxValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(decimal.MinValue, "decimal.MinValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(decimal.MaxValue, "decimal.MaxValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(float.MinValue, "float.MinValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(float.MaxValue, "float.MaxValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(double.MinValue, "double.MinValue");
+            Cactus.Blade.Guard.Guard.MustBe.Zero(double.MaxValue, "double.MaxValue");
         }
 
         [Fact]
         public void ThrowsGivenZeroValueIntZero()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(0, "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(0, "zero"));
         }
 
         [Fact]
         public void ThrowsGivenZeroValueLongZero()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(0L, "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(0L, "zero"));
         }
 
         [Fact]
         public void ThrowsGivenZeroValueDecimalZero()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(0.0M, "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(0.0M, "zero"));
         }
 
         [Fact]
         public void ThrowsGivenZeroValueFloatZero()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(0.0f, "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(0.0f, "zero"));
         }
 
         [Fact]
         public void ThrowsGivenZeroValueDoubleZero()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(0.0, "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(0.0, "zero"));
         }
 
 
@@ -58,55 +58,55 @@ namespace Guard.Test
         [Fact]
         public void ThrowsGivenZeroValueDefaultInt()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(default(int), "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(default(int), "zero"));
         }
 
         [Fact]
         public void ThrowsGivenZeroValueDefaultLong()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(default(long), "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(default(long), "zero"));
         }
 
         [Fact]
         public void ThrowsGivenZeroValueDefaultDecimal()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(default(decimal), "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(default(decimal), "zero"));
         }
 
         [Fact]
         public void ThrowsGivenZeroValueDefaultFloat()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(default(float), "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(default(float), "zero"));
         }
 
         [Fact]
         public void ThrowsGivenZeroValueDefaultDouble()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(default(double), "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(default(double), "zero"));
         }
 
 
         [Fact]
         public void ThrowsGivenZeroValueDecimalDotZero()
         {
-            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.Against.Zero(decimal.Zero, "zero"));
+            Assert.Throws<ArgumentException>(() => Cactus.Blade.Guard.Guard.MustBe.Zero(decimal.Zero, "zero"));
         }
 
         [Fact]
         public void ReturnsExpectedValueWhenGivenNonZeroValue()
         {
-            Assert.Equal(-1, Cactus.Blade.Guard.Guard.Against.Zero(-1, "minusOne"));
-            Assert.Equal(1, Cactus.Blade.Guard.Guard.Against.Zero(1, "plusOne"));
-            Assert.Equal(int.MinValue, Cactus.Blade.Guard.Guard.Against.Zero(int.MinValue, "int.MinValue"));
-            Assert.Equal(int.MaxValue, Cactus.Blade.Guard.Guard.Against.Zero(int.MaxValue, "int.MaxValue"));
-            Assert.Equal(long.MinValue, Cactus.Blade.Guard.Guard.Against.Zero(long.MinValue, "long.MinValue"));
-            Assert.Equal(long.MaxValue, Cactus.Blade.Guard.Guard.Against.Zero(long.MaxValue, "long.MaxValue"));
-            Assert.Equal(decimal.MinValue, Cactus.Blade.Guard.Guard.Against.Zero(decimal.MinValue, "decimal.MinValue"));
-            Assert.Equal(decimal.MaxValue, Cactus.Blade.Guard.Guard.Against.Zero(decimal.MaxValue, "decimal.MaxValue"));
-            Assert.Equal(float.MinValue, Cactus.Blade.Guard.Guard.Against.Zero(float.MinValue, "float.MinValue"));
-            Assert.Equal(float.MaxValue, Cactus.Blade.Guard.Guard.Against.Zero(float.MaxValue, "float.MaxValue"));
-            Assert.Equal(double.MinValue, Cactus.Blade.Guard.Guard.Against.Zero(double.MinValue, "double.MinValue"));
-            Assert.Equal(double.MaxValue, Cactus.Blade.Guard.Guard.Against.Zero(double.MaxValue, "double.MaxValue"));
+            Assert.Equal(-1, Cactus.Blade.Guard.Guard.MustBe.Zero(-1, "minusOne"));
+            Assert.Equal(1, Cactus.Blade.Guard.Guard.MustBe.Zero(1, "plusOne"));
+            Assert.Equal(int.MinValue, Cactus.Blade.Guard.Guard.MustBe.Zero(int.MinValue, "int.MinValue"));
+            Assert.Equal(int.MaxValue, Cactus.Blade.Guard.Guard.MustBe.Zero(int.MaxValue, "int.MaxValue"));
+            Assert.Equal(long.MinValue, Cactus.Blade.Guard.Guard.MustBe.Zero(long.MinValue, "long.MinValue"));
+            Assert.Equal(long.MaxValue, Cactus.Blade.Guard.Guard.MustBe.Zero(long.MaxValue, "long.MaxValue"));
+            Assert.Equal(decimal.MinValue, Cactus.Blade.Guard.Guard.MustBe.Zero(decimal.MinValue, "decimal.MinValue"));
+            Assert.Equal(decimal.MaxValue, Cactus.Blade.Guard.Guard.MustBe.Zero(decimal.MaxValue, "decimal.MaxValue"));
+            Assert.Equal(float.MinValue, Cactus.Blade.Guard.Guard.MustBe.Zero(float.MinValue, "float.MinValue"));
+            Assert.Equal(float.MaxValue, Cactus.Blade.Guard.Guard.MustBe.Zero(float.MaxValue, "float.MaxValue"));
+            Assert.Equal(double.MinValue, Cactus.Blade.Guard.Guard.MustBe.Zero(double.MinValue, "double.MinValue"));
+            Assert.Equal(double.MaxValue, Cactus.Blade.Guard.Guard.MustBe.Zero(double.MaxValue, "double.MaxValue"));
         }
     }
 }
