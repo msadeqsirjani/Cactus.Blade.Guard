30a433ac14bf595008056f27a85665dc322fbf2f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 13:01:11 +0330

    Create Int32.Max.cs

---
+++ Guard/System.Int32/Int32.Max.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Max(int input, int minimum)
+    {
+        return Guard.MustBe.Max(input, minimum);
+    }
+
+    public static IGuard Max(int input, int minimum, string paramName)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName);
+    }
+
+    public static IGuard Max(int input, int minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Max(input, minimum, paramName, message);
+    }
+}
