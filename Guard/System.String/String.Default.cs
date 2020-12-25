736b5278bd3f1ebc88d08d16a0f4a7fc85aacde7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:28:20 +0330

    Update String.Default.cs

--- Guard/System.String/String.Default.cs
+++ Guard/System.String/String.Default.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static string Default(this IGuard _, string input)
+    public static IGuard Default(this IGuard _, string input)
     {
         return Guard.MustBe.Default(input, null, null);
     }
 
-    public static string Default(this IGuard _, string input, string paramName)
+    public static IGuard Default(this IGuard _, string input, string paramName)
     {
         return Guard.MustBe.Default(input, paramName, null);
     }
 
-    public static string Default(this IGuard _, string input, string paramName, string message)
+    public static IGuard Default(this IGuard _, string input, string paramName, string message)
     {
         return Guard.MustBe.Default<string>(input, paramName, message);
     }
