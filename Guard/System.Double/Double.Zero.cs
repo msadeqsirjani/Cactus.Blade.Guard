b5a7f80a6af61bf18c7fa359ab0eb1e0782523a3
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:04:04 +0330

    Update Double.Zero.cs

--- Guard/System.Double/Double.Zero.cs
+++ Guard/System.Double/Double.Zero.cs
@@ -4,12 +4,12 @@ public static partial class GuardExtension
 {
     public static IGuard Zero(this IGuard _, double input)
     {
-        return Guard.MustBe.Zero(input, null, null);
+        return Guard.MustBe.Zero<double>(input);
     }
 
     public static IGuard Zero(this IGuard _, double input, string paramName)
     {
-        return Guard.MustBe.Zero(input, paramName, null);
+        return Guard.MustBe.Zero<double>(input, paramName);
     }
 
 
