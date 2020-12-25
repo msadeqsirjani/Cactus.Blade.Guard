d2d47d2c850b87dc51792d3fbbd1269d7a890c5d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:26:03 +0330

    Update Single.In.cs

--- Guard/System.Single/CodeObject/Single.In.cs
+++ Guard/System.Single/CodeObject/Single.In.cs
@@ -5,17 +5,17 @@ using System.Linq;
 
 public static partial class GuardExtension
 {
-    public static float In(this IGuard _, float value, params float[] values)
+    public static IGuard In(this IGuard _, float value, params float[] values)
     {
         return Guard.MustBe.In(value, null, null, values);
     }
 
-    public static float In(this IGuard _, float value, string paramName, params float[] values)
+    public static IGuard In(this IGuard _, float value, string paramName, params float[] values)
     {
         return Guard.MustBe.In(value, paramName, null, values);
     }
 
-    public static float In(this IGuard _, float value, string paramName, string message, params float[] values)
+    public static IGuard In(this IGuard _, float value, string paramName, string message, params float[] values)
     {
         paramName ??= nameof(value);
         message ??= Message.In(value, values);
@@ -23,7 +23,7 @@ public static partial class GuardExtension
         if (!values.Any())
             throw new ArgumentNullException(Message.Null(values), nameof(values));
 
-        if (value.In(values)) return value;
+        if (value.In(values)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
