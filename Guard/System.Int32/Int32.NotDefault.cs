f07879ffc66a352869ba738d926e7a74b66e94b5
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:22:49 +0330

    Update Int32.NotDefault.cs

--- Guard/System.Int32/Int32.NotDefault.cs
+++ Guard/System.Int32/Int32.NotDefault.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static int NotDefault(this IGuard _, int input)
+    public static IGuard NotDefault(this IGuard _, int input)
     {
         return Guard.MustBe.NotDefault(input, null, null);
     }
 
-    public static int NotDefault(this IGuard _, int input, string paramName)
+    public static IGuard NotDefault(this IGuard _, int input, string paramName)
     {
         return Guard.MustBe.NotDefault(input, paramName, null);
     }
 
-    public static int NotDefault(this IGuard _, int input, string paramName, string message)
+    public static IGuard NotDefault(this IGuard _, int input, string paramName, string message)
     {
         return Guard.MustBe.NotDefault<int>(input, paramName, message);
     }
