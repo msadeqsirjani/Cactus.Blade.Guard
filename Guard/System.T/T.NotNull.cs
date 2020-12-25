ea7c3f387e3b2b2b8e67a4f95305548c7ebcc6fd
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:15:56 +0330

    Create T.NotNull.cs

---
+++ Guard/System.T/T.NotNull.cs
@@ -0,0 +1,27 @@
+﻿using Cactus.Blade.Guard;
+using Cactus.Blade.Guard.Common;
+using System;
+
+public static partial class GuardExtension
+{
+    public static IGuard NotNull<T>(this IGuard _, T input)
+    {
+        return Guard.MustBe.NotNull(input, null, null);
+    }
+
+    public static IGuard NotNull<T>(this IGuard _, T input, string parameter)
+    {
+        return Guard.MustBe.NotNull(input, parameter, null);
+    }
+
+    public static IGuard NotNull<T>(this IGuard guard, T input, string paramName, string message)
+    {
+        paramName ??= nameof(input);
+        message ??= Message.NotNull(paramName);
+
+        if (input is null)
+            throw new ArgumentNullException(paramName, message);
+
+        return guard;
+    }
+}
