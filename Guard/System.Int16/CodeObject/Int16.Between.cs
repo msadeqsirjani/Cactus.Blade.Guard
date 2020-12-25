b8cf17d56fbd3d36fb04d73eaed982210e1ee379
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:25:12 +0330

    Update Int16.Between.cs

--- Guard/System.Int16/CodeObject/Int16.Between.cs
+++ Guard/System.Int16/CodeObject/Int16.Between.cs
@@ -4,22 +4,22 @@ using System;
 
 public static partial class GuardExtension
 {
-    public static short Between(this IGuard _, short value, short minimum, short maximum)
+    public static IGuard Between(this IGuard _, short value, short minimum, short maximum)
     {
         return Guard.MustBe.Between(value, null, null, minimum, maximum);
     }
 
-    public static short Between(this IGuard _, short value, string paramName, short minimum, short maximum)
+    public static IGuard Between(this IGuard _, short value, string paramName, short minimum, short maximum)
     {
         return Guard.MustBe.Between(value, paramName, null, minimum, maximum);
     }
 
-    public static short Between(this IGuard _, short value, string paramName, string message, short minimum, short maximum)
+    public static IGuard Between(this IGuard _, short value, string paramName, string message, short minimum, short maximum)
     {
         paramName ??= nameof(value);
         message ??= Message.Between(value, minimum, maximum);
 
-        if (value.Between(minimum, maximum)) return value;
+        if (value.Between(minimum, maximum)) return _;
 
         throw new ArgumentOutOfRangeException(message, paramName);
     }
