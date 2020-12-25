8c3a5b80310b256eeb9dd589c27c0999a563336e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 12:55:24 +0330

    Create Decimal.Min.cs

---
+++ Guard/System.Decimal/Decimal.Min.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Min(decimal input, decimal minimum)
+    {
+        return Guard.MustBe.Min(input, minimum);
+    }
+
+    public static IGuard Min(decimal input, decimal minimum, string paramName)
+    {
+        return Guard.MustBe.Min(input, minimum, paramName);
+    }
+
+    public static IGuard Min(decimal input, decimal minimum, string paramName, string message)
+    {
+        return Guard.MustBe.Min(input, minimum, paramName, message);
+    }
+}
