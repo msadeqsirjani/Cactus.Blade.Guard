de860a190a7a4faf197c951f525c6a96a7d40ee9
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:15:47 +0330

    Create String.Null.cs

---
+++ Guard/System.String/String.Null.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Null(string input)
+    {
+        return Guard.MustBe.Null(input, null, null);
+    }
+
+    public static IGuard Null(string input, string paramName)
+    {
+        return Guard.MustBe.Null(input, paramName, null);
+    }
+
+    public static IGuard Null(string input, string paramName, string message)
+    {
+        return Guard.MustBe.Null(input, paramName, message);
+    }
+}
