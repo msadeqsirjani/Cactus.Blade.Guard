e29bfd153cc7c9d6f8c81585550bb3ae45f47adb
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:14 +0330

    Update UInt32.Zero.cs

--- Guard/System.UInt32/UInt32.Zero.cs
+++ Guard/System.UInt32/UInt32.Zero.cs
@@ -2,18 +2,18 @@
 
 public static partial class GuardExtension
 {
-    public static uint Zero(this IGuard _, uint input)
+    public static IGuard Zero(this IGuard _, uint input)
     {
         return Guard.MustBe.Zero(input, null, null);
     }
 
-    public static uint Zero(this IGuard _, uint input, string paramName)
+    public static IGuard Zero(this IGuard _, uint input, string paramName)
     {
         return Guard.MustBe.Zero(input, paramName, null);
     }
 
 
-    public static uint Zero(this IGuard _, uint input, string paramName, string message)
+    public static IGuard Zero(this IGuard _, uint input, string paramName, string message)
     {
         return Guard.MustBe.Zero<uint>(input, paramName, message);
     }
