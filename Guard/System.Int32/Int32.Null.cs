790d6a0aade17209caa2f288b3a9223e64b8939b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:16:05 +0330

    Create Int32.Null.cs

---
+++ Guard/System.Int32/Int32.Null.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Null(int? input)
+    {
+        return Guard.MustBe.Null(input, null, null);
+    }
+
+    public static IGuard Null(int? input, string paramName)
+    {
+        return Guard.MustBe.Null(input, paramName, null);
+    }
+
+    public static IGuard Null(int? input, string paramName, string message)
+    {
+        return Guard.MustBe.Null(input, paramName, message);
+    }
+}
