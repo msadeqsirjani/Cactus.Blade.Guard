86e81d7605ace45c2a658d2e24f56a582853f7b8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:16:36 +0330

    Create Char.Null.cs

---
+++ Guard/System.Char/Char.Null.cs
@@ -0,0 +1,19 @@
+﻿using Cactus.Blade.Guard;
+
+public static partial class GuardExtension
+{
+    public static IGuard Null(char? input)
+    {
+        return Guard.MustBe.Null(input, null, null);
+    }
+
+    public static IGuard Null(char? input, string paramName)
+    {
+        return Guard.MustBe.Null(input, paramName, null);
+    }
+
+    public static IGuard Null(char? input, string paramName, string message)
+    {
+        return Guard.MustBe.Null(input, paramName, message);
+    }
+}
