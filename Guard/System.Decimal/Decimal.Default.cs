3c752d80006bf534fb6af056a491e9e0879150ba
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:21:52 +0330

    Update Decimal.Default.cs

--- Guard/System.Decimal/Decimal.Default.cs
+++ Guard/System.Decimal/Decimal.Default.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static decimal Default(this IGuard _, decimal input)
+    public static IGuard Default(this IGuard _, decimal input)
     {
         return Guard.MustBe.Default(input, null, null);
     }
 
-    public static decimal Default(this IGuard _, decimal input, string paramName)
+    public static IGuard Default(this IGuard _, decimal input, string paramName)
     {
         return Guard.MustBe.Default(input, paramName, null);
     }
 
-    public static decimal Default(this IGuard _, decimal input, string paramName, string message)
+    public static IGuard Default(this IGuard _, decimal input, string paramName, string message)
     {
         return Guard.MustBe.Default<decimal>(input, paramName, message);
     }
