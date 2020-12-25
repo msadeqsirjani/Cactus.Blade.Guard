c01627632639897b03733d455d9d0ef73e71e723
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 10:27:37 +0330

    Update UInt64.NotDefault.cs

--- Guard/System.UInt64/UInt64.NotDefault.cs
+++ Guard/System.UInt64/UInt64.NotDefault.cs
@@ -2,17 +2,17 @@
 
 public static partial class GuardExtension
 {
-    public static ulong NotDefault(this IGuard _, ulong input)
+    public static IGuard NotDefault(this IGuard _, ulong input)
     {
         return Guard.MustBe.NotDefault(input, null, null);
     }
 
-    public static ulong NotDefault(this IGuard _, ulong input, string paramName)
+    public static IGuard NotDefault(this IGuard _, ulong input, string paramName)
     {
         return Guard.MustBe.NotDefault(input, paramName, null);
     }
 
-    public static ulong NotDefault(this IGuard _, ulong input, string paramName, string message)
+    public static IGuard NotDefault(this IGuard _, ulong input, string paramName, string message)
     {
         return Guard.MustBe.NotDefault<ulong>(input, paramName, message);
     }
