3a7ffe74098200d981de643c08f60558477f2ad1
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-25 11:15:34 +0330

    Update Boolean.Null.cs

--- Guard/System.Boolean/Boolean.Null.cs
+++ Guard/System.Boolean/Boolean.Null.cs
@@ -1,4 +1,19 @@
-﻿public static partial class GuardExtension
-{
+﻿using Cactus.Blade.Guard;
 
+public static partial class GuardExtension
+{
+    public static IGuard Null(bool? input)
+    {
+        return Guard.MustBe.Null(input, null, null);
+    }
+
+    public static IGuard Null(bool? input, string paramName)
+    {
+        return Guard.MustBe.Null(input, paramName, null);
+    }
+
+    public static IGuard Null(bool? input, string paramName, string message)
+    {
+        return Guard.MustBe.Null(input, paramName, message);
+    }
 }
